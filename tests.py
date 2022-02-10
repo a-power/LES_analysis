4568d9988fd632e42d5202fa232d9bc8eb122430
a-power <a.power@pgr.reading.ac.uk>
2022-02-10 15:56:30 +0000

    "fixed missing bracket"

--- tests.py
+++ tests.py
@@ -4,3 +4,4 @@ dir_in = '/gws/nopw/j04/paracon_rdg/users/toddj/updates_suite/BOMEX_m0020_g0800/
 file_in = dir_in+"BOMEX_m0020_g0800_all_12600.nc"
 data_in = Dataset(file_in, mode='r')
 print(data_in.variables)
+
