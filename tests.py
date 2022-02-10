cd6787a49e54a27958b3509759f8053d81786a15
a-power <a.power@pgr.reading.ac.uk>
2022-02-10 15:55:22 +0000

    "test for variables in file"

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 from netCDF4 import Dataset
 
-dir_in = '/gws/nopw/j04/paracon_rdg/users/toddj/updates_suite/BOMEX_m0020_g0800/diagnostic_files'
+dir_in = '/gws/nopw/j04/paracon_rdg/users/toddj/updates_suite/BOMEX_m0020_g0800/diagnostic_files/'
 file_in = dir_in+"BOMEX_m0020_g0800_all_12600.nc"
 data_in = Dataset(file_in, mode='r')
 print(data_in.variables)
