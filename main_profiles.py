ac2affef41a2cdfd1bcffa9d877572c081d669e2
a-power <a.power@pgr.reading.ac.uk>
2022-02-11 12:04:14 +0000

    corrected test run using ww mean prof

--- main_profiles.py
+++ main_profiles.py
@@ -1,19 +1,20 @@
 import numpy as np
-import xarray as xr
-import functions as f
+import time_av_profiles as avp
 import matplotlib.pyplot as plt
 
-time_in = '14400'
+set_time = '14400'
 
-indir = '/gws/nopw/j04/paracon_rdg/users/toddj/updates_suite/BOMEX_m'
+mydir = '/gws/nopw/j04/paracon_rdg/users/toddj/updates_suite/BOMEX_m'
 
 model_res_list = ['0020_g0800', '0025_g0600', '0040_g0400', '0080_g0200', '0160_g0100', '0320_g0050']
 model_res_list_int = np.array([20, 25, 40, 80, 160, 320])
 
-vars = ['ww_mean']
+myvars = ['ww_mean']
 z_i = 1000
 
-for i, var in enumerate(vars):
+z = avp.time_av_prof(myvars, model_res_list, set_time, mydir)
+
+for i, var in enumerate(myvars):
 
     figs = plt.figure(figsize=(6, 6))
     plt.ylabel(r'z')
@@ -23,4 +24,4 @@ for i, var in enumerate(vars):
         var_plot = np.load(f'files/{res}_{var}_time_av.npy')
         plt.plot(var_plot, z/z_i, label=f'$\\Delta x$ = {str(res)} m')
     plt.legend(fontsize=12)
-    figs.savefig(f'plots/{var}_{timein}_time_av_profile.png')
+    figs.savefig(f'plots/{var}_{set_time}_time_av_profile.png')
--- time_av_profiles.py
+++ time_av_profiles.py
@@ -8,7 +8,8 @@ def time_av_prof(vars, dx_list, time_in, indir):
         time_in = ds_in['time_series_600_600']
         times = time_in.data[0]
         nt = len(times)
-        z = ds_in['z'][0]
+        z_in = ds_in['z']
+        z = z_in.data[0]
 
         for j, var_in in enumerate(vars):
             var_data = np.zeros(z)
@@ -18,4 +19,4 @@ def time_av_prof(vars, dx_list, time_in, indir):
                 var_data += my_var.data
             av_var_data = var_data/nt
             np.save(f'files/{dx}_{var_in}_time_av', av_var_data)
-    return
+    return z
