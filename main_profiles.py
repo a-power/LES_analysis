c7d7dd313e0640505d3241b133e578a26308d0b9
a-power <a.power@pgr.reading.ac.uk>
2022-02-11 15:35:01 +0000

    printing steps using ww mean prof - figures not saving on jasmin

--- main_profiles.py
+++ main_profiles.py
@@ -11,19 +11,22 @@ model_res_list_int = np.array([20, 25, 40, 80, 160, 320])
 
 myvars = ['ww_mean']
 z_i = 1000
-
 z = avp.time_av_prof(myvars, model_res_list, set_time, mydir)
+print("retrieved data and time averaged complete")
 
 for i, var in enumerate(myvars):
+    print('plotting', var, "number of vars = ", i)
 
     figs = plt.figure(figsize=(6, 6))
     plt.ylabel(r'z')
-    plt.xlabel(f'${var}$')
+    plt.xlabel(f'{var}')
 
     for m, res in enumerate(model_res_list_int):
         var_plot = np.load(f'files/{res}_{var}_time_av.npy')
+        print(var_plot)
         plt.plot(var_plot, z/z_i, label=f'$\\Delta x$ = {str(res)} m')
     plt.legend(fontsize=12)
     figs.savefig(f'plots/{var}_{set_time}_time_av_profile.png')
     plt.show()
+    print('end fig')
 
