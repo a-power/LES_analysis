d9df4fb56491776e83acda22a30ecbdffd64abad
a-power <a.power@pgr.reading.ac.uk>
2022-02-14 13:44:38 +0000

    update - not fully finished

--- main_dynamic.py
+++ main_dynamic.py
@@ -2,8 +2,12 @@ import matplotlib.pyplot as plt
 import time_av_dynamic as tdy
 
 set_time = '14400'
-mydir = '/gws/nopw/j04/paracon_rdg/users/toddj/updates_suite/BOMEX_m'
+indir = '/gws/nopw/j04/paracon_rdg/users/toddj/updates_suite/BOMEX_m'
 model_res_list = ['0020_g0800']
+outdir = '/gws/nopw/j04/paracon_rdg/users/apower/LES_analysis/'
+odir = outdir + '20m_gauss_dyn' +'/'
+
+os.makedirs(odir, exist_ok = True)
 
 sigma_list = np.array([20, 40])
 opgrid = 'w'
