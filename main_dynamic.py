34fe13c4134d7b2bc514c829489eeb4a37503fc0
a-power <a.power@pgr.reading.ac.uk>
2022-02-21 17:18:23 +0000

    code for short serial

--- main_dynamic.py
+++ main_dynamic.py
@@ -7,7 +7,7 @@ set_time = '14400'
 in_dir = '/gws/nopw/j04/paracon_rdg/users/toddj/updates_suite/BOMEX_m'
 model_res_list = ['0020_g0800']
 outdir_og = '/gws/nopw/j04/paracon_rdg/users/apower/LES_analysis/'
-outdir = outdir_og + '20m_gauss_dyn' +'/'
+outdir = outdir_og + '20m_gauss_dyn_short' +'/'
 
 plotdir = outdir_og+'plots/dyn/'
 
