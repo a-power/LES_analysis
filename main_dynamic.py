a402315181c71b21371c71635d6d334557baf29f
a-power <a.power@pgr.reading.ac.uk>
2022-02-22 10:20:05 +0000

    altered main to go to work/scratch-pw

--- main_dynamic.py
+++ main_dynamic.py
@@ -6,9 +6,8 @@ import os
 set_time = '14400'
 in_dir = '/gws/nopw/j04/paracon_rdg/users/toddj/updates_suite/BOMEX_m'
 model_res_list = ['0020_g0800']
-outdir_og = '/gws/nopw/j04/paracon_rdg/users/apower/LES_analysis/'
+outdir_og = '/work/scratch-pw/apower'
 outdir = outdir_og + '20m_gauss_dyn_p' +'/'
-
 plotdir = outdir_og+'plots/dyn/'
 
 os.makedirs(outdir, exist_ok = True)
