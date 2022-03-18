df83c1ec776bce532c52272c1f98aae1d42a87b0
a-power <a.power@pgr.reading.ac.uk>
2022-03-18 16:57:58 +0000

    Cs for dry cbl

--- main_Cs.py
+++ main_Cs.py
@@ -4,11 +4,15 @@ import xarray as xr
 import numpy as np
 
 av_type = 'all'
-ingrid = 'w'
+mygrid = 'p'
 
-plotdir = '/gws/nopw/j04/paracon_rdg/users/apower/LES_analysis/plots/dyn/trace/'
-path20f = '/gws/nopw/j04/paracon_rdg/users/apower/LES_analysis/20m_gauss_dyn_w_trace/'
-file20 = "BOMEX_m0020_g0800_all_14400_filter_"
+# plotdir = '/gws/nopw/j04/paracon_rdg/users/apower/LES_analysis/plots/dyn/trace/'
+# path20f = '/gws/nopw/j04/paracon_rdg/users/apower/LES_analysis/20m_gauss_dyn_w_trace/'
+# file20 = "BOMEX_m0020_g0800_all_14400_filter_"
+
+plotdir = '/storage/silver/MONC_data/Alanna/dry_CBL/plots/dyn/time_av'
+path20f = '/storage/silver/MONC_data/Alanna/dry_CBL/20m_gauss_sig_Delta'
+file20 = "cbl_13200__filter_"
 
 data_2D = path20f+file20+str('ga00.nc')
 data_4D = path20f+file20+str('ga01.nc')
@@ -18,22 +22,23 @@ data_4D = path20f+file20+str('ga01.nc')
 # times = time_data.data
 # ds_in.close()
 
-Cs_2D_prof_t0, times = t_dy.Cs(data_2D, dx=20, dx_hat=40, ingrid = 'w', t_in=0)
-Cs_2D_prof_t1, times = t_dy.Cs(data_2D, dx=20, dx_hat=40, ingrid = 'w', t_in=1)
-Cs_2D_prof_t2, times = t_dy.Cs(data_2D, dx=20, dx_hat=40, ingrid = 'w', t_in=2)
-Cs_2D_av = (Cs_2D_prof_t0 + Cs_2D_prof_t1 + Cs_2D_prof_t2)/3
+Cs_2D_prof_t0, times = t_dy.Cs(data_2D, dx=20, dx_hat=40, ingrid = mygrid, t_in=0)
+Cs_2D_prof_t1, times = t_dy.Cs(data_2D, dx=20, dx_hat=40, ingrid = mygrid, t_in=1)
+#Cs_2D_prof_t2, times = t_dy.Cs(data_2D, dx=20, dx_hat=40, ingrid = mygrid, t_in=2)
+Cs_2D_av = (Cs_2D_prof_t0 + Cs_2D_prof_t1)/len(times) # + Cs_2D_prof_t2
 
-Cs_4D_prof_t0, times = t_dy.Cs(data_4D, dx=20, dx_hat=80, ingrid = 'w', t_in=0)
-Cs_4D_prof_t1, times = t_dy.Cs(data_4D, dx=20, dx_hat=80, ingrid = 'w', t_in=1)
-Cs_4D_prof_t2, times = t_dy.Cs(data_4D, dx=20, dx_hat=80, ingrid = 'w', t_in=2)
-Cs_4D_av = (Cs_4D_prof_t0 + Cs_4D_prof_t1 + Cs_4D_prof_t2)/3
+Cs_4D_prof_t0, times = t_dy.Cs(data_4D, dx=20, dx_hat=80, ingrid = mygrid, t_in=0)
+Cs_4D_prof_t1, times = t_dy.Cs(data_4D, dx=20, dx_hat=80, ingrid = mygrid, t_in=1)
+#Cs_4D_prof_t2, times = t_dy.Cs(data_4D, dx=20, dx_hat=80, ingrid = mygrid, t_in=2)
+Cs_4D_av = (Cs_4D_prof_t0 + Cs_4D_prof_t1)/len(times) # + Cs_4D_prof_t2
 
 
 #########################plots#########################
 
 #if times_2D.all() == times_4D.all():
 
-z = np.arange(0,3020,20)
+z = np.arange(0,(len(Cs_2D_prof_t0)*20),20)
+z_i = 1000
 
 
 y = 200
@@ -82,14 +87,14 @@ plt.cla()
 plt.close()
 
 fig2 = plt.figure(figsize=(10, 8))
-plt.plot(Cs_2D_prof_t0, z/500, 'r-', markersize=6, label='$t0 C_{s 2 \\Delta} $')
-plt.plot(Cs_4D_prof_t0, z/500, 'r-*', markersize=6, label='$t0 C_{s 4 \\Delta} $')
-plt.plot(Cs_2D_prof_t1, z / 500, 'g-', markersize=6, label='$t1 C_{s 2 \\Delta} $')
-plt.plot(Cs_4D_prof_t1, z / 500, 'g-*', markersize=6, label='$t1 C_{s 4 \\Delta} $')
-plt.plot(Cs_2D_prof_t2, z / 500, 'b-', markersize=6, label='$t2 C_{s 2 \\Delta} $')
-plt.plot(Cs_4D_prof_t2, z / 500, 'b-*', markersize=6, label='$t2 C_{s 4 \\Delta} $')
-plt.plot(Cs_2D_av, z / 500, 'k-', markersize=6, label='$Average C_{s 2 \\Delta} $')
-plt.plot(Cs_4D_av, z / 500, 'k-*', markersize=6, label='$Average C_{s 4 \\Delta} $')
+plt.plot(Cs_2D_prof_t0, z/z_i, 'r-', markersize=6, label='$t0 C_{s 2 \\Delta} $')
+plt.plot(Cs_4D_prof_t0, z/z_i, 'r-*', markersize=6, label='$t0 C_{s 4 \\Delta} $')
+plt.plot(Cs_2D_prof_t1, z / z_i, 'g-', markersize=6, label='$t1 C_{s 2 \\Delta} $')
+plt.plot(Cs_4D_prof_t1, z / z_i, 'g-*', markersize=6, label='$t1 C_{s 4 \\Delta} $')
+# plt.plot(Cs_2D_prof_t2, z / z_i, 'b-', markersize=6, label='$t2 C_{s 2 \\Delta} $')
+# plt.plot(Cs_4D_prof_t2, z / z_i, 'b-*', markersize=6, label='$t2 C_{s 4 \\Delta} $')
+plt.plot(Cs_2D_av, z / z_i, 'k-', markersize=6, label='$Average C_{s 2 \\Delta} $')
+plt.plot(Cs_4D_av, z / z_i, 'k-*', markersize=6, label='$Average C_{s 4 \\Delta} $')
 plt.xlim(-0.01, 0.2)
 plt.title(f'times: {times}')
 plt.ylabel('$z/z_i$')
@@ -101,14 +106,14 @@ plt.cla()
 plt.close()
 
 fig3 = plt.figure(figsize=(10, 8))
-plt.plot(Cs_2D_prof_t0, z / 500, 'r-', markersize=6, label='$t0 C_{s 2 \\Delta} $')
-plt.plot(Cs_4D_prof_t0, z / 500, 'r-*', markersize=6, label='$t0 C_{s 4 \\Delta} $')
-plt.plot(Cs_2D_prof_t1, z / 500, 'g-', markersize=6, label='$t1 C_{s 2 \\Delta} $')
-plt.plot(Cs_4D_prof_t1, z / 500, 'g-*', markersize=6, label='$t1 C_{s 4 \\Delta} $')
-plt.plot(Cs_2D_prof_t2, z / 500, 'b-', markersize=6, label='$t2 C_{s 2 \\Delta} $')
-plt.plot(Cs_4D_prof_t2, z / 500, 'b-*', markersize=6, label='$t2 C_{s 4 \\Delta} $')
-plt.plot(Cs_2D_av, z / 500, 'k-', markersize=6, label='$Average C_{s 2 \\Delta} $')
-plt.plot(Cs_4D_av, z / 500, 'k-*', markersize=6, label='$Average C_{s 4 \\Delta} $')
+plt.plot(Cs_2D_prof_t0, z/z_i, 'r-', markersize=6, label='$t0 C_{s 2 \\Delta} $')
+plt.plot(Cs_4D_prof_t0, z/z_i, 'r-*', markersize=6, label='$t0 C_{s 4 \\Delta} $')
+plt.plot(Cs_2D_prof_t1, z / z_i, 'g-', markersize=6, label='$t1 C_{s 2 \\Delta} $')
+plt.plot(Cs_4D_prof_t1, z / z_i, 'g-*', markersize=6, label='$t1 C_{s 4 \\Delta} $')
+# plt.plot(Cs_2D_prof_t2, z / z_i, 'b-', markersize=6, label='$t2 C_{s 2 \\Delta} $')
+# plt.plot(Cs_4D_prof_t2, z / z_i, 'b-*', markersize=6, label='$t2 C_{s 4 \\Delta} $')
+plt.plot(Cs_2D_av, z / z_i, 'k-', markersize=6, label='$Average C_{s 2 \\Delta} $')
+plt.plot(Cs_4D_av, z / z_i, 'k-*', markersize=6, label='$Average C_{s 4 \\Delta} $')
 plt.xlim(-0.01, 0.2)
 plt.ylim(0, 2)
 plt.title(f'times: {times}')
@@ -121,13 +126,13 @@ plt.cla()
 plt.close()
 
 fig4 = plt.figure(figsize=(10, 8))
-plt.plot(Cs_2D_prof_t0, z/500, 'r-', markersize=6, label='$t0 C_{s 2 \\Delta} $')
+plt.plot(Cs_2D_prof_t0, z/z_i, 'r-', markersize=6, label='$t0 C_{s 2 \\Delta} $')
 #plt.plot(Cs_4D_prof_t0, z/500, 'r-*', markersize=6, label='$t0 C_{s 4 \\Delta} $')
-plt.plot(Cs_2D_prof_t1, z / 500, 'g-', markersize=6, label='$t1 C_{s 2 \\Delta} $')
+plt.plot(Cs_2D_prof_t1, z / z_i, 'g-', markersize=6, label='$t1 C_{s 2 \\Delta} $')
 #plt.plot(Cs_4D_prof_t1, z / 500, 'g-*', markersize=6, label='$t1 C_{s 4 \\Delta} $')
-plt.plot(Cs_2D_prof_t2, z / 500, 'b-', markersize=6, label='$t2 C_{s 2 \\Delta} $')
+#plt.plot(Cs_2D_prof_t2, z / 500, 'b-', markersize=6, label='$t2 C_{s 2 \\Delta} $')
 #plt.plot(Cs_4D_prof_t2, z / 500, 'b-*', markersize=6, label='$t2 C_{s 4 \\Delta} $')
-plt.plot(Cs_2D_av, z / 500, 'k-', markersize=6, label='$Average C_{s 2 \\Delta} $')
+plt.plot(Cs_2D_av, z / z_i, 'k-', markersize=6, label='$Average C_{s 2 \\Delta} $')
 #plt.plot(Cs_4D_av, z / 500, 'k-*', markersize=6, label='$Average C_{s 4 \\Delta} $')
 plt.xlim(-0.01, 0.2)
 plt.title(f'times: {times}')
@@ -141,13 +146,13 @@ plt.close()
 
 fig5 = plt.figure(figsize=(10, 8))
 #plt.plot(Cs_2D_prof_t0, z/500, 'r-', markersize=6, label='$t0 C_{s 2 \\Delta} $')
-plt.plot(Cs_4D_prof_t0, z/500, 'r-*', markersize=6, label='$t0 C_{s 4 \\Delta} $')
+plt.plot(Cs_4D_prof_t0, z/z_i, 'r-*', markersize=6, label='$t0 C_{s 4 \\Delta} $')
 #plt.plot(Cs_2D_prof_t1, z / 500, 'g-', markersize=6, label='$t1 C_{s 2 \\Delta} $')
-plt.plot(Cs_4D_prof_t1, z / 500, 'g-*', markersize=6, label='$t1 C_{s 4 \\Delta} $')
+plt.plot(Cs_4D_prof_t1, z / z_i, 'g-*', markersize=6, label='$t1 C_{s 4 \\Delta} $')
 #plt.plot(Cs_2D_prof_t2, z / 500, 'b-', markersize=6, label='$t2 C_{s 2 \\Delta} $')
-plt.plot(Cs_4D_prof_t2, z / 500, 'b-*', markersize=6, label='$t2 C_{s 4 \\Delta} $')
+#plt.plot(Cs_4D_prof_t2, z / z_i, 'b-*', markersize=6, label='$t2 C_{s 4 \\Delta} $')
 #plt.plot(Cs_2D_av, z / 500, 'k-', markersize=6, label='$Average C_{s 2 \\Delta} $')
-plt.plot(Cs_4D_av, z / 500, 'k-*', markersize=6, label='$Average C_{s 4 \\Delta} $')
+plt.plot(Cs_4D_av, z / z_i, 'k-*', markersize=6, label='$Average C_{s 4 \\Delta} $')
 plt.xlim(-0.01, 0.2)
 plt.title(f'times: {times}')
 plt.ylabel('$z/z_i$')
@@ -159,14 +164,14 @@ plt.cla()
 plt.close()
 
 fig6 = plt.figure(figsize=(10, 8))
-plt.plot(Cs_2D_prof_t0, z/500, 'r-', markersize=6, label='$t0 C_{s 2 \\Delta} $')
-plt.plot(Cs_4D_prof_t0, z/500, 'r-*', markersize=6, label='$t0 C_{s 4 \\Delta} $')
-plt.plot(Cs_2D_prof_t1, z / 500, 'g-', markersize=6, label='$t1 C_{s 2 \\Delta} $')
-plt.plot(Cs_4D_prof_t1, z / 500, 'g-*', markersize=6, label='$t1 C_{s 4 \\Delta} $')
-plt.plot(Cs_2D_prof_t2, z / 500, 'b-', markersize=6, label='$t2 C_{s 2 \\Delta} $')
-plt.plot(Cs_4D_prof_t2, z / 500, 'b-*', markersize=6, label='$t2 C_{s 4 \\Delta} $')
-plt.plot(Cs_2D_av, z / 500, 'k-', markersize=6, label='$Average C_{s 2 \\Delta} $')
-plt.plot(Cs_4D_av, z / 500, 'k-*', markersize=6, label='$Average C_{s 4 \\Delta} $')
+plt.plot(Cs_2D_prof_t0, z/z_i, 'r-', markersize=6, label='$t0 C_{s 2 \\Delta} $')
+plt.plot(Cs_4D_prof_t0, z/z_i, 'r-*', markersize=6, label='$t0 C_{s 4 \\Delta} $')
+plt.plot(Cs_2D_prof_t1, z / z_i, 'g-', markersize=6, label='$t1 C_{s 2 \\Delta} $')
+plt.plot(Cs_4D_prof_t1, z / z_i, 'g-*', markersize=6, label='$t1 C_{s 4 \\Delta} $')
+# plt.plot(Cs_2D_prof_t2, z / z_i, 'b-', markersize=6, label='$t2 C_{s 2 \\Delta} $')
+# plt.plot(Cs_4D_prof_t2, z / z_i, 'b-*', markersize=6, label='$t2 C_{s 4 \\Delta} $')
+plt.plot(Cs_2D_av, z / z_i, 'k-', markersize=6, label='$Average C_{s 2 \\Delta} $')
+plt.plot(Cs_4D_av, z / z_i, 'k-*', markersize=6, label='$Average C_{s 4 \\Delta} $')
 plt.xlim(-0.01, 0.2)
 plt.title(f'times: {times}')
 plt.ylabel('$z/z_i$')
@@ -178,8 +183,8 @@ plt.cla()
 plt.close()
 
 fig7 = plt.figure(figsize=(10, 8))
-plt.plot(Cs_2D_prof_t0, z/500, 'r-', markersize=6, label='$t0 C_{s 2 \\Delta} $')
-plt.plot(Cs_4D_prof_t0, z/500, 'b-', markersize=6, label='$t0 C_{s 4 \\Delta} $')
+plt.plot(Cs_2D_prof_t0, z/z_i, 'r-', markersize=6, label='$t0 C_{s 2 \\Delta} $')
+plt.plot(Cs_4D_prof_t0, z/z_i, 'b-', markersize=6, label='$t0 C_{s 4 \\Delta} $')
 # plt.plot(Cs_2D_prof_t1, z / 500, 'g-', markersize=6, label='$t1 C_{s 2 \\Delta} $')
 # plt.plot(Cs_4D_prof_t1, z / 500, 'g-*', markersize=6, label='$t1 C_{s 4 \\Delta} $')
 # plt.plot(Cs_2D_prof_t2, z / 500, 'b-', markersize=6, label='$t2 C_{s 2 \\Delta} $')
