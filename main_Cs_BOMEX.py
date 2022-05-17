4c9a40561b032e10263863f981c809c619d63d03
a-power <a.power@pgr.reading.ac.uk>
2022-05-18 00:28:07 +0100

    changed Mij calc to use peters filtered |S| rather than calc myself using filtered Sij. Running on the RACC. Ficed some typos

--- main_Cs_BOMEX.py
+++ main_Cs_BOMEX.py
@@ -25,7 +25,7 @@ data_4D = path20f+file20+str('ga01.nc')
 dataset_name2 = path20f+file20+'profiles_2D.nc'
 dataset_name4 = path20f+file20+'profiles_4D.nc'
 
-Cs_prof_sq_2d, Cs_prof_2d, LM_prof_2d, MM_prof_2d, Cs_sq_field_2d, LM_field_2d, MM_field_2d, L_ij, M_ij = \
+Cs_prof_sq_2d, Cs_prof_2d, LM_prof_2d, MM_prof_2d, Cs_sq_field_2d, LM_field_2d, MM_field_2d, L_ij_2, M_ij_2 = \
     t_dy.Cs(data_2D, dx=20, dx_hat=40, ingrid = mygrid, t_in=0, save_all=3)
 
 print(Cs_prof_sq_2d, Cs_prof_2d, LM_prof_2d, MM_prof_2d, Cs_sq_field_2d, LM_field_2d, MM_field_2d)
@@ -41,8 +41,8 @@ save_field(ds_in2, MM_prof_2d)
 save_field(ds_in2, Cs_sq_field_2d)
 save_field(ds_in2, LM_field_2d)
 save_field(ds_in2, MM_field_2d)
-save_field(ds_in2, L_ij)
-save_field(ds_in2, M_ij)
+save_field(ds_in2, L_ij_2)
+save_field(ds_in2, M_ij_2)
 
 Cs_prof_sq_2d = None        #free memory
 Cs_prof_2d = None           #free memory
@@ -51,12 +51,14 @@ MM_prof_2d = None           #free memory
 Cs_sq_field_2d = None       #free memory
 LM_field_2d = None          #free memory
 MM_field_2d = None          #free memory
+L_ij_2 = None
+M_ij_2 = None
 
 ds_2.close()
 
 
-Cs_prof_sq_4d, Cs_prof_4d, LM_prof_4d, MM_prof_4d, Cs_sq_field_4d, LM_field_4d, MM_field_4d = \
-    t_dy.Cs(data_4D, dx=20, dx_hat=80, ingrid = mygrid, t_in=0, save_all=2)
+Cs_prof_sq_4d, Cs_prof_4d, LM_prof_4d, MM_prof_4d, Cs_sq_field_4d, LM_field_4d, MM_field_4d, L_ij_4, M_ij_4  = \
+    t_dy.Cs(data_4D, dx=20, dx_hat=80, ingrid = mygrid, t_in=0, save_all=3)
 
 ds_4 = xr.Dataset()
 ds_4.to_netcdf(dataset_name4, mode='w')
@@ -69,6 +71,8 @@ save_field(ds_in4, MM_prof_4d)
 save_field(ds_in4, Cs_sq_field_4d)
 save_field(ds_in4, LM_field_4d)
 save_field(ds_in4, MM_field_4d)
+save_field(ds_in4, L_ij_4)
+save_field(ds_in4, M_ij_4)
 
 ds_4.close()
 
