9df2d46b647016ce40fe8866752af4dfc4f4b3ab
a-power <a.power@pgr.reading.ac.uk>
2022-06-24 16:40:17 +0100

    trying to fix plots for mean profiles

--- main_profiles.py
+++ main_profiles.py
@@ -2,19 +2,26 @@ import numpy as np
 import time_av_profiles as avp
 import matplotlib.pyplot as plt
 
-mydata = 'dry_cbl' #'bomex'
+ #'dry_cbl'
 
-#set_time = '14400'
-set_time='13200'
+set_time = '14400'
+#set_time='13200'
 
-#mydir = '/gws/nopw/j04/paracon_rdg/users/toddj/updates_suite/BOMEX_m'
-mydir='/storage/silver/scenario/si818415/phd/'
+mydir = '/gws/nopw/j04/paracon_rdg/users/toddj/updates_suite/BOMEX_m'
+mydir_filt = '/work/scratch-pw/apower/20m_gauss_dyn_update_subfilt/BOMEX_m0020_g0800_all_14400_gaussian_filter_ga0'
+#mydir='/storage/silver/scenario/si818415/phd/'
 
-#model_res_list = ['0020_g0800', '0025_g0640', '0040_g0400', '0080_g0200', '0160_g0100', '0320_g0050']
-#model_res_list_int = np.array([20, 25, 40, 80, 160, 320])
+model_res_list = ['0020_g0800', '0040_g0400', '0080_g0200']
+model_res_list_int = np.array([20, 40, 80])
 
-model_res_list = ['20', '50', '100', '200', '400', '800']
-model_res_list_int = np.array([20, 50, 100, 200, 400, 800])
+filter_res_list = ['0', '1']
+filter_res_list_int = np.array([40, 80])
+
+# model_res_list = ['0020_g0800', '0025_g0640', '0040_g0400', '0080_g0200', '0160_g0100', '0320_g0050']
+# model_res_list_int = np.array([20, 25, 40, 80, 160, 320])
+
+# model_res_list = ['20', '50', '100', '200', '400', '800']
+# model_res_list_int = np.array([20, 50, 100, 200, 400, 800])
 
 
 
@@ -25,22 +32,70 @@ model_res_list_int = np.array([20, 50, 100, 200, 400, 800])
 #           'liquid_mmr_mean','wtheta_ad_mean','wtheta_cn_mean','wql_ad_mean','wql_cn_mean',\
 #           'wqv_ad_mean', 'wqv_cn_mean', 'total_cloud_fraction']
 
-myvars = ['w_wind_mean']
+myvars = ['w_wind_mean','ww_mean','theta_mean', 'wtheta_cn_mean', 'wql_cn_mean',\
+          'wqv_cn_mean', 'total_cloud_fraction']
 
-avp.time_av_prof(myvars, model_res_list, set_time, mydir, mydata)
+avp.time_av_prof(myvars, model_res_list, set_time, mydir, 'bomex_og')
 
-for i, var in enumerate(myvars):
+my_vars_filt = ['f(w_on_w.w_on_w)_r', 'f(w_on_w.th_on_w)_r', 'f(w_on_w.q_total_on_w)_r']
+avp.time_av_prof(my_vars_filt, filter_res_list, set_time, mydir_filt, 'bomex_filt')
+
+plot_vars = []
+plot_vars.append(myvars)
+plot_vars.append('wqt')
+plot_vars.append(my_vars_filt)
+
+for i, var in enumerate(plot_vars):
 
     figs = plt.figure(figsize=(6, 6))
     plt.ylabel(r'z')
-    plt.xlabel(f'{var}')
+
+    if var == 'w_wind_mean':
+        var_name = "$\overline{w'}$"
+        mydata = 'bomex_og'
+    elif var == 'ww_mean':
+        var_name = "$\overline{w'^2}$"
+        mydata = 'bomex_og'
+    elif var == 'theta_mean':
+        var_name = "$\overline{\\theta'}$"
+        mydata = 'bomex_og'
+    elif var == 'wtheta_cn_mean':
+        var_name = "$\overline{w' \\theta'}$"
+        mydata = 'bomex_og'
+    elif var == 'total_cloud_fraction':
+        var_name = "Total cloud fraction"
+        mydata = 'bomex_og'
+    elif var == 'wqv_cn_mean':
+        var_name = "$\overline{w' q_v'}$"
+        mydata = 'bomex_og'
+    elif var == 'wql_cn_mean':
+        var_name = "$\overline{w' q_l'}$"
+        mydata = 'bomex_og'
+    elif var == 'wqt':
+        var_name = "$\overline{w' q_t'}$"
+        mydata = 'bomex_og'
+
+    elif var == 'f(w_on_w.w_on_w)_r':
+        var_name = "$\overline{w'^2}$"
+        mydata = 'bomex_filt'
+    elif var == 'f(w_on_w.th_on_w)_r':
+        var_name = "$\overline{w' \\theta'}$"
+        mydata = 'bomex_filt'
+    elif var == 'f(w_on_w.q_total_on_w)_r':
+        var_name = "$\overline{w' q_t'}$"
+        mydata = 'bomex_filt'
+
+    else:
+        print('name not configured for this variable')
+
+    plt.xlabel(f'{var_name}')
 
     for m, res in enumerate(model_res_list):
         var_plot = np.load(f'files/{mydata}/{res}_{var}_time_av.npy')
         z_plot = np.load(f'files/{mydata}/{res}_z.npy')
         plt.plot(var_plot, z_plot, label=f'$\\Delta x$ = {str(model_res_list_int[m])} m')
     plt.legend(fontsize=12)
-    figs.savefig(f'plots/{mydata}/{var}_{set_time}_time_av_profile.png')
+    figs.savefig(f'plots/{mydata}/{var}_{set_time}_profile.png')
     print("Finished plotting profile for ", var, ",", \
           len(model_res_list)-(m+1), "variables remaining")
 
