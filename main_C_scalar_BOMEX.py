5627ff4eae9fed82769b719ab53ba332f12f4835
a-power <a.power@pgr.reading.ac.uk>
2022-10-07 17:20:21 +0100

    scalar chanfges more general

--- main_C_scalar_BOMEX.py
+++ main_C_scalar_BOMEX.py
@@ -5,6 +5,8 @@ import xarray as xr
 
 av_type = 'all'
 mygrid = 'w'
+scalar = 'q_total' #'theta'
+
 
 plotdir = '/gws/nopw/j04/paracon_rdg/users/apower/LES_analysis/plots/dyn/update_subfilt/'
 path20f = '/work/scratch-pw/apower/20m_gauss_dyn_update_subfilt/'
@@ -20,14 +22,14 @@ data_2D = path20f+file20+str('ga00.nc')
 data_4D = path20f+file20+str('ga01.nc')
 
 
-dataset_name2 = path20f+file20+'Cq_2D.nc'
-dataset_name4 = path20f+file20+'Cq_4D.nc'
+dataset_name2 = path20f+file20+f'C{scalar}_2D.nc'
+dataset_name4 = path20f+file20+f'C{scalar}_4D.nc'
 
 
 ########################################################################
 
 C_q_sq_prof_2D, C_q_prof_2D, HR_prof_2D, RR_prof_2D, C_q_sq_field_2D, HR_field_2D, RR_field_2D, Hj_2D, Rj_2D = \
-    t_dy.C_scalar('q_total', data_2D, dx=20, dx_hat=40, ingrid=mygrid)
+    t_dy.C_scalar(scalar, data_2D, dx=20, dx_hat=40, ingrid=mygrid)
 
 ds_2 = xr.Dataset()
 ds_2.to_netcdf(dataset_name2, mode='w')
@@ -58,7 +60,7 @@ Rj_2D = None
 ##########################################
 
 C_q_sq_prof_4D, C_q_prof_4D, HR_prof_4D, RR_prof_4D, C_q_sq_field_4D, HR_field_4D, RR_field_4D, Hj_4D, Rj_4D = \
-    t_dy.C_scalar('q_total', data_2D, dx=20, dx_hat=80, ingrid=mygrid)
+    t_dy.C_scalar(scalar, data_2D, dx=20, dx_hat=80, ingrid=mygrid)
 
 ds_4 = xr.Dataset()
 ds_4.to_netcdf(dataset_name4, mode='w')
