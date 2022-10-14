24c924cb8054b4915f617c6aa8b4cc98ebbe6b40
a-power <a.power@pgr.reading.ac.uk>
2022-10-14 14:04:48 +0100

    plotting the l_mixes for all

--- plot_C.py
+++ plot_C.py
@@ -7,47 +7,48 @@ import dynamic_functions as dyn
 np.seterr(divide='ignore') #ignore divide by zero errors in beta calcs
 np.seterr(invalid='ignore')
 
-dir_data_Cs = '/gws/nopw/j04/paracon_rdg/users/apower/LES_analysis/20m_update_subfilt/BOMEX_m0020_g0800_all_14400_gaussian_filter_profiles_'
-dir_data_C_scalar = '/work/scratch-pw/apower/20m_gauss_dyn_update_subfilt/BOMEX_m0020_g0800_all_14400_gaussian_filter_C_scalar_'
-dir_data_Cq_tot = '/work/scratch-pw/apower/20m_gauss_dyn_update_subfilt/BOMEX_m0020_g0800_all_14400_gaussian_filter_Cq_'
-plotdir = '/gws/nopw/j04/paracon_rdg/users/apower/LES_analysis/20m_update_subfilt/plots/'
+dir_data_Cs = '/work/scratch-pw/apower/20m_gauss_dyn/BOMEX_m0020_g0800_all_14400_gaussian_filter_Cs_'
+dir_data_C_th = '/work/scratch-pw/apower/20m_gauss_dyn/BOMEX_m0020_g0800_all_14400_gaussian_filter_C_th_'
+dir_data_Cq_tot = '/work/scratch-pw/apower/20m_gauss_dyn/BOMEX_m0020_g0800_all_14400_gaussian_filter_Cqt_'
+plotdir = '/gws/nopw/j04/paracon_rdg/users/apower/LES_analysis/20m_gauss_dyn/plots/'
 os.makedirs(plotdir, exist_ok = True)
 
 data_2D_s = xr.open_dataset(dir_data_Cs + '2D.nc')
 data_4D_s = xr.open_dataset(dir_data_Cs + '4D.nc')
 
-data_2D_scalar = xr.open_dataset(dir_data_C_scalar + '2D.nc')
-data_4D_scalar = xr.open_dataset(dir_data_C_scalar + '4D.nc')
+data_2D_th = xr.open_dataset(dir_data_C_th + '2D.nc')
+data_4D_th = xr.open_dataset(dir_data_C_th + '4D.nc')
 
-data_2D_qtot = xr.open_dataset(dir_data_Cq_tot + '2D_attempt.nc')
-data_4D_qtot = xr.open_dataset(dir_data_Cq_tot + '4D_attempt.nc')
+data_2D_qtot = xr.open_dataset(dir_data_Cq_tot + '2D.nc')
+data_4D_qtot = xr.open_dataset(dir_data_Cq_tot + '4D.nc')
 
 
 z = np.arange(0, 3020, 20)
 z_i = 490
 
+#index of 0 at the start is to get rid of the dummy time index thats required to save the files
 
 Cs_2 = data_2D_s['Cs_prof'].data[0, ...]
 Cs_4 = data_4D_s['Cs_prof'].data[0, ...]
 
-Cth_2 = data_2D_scalar['C_th_prof'].data[0, ...]
-Cth_4 = data_4D_scalar['C_th_prof'].data[0, ...]
+Cth_2 = data_2D_th['C_th_prof'].data[0, ...]
+Cth_4 = data_4D_th['C_th_prof'].data[0, ...]
 
 Cq_2 = data_2D_qtot['C_q_total_prof'].data[0, ...]
 Cq_4 = data_4D_qtot['C_q_total_prof'].data[0, ...]
 
-# Cql_2 = data_2D_scalar['C_ql_prof'].data[0, ...]
-# Cql_4 = data_4D_scalar['C_ql_prof'].data[0, ...]
+# Cql_2 = data_2D_th['C_ql_prof'].data[0, ...]
+# Cql_4 = data_4D_th['C_ql_prof'].data[0, ...]
 #
-# Cqv_2 = data_2D_scalar['C_qv_prof'].data[0, ...]
-# Cqv_4 = data_4D_scalar['C_qv_prof'].data[0, ...]
+# Cqv_2 = data_2D_th['C_qv_prof'].data[0, ...]
+# Cqv_4 = data_4D_th['C_qv_prof'].data[0, ...]
 
 
 Cs_sq_2 = data_2D_s['Cs_sq_prof'].data[0, ...]
 Cs_sq_4 = data_4D_s['Cs_sq_prof'].data[0, ...]
 
-Cth_sq_2 = data_2D_scalar['C_th_sq_prof'].data[0, ...]
-Cth_sq_4 = data_4D_scalar['C_th_sq_prof'].data[0, ...]
+Cth_sq_2 = data_2D_th['C_th_sq_prof'].data[0, ...]
+Cth_sq_4 = data_4D_th['C_th_sq_prof'].data[0, ...]
 
 Cq_sq_2 = data_2D_qtot['C_q_total_sq_prof'].data[0, ...]
 Cq_sq_4 = data_4D_qtot['C_q_total_sq_prof'].data[0, ...]
@@ -89,7 +90,7 @@ plt.legend(fontsize=16, loc='upper right')
 plt.xlabel('$\\beta$', fontsize=14)
 plt.ylabel("z (m)")
 plt.xlim(0, 1)
-plt.savefig(plotdir+'beta_profs.png', pad_inches=0)
+plt.savefig(plotdir+'all_beta_profs.png', pad_inches=0)
 
 
 
@@ -122,7 +123,7 @@ plt.legend(fontsize=16, loc='upper right')
 plt.xlabel('$\\beta$', fontsize=14)
 plt.ylabel("z/z$_{ML}$")
 plt.xlim(0, 1)
-plt.savefig(plotdir+'beta_profs_scaled.png', pad_inches=0)
+plt.savefig(plotdir+'all_beta_profs_scaled.png', pad_inches=0)
 
 
 ###########################################################################################################
@@ -202,7 +203,6 @@ plt.savefig(plotdir+'Cth_prof_scaled.png', pad_inches=0)
 
 #########################################################################################################################
 
-
 monc_l_20 = dyn.l_mix_MONC(0.23, 20, z, k=0.4)
 monc_l_40 = dyn.l_mix_MONC(0.23, 40, z, k=0.4)
 monc_l_80 = dyn.l_mix_MONC(0.23, 80, z, k=0.4)
@@ -233,9 +233,88 @@ plt.legend(fontsize=16, loc='upper right')
 #plt.xlim(1, 3)
 plt.savefig(plotdir+'l_mix_w_MONC_scaled.png', pad_inches=0)
 
-print('plotted l')
+print('plotted l th')
 
 #########################################################################################################################
+
+#scalars
+
+#########################################################################################################################
+
+C_scalar = np.sqrt((0.23*0.23)/0.7)
+
+monc_l_scalar_20 = dyn.l_mix_MONC(0.23, 20, z, k=0.4)
+monc_l_scalar_40 = dyn.l_mix_MONC(0.23, 40, z, k=0.4)
+monc_l_scalar_80 = dyn.l_mix_MONC(0.23, 80, z, k=0.4)
+
+plt.figure(figsize=(6,7))
+plt.plot(Cth_beta*(20), z, '.', markersize = 10, label = '$\\Delta = 20$m')
+plt.plot(Cth_2*(40), z, '.', markersize = 10, label = '$\\Delta = 40$m')
+plt.plot(Cth_4*(80), z, '.', markersize = 10, label = '$\\Delta = 80$m')
+plt.plot(monc_l_scalar_20, z, 'tab:blue')
+plt.plot(monc_l_scalar_40, z, 'tab:orange')
+plt.plot(monc_l_scalar_80, z, 'tab:green')
+plt.xlabel('$l_{\\theta}$', fontsize=16)
+plt.ylabel("z (m)", fontsize=16)
+plt.legend(fontsize=16, loc='upper right')
+#plt.xlim(1, 3)
+plt.savefig(plotdir+'l_th_w_MONC.png', pad_inches=0)
+
+plt.figure(figsize=(6,7))
+plt.plot(Cth_beta*(20), z/z_i, '.', markersize = 10, label = '$\\Delta = 20$m')
+plt.plot(Cth_2*(40), z/z_i, '.', markersize = 10, label = '$\\Delta = 40$m')
+plt.plot(Cth_4*(80), z/z_i, '.', markersize = 10, label = '$\\Delta = 80$m')
+plt.plot(monc_l_scalar_20, z/z_i, 'tab:blue')
+plt.plot(monc_l_scalar_40, z/z_i, 'tab:orange')
+plt.plot(monc_l_scalar_80, z/z_i, 'tab:green')
+plt.xlabel('$l_{\\theta}$', fontsize=16)
+plt.ylabel("z/z$_{ML}$", fontsize=16)
+plt.legend(fontsize=16, loc='upper right')
+#plt.xlim(1, 3)
+plt.savefig(plotdir+'l_th_w_MONC_scaled.png', pad_inches=0)
+
+print('plotted l_th')
+
+#########################################################################################################################
+
+
+
+#########################################################################################################################
+
+
+plt.figure(figsize=(6,7))
+plt.plot(Cq_beta*(20), z, '.', markersize = 10, label = '$\\Delta = 20$m')
+plt.plot(Cq_2*(40), z, '.', markersize = 10, label = '$\\Delta = 40$m')
+plt.plot(Cq_4*(80), z, '.', markersize = 10, label = '$\\Delta = 80$m')
+plt.plot(monc_l_scalar_20, z, 'tab:blue')
+plt.plot(monc_l_scalar_40, z, 'tab:orange')
+plt.plot(monc_l_scalar_80, z, 'tab:green')
+plt.xlabel('$l_{qt}$', fontsize=16)
+plt.ylabel("z (m)", fontsize=16)
+plt.legend(fontsize=16, loc='upper right')
+#plt.xlim(1, 3)
+plt.savefig(plotdir+'l_qt_w_MONC.png', pad_inches=0)
+
+plt.figure(figsize=(6,7))
+plt.plot(Cq_beta*(20), z/z_i, '.', markersize = 10, label = '$\\Delta = 20$m')
+plt.plot(Cq_2*(40), z/z_i, '.', markersize = 10, label = '$\\Delta = 40$m')
+plt.plot(Cq_4*(80), z/z_i, '.', markersize = 10, label = '$\\Delta = 80$m')
+plt.plot(monc_l_scalar_20, z/z_i, 'tab:blue')
+plt.plot(monc_l_scalar_40, z/z_i, 'tab:orange')
+plt.plot(monc_l_scalar_80, z/z_i, 'tab:green')
+plt.xlabel('$l_{qt}$', fontsize=16)
+plt.ylabel("z/z$_{ML}$", fontsize=16)
+plt.legend(fontsize=16, loc='upper right')
+#plt.xlim(1, 3)
+plt.savefig(plotdir+'l_qt_w_MONC_scaled.png', pad_inches=0)
+
+print('plotted l_qt')
+
+#########################################################################################################################
+
+
+
+
 
 
 Pr_th_beta = dyn.Pr(Cs_beta_sq, Cth_beta_sq)
