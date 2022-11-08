e4a8010a85f557e05eab91e089d9513954a22335
a-power <a.power@pgr.reading.ac.uk>
2022-11-08 18:57:36 +0000

    typo fix

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ file20 = "BOMEX_m0020_g0800_all_14400_gaussian_filter_ga00.nc"
 indir = path20f+file20
 
 file_in = f'{indir}'
-    ds_in = xr.open_dataset(file_in)
-    time_data = ds_in['time']
-    times = time_data.data
-    print('time array is', times)
+ds_in = xr.open_dataset(file_in)
+time_data = ds_in['time']
+times = time_data.data
+print('time array is', times)
