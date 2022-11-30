10fb817238ab4243d32f184d1f8903a9c0b12b0c
a-power <a.power@pgr.reading.ac.uk>
2022-11-30 20:43:35 +0000

    typo fix

--- plot_cloud_focus_contour.py
+++ plot_cloud_focus_contour.py
@@ -226,7 +226,7 @@ def plotfield(field, x_or_y, axis_set, data_field_list, set_percentile, data_cl_
 
         plt.ylabel("z")
         og_ytic = plt.yticks()
-        plt.yticks(og_ytic[0], np.linspace((z_range[0])*(20/1000), (z_range[1])*(20/1000), len(og_ytic[0])))
+        plt.yticks(og_ytic[0], np.round(np.linspace((z_range[0])*(20/1000), (z_range[1])*(20/1000), len(og_ytic[0]))), 2)
         plt.savefig(plotdir+f'{field}_{deltas[i]}_{x_or_y}={axis_set}.png', pad_inches=0)
         plt.clf()
 
@@ -267,7 +267,7 @@ def plotfield(field, x_or_y, axis_set, data_field_list, set_percentile, data_cl_
                 print("axis_set must be 'x' or'y'.")
 
             og_ytic = plt.yticks()
-            plt.yticks(og_ytic[0], np.linspace((z_range[0])*(20 / 1000), (z_range[1])*(20 / 1000), len(og_ytic[0])))
+            plt.yticks(og_ytic[0], np.round(np.linspace((z_range[0])*(20 / 1000), (z_range[1])*(20 / 1000), len(og_ytic[0]))), 2)
             plt.ylabel("z (km)")
             plt.savefig(plotdir + f'{field}_sq_{deltas[i]}_{x_or_y}={axis_set}.png', pad_inches=0)
             plt.clf()
