0c75bea2ead6b13d1f4e3e8cab6aa65f2c495b75
a-power <a.power@pgr.reading.ac.uk>
2022-11-30 20:05:49 +0000

    typo fix

--- plot_cloud_focus_contour.py
+++ plot_cloud_focus_contour.py
@@ -9,7 +9,7 @@ homedir = '/gws/nopw/j04/paracon_rdg/users/apower/LES_analysis/20m_gauss_dyn/'
 mydir = homedir + 'LijMij_HjRj/BOMEX_m0020_g0800_all_14400_gaussian_filter_'
 dir_cloud = homedir + 'q_l/BOMEX_m0020_g0800_all_14400_gaussian_filter_ga0'
 
-plotdir = '/gws/nopw/j04/paracon_rdg/users/apower/LES_analysis/20m_gauss_dyn/plots/fields_cloud_contour/new_percentiles/'
+plotdir = '/gws/nopw/j04/paracon_rdg/users/apower/LES_analysis/20m_gauss_dyn/plots/fields_cloud_contour/cloud_focus/'
 os.makedirs(plotdir, exist_ok = True)
 
 dir_s = mydir + 'LijMij_'
@@ -225,7 +225,7 @@ def plotfield(field, x_or_y, axis_set, data_field_list, set_percentile, data_cl_
         plt.xticks(og_xtic[0], np.linspace((x_range[0])*(20/1000), (x_range[1])*(20/1000), len(og_xtic[0])))
         og_ytic = plt.yticks()
         plt.yticks(og_ytic[0], np.linspace((y_range[0])*(20/1000), (y_range[1])*(20/1000), len(og_ytic[0])))
-        plt.savefig(plotdir+f'{field}_{deltas[i]}_field_{x_or_y}={axis_set}.png', pad_inches=0)
+        plt.savefig(plotdir+f'{field}_{deltas[i]}_{x_or_y}={axis_set}.png', pad_inches=0)
         plt.clf()
 
         if field == 'Cqt_field' or field == 'Cth_field' or field == 'Cs_field':
@@ -262,7 +262,7 @@ def plotfield(field, x_or_y, axis_set, data_field_list, set_percentile, data_cl_
             og_ytic = plt.yticks()
             plt.yticks(og_ytic[0], np.linspace((y_range[0])*(20 / 1000), (y_range[1])*(20 / 1000), len(og_ytic[0])))
             plt.ylabel("z (km)")
-            plt.savefig(plotdir + f'{field}_sq_{deltas[i]}_field_{x_or_y}={axis_set}.png', pad_inches=0)
+            plt.savefig(plotdir + f'{field}_sq_{deltas[i]}_{x_or_y}={axis_set}.png', pad_inches=0)
             plt.clf()
 
         print(f'plotted fields for {field}')
