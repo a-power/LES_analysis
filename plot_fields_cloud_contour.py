215575c6e34a30d8f3b590c79b115f6a7bf5c767
a-power <a.power@pgr.reading.ac.uk>
2022-11-30 13:24:18 +0000

    title and start min colorbar of C^2 and LM + equiv to be at 0, trying to fix axis

--- plot_fields_cloud_contour.py
+++ plot_fields_cloud_contour.py
@@ -217,7 +217,7 @@ def plotfield(field, x_or_y, axis_set, data_field_list, set_percentile, data_cl_
         og_xtic = plt.xticks()
         plt.xticks(og_xtic[0], np.linspace(0, 16, len(og_xtic[0])))
         og_ytic = plt.yticks()
-        plt.yticks(og_ytic[0], np.linspace(0, 3, len(og_ytic[0])))
+        plt.yticks(np.linspace(0, 151, 7) , np.linspace(0, 3, 7))
         plt.savefig(plotdir+f'{field}_{deltas[i]}_field_{x_or_y}={axis_set}.png', pad_inches=0)
         plt.clf()
 
@@ -257,7 +257,7 @@ def plotfield(field, x_or_y, axis_set, data_field_list, set_percentile, data_cl_
             og_xtic = plt.xticks()
             plt.xticks(og_xtic[0],np.linspace(0, 16, len(og_xtic[0])))
             og_ytic = plt.yticks()
-            plt.yticks(og_ytic[0],np.linspace(0, 3, len(og_ytic[0])))
+            plt.yticks(np.linspace(0, 151, 7) ,np.linspace(0, 3, 7))
             plt.ylabel("z (km)")
             plt.savefig(plotdir + f'{field}_sq_{deltas[i]}_field_{x_or_y}={axis_set}.png', pad_inches=0)
             plt.clf()
