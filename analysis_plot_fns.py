43113ba7bf889123139d8971152463c612146a74
a-power <a.power@pgr.reading.ac.uk>
2023-02-14 17:08:24 +0000

    changing histogram to step plot, not stacked so as to avoid log problem

--- analysis_plot_fns.py
+++ analysis_plot_fns.py
@@ -85,8 +85,8 @@ def C_values(plotdir, field, data_field_list, data_cl_list, **kwargs):
 
         plt.figure(figsize=(7, 6))
         plt.hist([data_field_env[...,0:24].flatten(), data_field_env[...,24:151].flatten(), data_field_cloud[...].flatten()], \
-                 bins=500, histtype='step', stacked=False, label=["ML", "CL: clear sky", "CL: cloudy"],\
-                 linestyle=['solid', 'dotted', 'dashed'], linewidth=[2, 2, 2])
+                 bins=500, histtype='step', stacked=False, label=["ML", "CL: clear sky", "CL: cloudy"], \
+                 linewidths = (2, 2, 2), linestyles=('solid', 'dotted', 'dashed'))
         plt.legend()
 
         # og_xtic = plt.xticks()
