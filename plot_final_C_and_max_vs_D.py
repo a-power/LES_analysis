2d6f6becc454a491752400578eba8c00b4eb044b
a-power <a.power@pgr.reading.ac.uk>
2023-03-22 18:10:22 +0000

    not finished, just a save

--- plot_final_C_and_max_vs_D.py
+++ plot_final_C_and_max_vs_D.py
@@ -246,7 +246,7 @@ def get_max_l_from_C(max_C_cond):
     Delta_res = np.array([2*20, 4*20, 8*20, 16*20, 32*20, 64*20])
     max_l_cond = np.zeros_like(max_C_cond)
     for it in range(np.shape(max_C_cond)[1]):
-        max_l_cond = max_C_cond[:,it] * Delta_res[it]
+        max_l_cond[:,it] = max_C_cond[:,it] * Delta_res[it]
     print('shape of max l is ', np.shape(max_l_cond))
     return max_l_cond
 
