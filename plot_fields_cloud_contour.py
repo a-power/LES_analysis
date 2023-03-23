6ee45a11e4460f1788dd8aa0b1f38751de1204d8
a-power <a.power@pgr.reading.ac.uk>
2023-03-23 14:18:47 +0000

    if a==b or a==c change

--- plot_fields_cloud_contour.py
+++ plot_fields_cloud_contour.py
@@ -19,7 +19,7 @@ in_set_percentile_C2 = ['min',99] #note that the first entry of this can be 'min
 
 x_axis_start_end = [10, 15] #start and end points in km
 
-time_av_or_not = np.array([0, 1, 2]) #, 1, 2 #'yes' #if not then give the time stamp index/indices (integer) you want to look at (eg 0, 1, ..)
+time_av_or_not = np.array([0])#, 1, 2]) #, 1, 2 #'yes' #if not then give the time stamp index/indices (integer) you want to look at (eg 0, 1, ..)
 
 my_axis = 570
 my_x_y = 'y' # must be y due to direction of wind
