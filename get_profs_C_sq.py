355a3dd68d47f7953081d857f4e44434b59e764c
a-power <a.power@pgr.reading.ac.uk>
2023-04-05 09:06:01 +0100

    just for ga00 for now

--- get_profs_C_sq.py
+++ get_profs_C_sq.py
@@ -29,7 +29,7 @@ deltas=['2D', '4D', '8D', '16D', '32D', '64D']
 if beta==True:
     dataset_name = [outdir+myfile+'C_2D_', outdir+myfile+'C_4D_', outdir+myfile+'C_8D_',
                 outdir+myfile+'C_16D_', outdir+myfile+'C_32D_', outdir+myfile+'C_64D_']
-    extra_filter = ['0', '1']
+    extra_filter = ['0']#, '1']
 else:
     dataset_name = [outdir+myfile+'C_2D.nc', outdir+myfile+'C_4D.nc', outdir+myfile+'C_8D.nc',
                     outdir+myfile+'C_16D.nc', outdir+myfile+'C_32D.nc', outdir+myfile+'C_64D.nc']
