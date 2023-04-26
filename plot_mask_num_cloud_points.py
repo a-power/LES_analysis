b3d444abf2262a53ca7ad239148e1b9707fc264b
alanna <a.power@pgr.reading.ac.uk>
2023-04-26 12:55:35 +0100

    scatter plot of cloud liquid water content

--- plot_mask_num_cloud_points.py
+++ plot_mask_num_cloud_points.py
@@ -14,10 +14,10 @@ beta=True
 
 if beta==True:
     homedir = '/work/scratch-pw3/apower/20m_gauss_dyn/on_p_grid/beta_filtered_filters/smoothed_LM_HR_fields/'
-    dir_contour = homedir + 'BOMEX_m0020_g0800_all_14400_gaussian_filter_ga0'
+    dir_contour = homedir + 'BOMEX_m0020_g0800_all_14400_gaussian_filter_'
 else:
     homedir = '/work/scratch-pw3/apower/20m_gauss_dyn/on_p_grid/beta_filtered_filters/smoothed_LM_HR_fields/'
-    dir_contour = homedir + 'BOMEX_m0020_g0800_all_14400_gaussian_filter_ga0'
+    dir_contour = homedir + 'BOMEX_m0020_g0800_all_14400_gaussian_filter_'
 
 
 plotdir = '/gws/nopw/j04/paracon_rdg/users/apower/on_p_grid/plots/cloud_count/'
