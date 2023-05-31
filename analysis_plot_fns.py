b6bf508632c70efdf60e4284e6ff5f6e6ea999b0
alanna <a.power@pgr.reading.ac.uk>
2023-05-31 10:19:03 +0100

    fixes

--- analysis_plot_fns.py
+++ analysis_plot_fns.py
@@ -139,7 +139,7 @@ def plot_C_Delta_hist_comp(dir_in, field, condits = None, deltas=None):
         plt.vlines(0, ymin=0, ymax=((1e9)), linestyles='dashed', colors='black', linewidths=0.5)
         plt.yscale('log', nonposy='clip')
         plt.ylim(bottom_set, (top_set + (top_set / 10)))
-        plt.xlabel(f"{scalar} at time {time_set_in}", fontsize=16)
+        plt.xlabel(f"{scalar}", fontsize=16)
         plt.ylabel("number of value occurrences", fontsize=16)
         plt.savefig(data_dir + f'delta_hist_of_{field}_{condits[j]}_values.png', bbox_inches='tight')
         plt.clf()
