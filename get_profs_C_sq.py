84c07ff1583343d99861c88d8e8f6fc5f71c320c
a-power <a.power@pgr.reading.ac.uk>
2023-07-26 02:18:11 +0100

    altered directory

--- get_profs_C_sq.py
+++ get_profs_C_sq.py
@@ -8,10 +8,13 @@ import xarray as xr
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--times', type=str, default='18000')
+#parser.add_argument('--times', type=str, default='18000')
+parser.add_argument('--times', type=int, default=0)
+
+times_analysed = [ '18000', '25200', '32400', '39600' ]
 
 args = parser.parse_args()
-set_time = args.times
+set_time = times_analysed[args.times]
 
 beta=True
 case = 'ARM'
@@ -28,7 +31,7 @@ if case == 'BOMEX':
     myfile = 'BOMEX_m0020_g0800_all_14400_gaussian_filter_'
 
 elif case == 'ARM':
-    homedir = '/work/scratch-pw3/apower/ARM/on_p_grid/filtering_filtered/'
+    homedir = '/work/scratch-pw3/apower/ARM/corrected_sigmas/filtering_filtered/smoothed_LM_HR_fields/'
     dir_contour = homedir + f'diagnostics_3d_ts_{set_time}_gaussian_filter_ga0'
     myfile = f"diagnostics_3d_ts_{set_time}_gaussian_filter_"
 
@@ -64,7 +67,8 @@ else:
 # field_dir = ['Cs', 'C_th', 'C_qt']
 
 if beta==True:
-    fields = ['LM_field', 'HR_th_field', 'HR_q_total_f_field', 'MM_field', 'RR_th_field', 'RR_q_total_f_field']
+    fields = [f'f(LM_field_on_{mygrid})_r', f'f(HR_th_field_on_{mygrid})_r', f'f(HR_q_total_f_field_on_{mygrid})_r',
+              f'f(MM_field_on_{mygrid})_r', f'f(RR_th_field_on_{mygrid})_r', f'f(RR_q_total_f_field_on_{mygrid})_r']
 else:
     fields = ['LM_field', 'HR_th_field', 'HR_q_total_field', 'MM_field', 'RR_th_field', 'RR_q_total_field']
 field_dir = ['Cs', 'C_th', 'C_qt', 'Cs', 'C_th', 'C_qt']
@@ -75,10 +79,10 @@ field_dir = ['Cs', 'C_th', 'C_qt', 'Cs', 'C_th', 'C_qt']
 # w2_field = f'f(f(w_on_{mygrid}.w_on_{mygrid})_r_on_{mygrid})_r'
 # th_v_field = f'f(f(th_v_on_{mygrid})_r_on_{mygrid})_r'
 
-cloud_field = f'f(q_cloud_liquid_mass_on_{mygrid})_r'
-w_field = f'f(w_on_{mygrid})_r'
-w2_field = f'f(w_on_{mygrid}.w_on_{mygrid})_r'
-th_v_field = f'f(th_v_on_{mygrid})_r'
+cloud_field = f'f(f(q_cloud_liquid_mass_on_{mygrid})_r_on_{mygrid})_r'
+w_field = f'f(f(w_on_{mygrid})_r_on_{mygrid})_r'
+w2_field = f'f(f(w_on_{mygrid}.w_on_{mygrid})_r_on_{mygrid})_r'
+th_v_field = f'f(f(th_v_on_{mygrid})_r_on_{mygrid})_r'
 
 gen_opts = {'deltas': None,
             'other_vars': [w_field, th_v_field],
@@ -103,8 +107,8 @@ for j, delta_in in enumerate(deltas):
             for i, field_in in enumerate(fields):
 
                 mydataset = homedir + myfile + \
-                            str(f'{field_dir[i]}_{j}_{name_2_gauss}.nc') #_running_mean_filter_rm00.nc')
-                mydir_contour = dir_contour + f'{j}_gaussian_filter_ga0{name_2_gauss}.nc' #_running_mean_filter_rm00.nc'
+                            str(f'{field_dir[i]}_{j}_{name_2_gauss}_running_mean_filter_rm00.nc')
+                mydir_contour = dir_contour + f'{j}_gaussian_filter_ga0{name_2_gauss}_running_mean_filter_rm00.nc'
 
                 C_sq_prof, C_sq_env_prof, C_sq_cloud_prof, C_sq_combo2_prof, C_sq_combo3_prof = \
                     apf.get_conditional_profiles(field=field_in, **gen_opts, dataset_in = mydataset,
