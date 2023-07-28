ba5a64fc9c15833a6235811076f6d77e05629d65
a-power <a.power@pgr.reading.ac.uk>
2023-07-28 12:19:16 +0100

    save name

--- tests.py
+++ tests.py
@@ -1,7 +1,18 @@
-import multi_times_plot_final_C_and_max_vs_D as zc
+def calc_z_ML_and_CL(file_path, time_stamp=-1):
+
+    prof_data = xr.open_dataset(file_path)
+
+    wth_prof = prof_data['wtheta_cn_mean'].data[time_stamp, ...]
+    z_ML = np.where(wth_prof = np.min(wth_prof))
+
+    cloud = prof_data['total_cloud_fraction'].data[time_stamp, ...]
+    z_cloud = np.where(wth_prof != 0)
+    z_CL = [ np.min(z_cloud), np.max(z_cloud) ]
+
+    return z_ML, z_CL
 
 file_path = '/work/scratch-pw3/apower/ARM/MONC_out/diagnostics_ts_18000.nc'
 
-z_ML, z_CL = zc.calc_z_ML_and_CL(file_path, time_stamp=-1)
+z_ML, z_CL = calc_z_ML_and_CL(file_path, time_stamp=-1)
 
 print('z_ML = ', z_ML, 'z_CL = ', z_CL)
