6ec0034206f6c0166a09acee1f596ceb79c842d4
a-power <a.power@pgr.reading.ac.uk>
2023-07-28 13:29:43 +0100

    test

--- tests.py
+++ tests.py
@@ -6,14 +6,15 @@ def calc_z_ML_and_CL(file_path, time_stamp=-1):
     prof_data = xr.open_dataset(file_path)
 
     wth_prof = prof_data['wtheta_cn_mean'].data[time_stamp, ...]
-    print(wth_prof)
     wth_prof_list = wth_prof.tolist()
     z_ML = wth_prof_list.index(np.min(wth_prof))
 
     z_cloud = prof_data['total_cloud_fraction'].data[time_stamp, ...]
     z_cloud_list = z_cloud.tolist()
-    z_min_CL = np.min(z_cloud_list.index(z_cloud > 1e-7))
-    z_max_CL = np.max(z_cloud_list.index(z_cloud > 1e-7))
+    z_cloud_ind = z_cloud_list.index(z_cloud > 1e-7)
+    print(z_cloud_ind)
+    z_min_CL = np.min(z_cloud_ind)
+    z_max_CL = np.max(z_cloud_ind)
     z_CL = [ z_min_CL, z_max_CL ]
 
     return z_ML, z_CL
