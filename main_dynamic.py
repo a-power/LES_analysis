33ead62902034f1577c4515bc847eb5a24de7cf1
alanna <a.power@pgr.reading.ac.uk>
2023-08-28 16:45:14 +0100

    altering to 4 spaces per tab rather than 8????????

--- main_dynamic.py
+++ main_dynamic.py
@@ -4,16 +4,25 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--times', type=str, default='14400')
+parser.add_argument('--times', type=str, default='19200')
+parser.add_argument('--case', type=str, default='ARM')
 parser.add_argument('--start_in', type=int, default=0)
 
 args = parser.parse_args()
 set_time = [ args.times ]
+case = args.case
 start = args.start_in
+
 print('start type = ', type(start))
 
-case='ARM'
-MY_dx = 25
+if case=='ARM':
+    MY_dx = 25
+
+elif case=='BOMEX':
+    MY_dx = 20
+
+else:
+    print('case not recognised, need to input dx for this case')
 
 opgrid = 'p'
 
@@ -30,34 +39,35 @@ filter_name = 'gaussian'  # "wave_cutoff"
 
 
 if start == 0:
-        sigma_list = np.array([MY_dx, 2*MY_dx, 4*MY_dx, 8*MY_dx, 16*MY_dx, 32*MY_dx])
+    sigma_list = np.array([MY_dx, 2*MY_dx, 4*MY_dx, 8*MY_dx, 16*MY_dx, 32*MY_dx])
 elif start == 1:
-        sigma_list = np.array([2*MY_dx, 4*MY_dx, 8*MY_dx, 16*MY_dx, 32*MY_dx])
+    sigma_list = np.array([2*MY_dx, 4*MY_dx, 8*MY_dx, 16*MY_dx, 32*MY_dx])
 elif start == 2:
-        sigma_list = np.array([4*MY_dx, 8*MY_dx, 16*MY_dx, 32*MY_dx])
+    sigma_list = np.array([4*MY_dx, 8*MY_dx, 16*MY_dx, 32*MY_dx])
 elif start == 3:
-        sigma_list = np.array([8*MY_dx, 16*MY_dx, 32*MY_dx])
+    sigma_list = np.array([8*MY_dx, 16*MY_dx, 32*MY_dx])
 elif start == 4:
-        sigma_list = np.array([16*MY_dx, 32*MY_dx])
+    sigma_list = np.array([16*MY_dx, 32*MY_dx])
 elif start == 5:
-        sigma_list = np.array([32*MY_dx])
+    sigma_list = np.array([32*MY_dx])
 else:
-        print('need to set up the sigma list for start = ', start)
+    print('need to set up the sigma list for start = ', start)
 # #([20, 40, 80] ([160, 320, 640])
 
 
 
 if case=='BOMEX':
-        in_dir = '/gws/nopw/j04/paracon_rdg/users/toddj/updates_suite/BOMEX_m'
-        model_res_list = ['0020_g0800']
-        outdir_og = '/work/scratch-pw3/apower/'
-        outdir = outdir_og + f'20m_gauss_dyn/on_{opgrid}_grid/'
-        plotdir = outdir_og + 'plots/dyn/'
+    in_dir = '/gws/nopw/j04/paracon_rdg/users/toddj/updates_suite/BOMEX_m'
+    model_res_list = ['0020_g0800']
+    outdir_og = '/work/scratch-pw3/apower/'
+    outdir = outdir_og + f'20m_gauss_dyn/on_{opgrid}_grid/'
+    plotdir = outdir_og + 'plots/dyn/'
+
 elif case=='ARM':
-        in_dir = '/work/scratch-pw3/apower/ARM/'
-        outdir = in_dir + f'corrected_sigmas/'
-        plotdir = outdir + 'plots/dyn/'
-        model_res_list = [None]
+    in_dir = '/work/scratch-pw3/apower/ARM/'
+    outdir = in_dir + f'corrected_sigmas/'
+    plotdir = outdir + 'plots/dyn/'
+    model_res_list = [None]
 
 
 os.makedirs(outdir, exist_ok = True)
@@ -85,6 +95,6 @@ options_BOMEX = {
           }
 
 for j in range(len(set_time)):
-        for i, model_res in enumerate(model_res_list):
-                dy_s.run_dyn(model_res, set_time[j], filter_name, sigma_list, in_dir, outdir, options_ARM, \
+    for i, model_res in enumerate(model_res_list):
+        dy_s.run_dyn(model_res, set_time[j], filter_name, sigma_list, in_dir, outdir, options_ARM, \
                             opgrid, start_point=start, ref_file = None)
