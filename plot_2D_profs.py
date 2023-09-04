7b8ec3039b695984fd63d3e88dfc50cc49976c48
alanna <a.power@pgr.reading.ac.uk>
2023-09-04 13:06:01 +0100

    adjusting BOMEX plot scales

--- plot_2D_profs.py
+++ plot_2D_profs.py
@@ -90,7 +90,7 @@ def plot_MONC_profs(file_path, times, time_stamp_in='mean'):
 
 
     if len(times) == 1:
-        fig, ax = plt.subplots(nrows=1, ncols=3, sharey=True, figsize=(9, 6))
+        fig, ax = plt.subplots(nrows=1, ncols=3, sharey=True, figsize=(10, 5))
     else:
         fig, ax = plt.subplots(nrows=3, ncols=len(times), sharey=False, figsize=(18,12))
 
