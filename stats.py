c1eaffe4caca80fd817d594e6978ea0f4ce431ad
alanna <a.power@pgr.reading.ac.uk>
2023-09-05 16:40:23 +0100

    test to see if it works

--- stats.py
+++ stats.py
@@ -9,43 +9,137 @@ from matplotlib.colors import TwoSlopeNorm
 import xarray as xr
 import os
 import csv
+parser.add_argument('--case_in', type=str, default='ARM')
+parser.add_argument('--time_it', type=str, default='0')
 
+args = parser.parse_args()
+case = args.case_in
 
+mygrid = 'p'
 set_CL_r = []
-Deltas = []
+Deltas = ['0', '1', '2', '3', '4', '5']
+beta_filt_num = ['0']
+
+if case == 'BOMEX':
+    data_path = '/storage/silver/MONC_data/Alanna/BOMEX/beta_filtered_data/smoothed_LM_HR_fields/'
+    times_analysed = [ '18000', '25200', '32400', '39600' ]
+    set_time = times_analysed[args.time_it]
+    file_name = f'BOMEX_m0020_g0800_all_{set_time}_gaussian_filter_'
+
+    zn_set = np.arange(0, 3020, 20)
+
+    # todd_dir = '/gws/nopw/j04/paracon_rdg/users/toddj/updates_suite/BOMEX_m0020_g0800/diagnostic_files/'
+    # prof_file = todd_dir + 'BOMEX_m0020_g0800_all_14400.nc'
+
+    z_cl_r_ind_set = [33, 77] # set
+    z_cl_r_ind_calc = [22, 109]  # calc
+
+    z_ML_r_ind = [10, 22] # 22 as calc but the profs
 
 
-def get_masks(contour_field_in, cloud_thres, other_vars, other_var_thres,
-                             less_greater_in, and_or_in, grid):
+if case == 'ARM':
+    data_path = '/work/scratch-pw3/apower/ARM/corrected_sigmas/filtering_filtered/smoothed_LM_HR_fields/'
+    times_analysed = [ '14400' ]
+    set_time = times_analysed[args.time_it]
+    file_name = f'diagnostics_3d_ts_{set_time}_gaussian_filter_'
+
+    zn_set = np.arange(0, 4410, 10)
+    z_ML_bottom = 10
+
+    profiles_dir = f'/work/scratch-pw3/apower/ARM/MONC_out/diagnostics_ts_{set_time}.nc'
+
+
+
+w_field = f'f(f(w_on_{mygrid})_r_on_{mygrid})_r'
+w2_field = f'f(f(w_on_{mygrid}.w_on_{mygrid})_r_on_{mygrid})_r'
+th_v_field = f'f(f(th_v_on_{mygrid})_r_on_{mygrid})_r'
+
+
+mask_options = {'cloud_thres': 1e-7,
+                   'other_vars': [w_field, th_v_field],
+                    'other_var_thres': [0.5, 0],
+                    'less_greater_in': ['less', 'less'],
+                    'and_or_in': ['and', 'and']
+                    }
+
+
+
+def calc_z_ML_and_CL(file_path, time_stamp=-1):
+
+    prof_data = xr.open_dataset(file_path)
+
+    wth_prof = prof_data['wtheta_cn_mean'].data[time_stamp, ...]
+    wth_prof_list = wth_prof.tolist()
+    z_ML = wth_prof_list.index(np.min(wth_prof))
+
+    z_cloud = prof_data['total_cloud_fraction'].data[time_stamp, ...]
+    z_cloud_where = np.where(z_cloud > 1e-7)
+    z_ind = np.arange(0, len(z_cloud))
+    z_cloud_ind = z_ind[z_cloud_where]
+    #print('z_cloud_ind =', z_cloud_ind)
+    z_min_CL = np.min(z_cloud_ind)
+    z_max_CL = np.max(z_cloud_ind)
+    z_CL = [ z_min_CL, z_max_CL ]
+
+    zn_out = prof_data['zn'].data[...] # timeless parameter?
+
+    return z_ML, z_CL, zn_out
+
+
+
+
+if case == 'ARM':
+    z_ML_top_index, z_cl_r_ind_calc, zn_arr = calc_z_ML_and_CL(profiles_dir)
+    z_ML_r_ind = [z_ML_bottom, z_ML_top_index]
+elif case== 'BOMEX':
+    #z_cl_r_ind_calc set
+    z_ML_top_index = z_ML_r_ind[1]
+else:
+    print('case ', case, ' is undefined')
+
+z_ML = zn_set[z_ML_top_index]
+
+z_cl_r_set_m = [ zn_set[z_cl_r_ind_set[0]],  zn_set[z_cl_r_ind_set[1]] ]
+z_cl_range_calc_m = [ zn_set[z_cl_r_ind_calc[0]], zn_set[z_cl_r_ind_calc[1]] ]
+
+
+
+
+def get_masks(dataset_path, file_name_in, Delta_in, beta_filt,
+              cloud_thres, other_vars, other_var_thres,
+                             less_greater_in, and_or_in, grid=mygrid):
+
+    contour_field_in = dataset_path + file_name_in + \
+        f'ga0{Delta_in}_gaussian_filter_ga0{beta_filt}_running_mean_filter_rm00.nc'
 
     cloud_only_mask, env_only_mask = \
         clo.cloud_vs_env_masks(contour_field_in, cloud_liquid_threshold=cloud_thres)
 
 
-    cloud_up_mask, cloud_core_mask = clo.cloudy_and_or(contour_field_in,
-                                                                 other_var=other_vars, var_thres=other_var_thres,
+    cloud_up_mask, cloud_core_mask = clo.cloudy_and_or(contour_field_in, other_var=other_vars, var_thres=other_var_thres,
                                                                  less_greater=less_greater_in, and_or=and_or_in,
                                                                  cloud_liquid_threshold=cloud_thres, grid=grid)
 
     return cloud_only_mask, env_only_mask, cloud_up_mask, cloud_core_mask
 
 
-def get_stats_for_C(dataset_in, param, ML_int, CL_int, CL_depth_int, t_in, grid,
-              cloud_only_mask_in, env_only_mask_in, cloud_up_mask_in, cloud_core_mask_in):
 
-    csv_file_path = ''
-    Delta_in = ''
+def get_stats_for_C(dataset_path, file_name_in, Delta_in, beta_filt, param, ML_r_int, CL_depth_int_calc, CL_depth_int_set,
+              cloud_only_mask_in, env_only_mask_in, cloud_up_mask_in, cloud_core_mask_in, grid=mygrid):
+
+    csv_file_path = dataset_path + 'stats/'
+    dataset_in = dataset_path + file_name_in + param + '_' + Delta_in + '_' + beta_filt + '_running_mean_filter_rm00.nc'
 
 
     if param == 'Cs':
         field_name = '$C_s$'
         field_name_sq = '$C_s^2$'
 
-    elif param == 'Cth':
+    elif param == 'C_th':
         field_name = '$C_{\\theta}$'
         field_name_sq = '$C_{\\theta}^2$'
 
-    elif param == 'Cqt':
+    elif param == 'C_qt':
         field_name = '$C_{qt}$'
         field_name_sq = '$C_{qt}^2$'
 
@@ -76,7 +170,7 @@ def get_stats_for_C(dataset_in, param, ML_int, CL_int, CL_depth_int, t_in, grid,
         else:
             print('LM_field_on_{grid} not in file')
 
-    elif param == 'Cth':
+    elif param == 'C_th':
 
         if f'f(HR_th_field_on_{grid})_r' in data_set:
             num_field = data_set[f'f(HR_th_field_on_{grid})_r'].data[...]
@@ -88,7 +182,7 @@ def get_stats_for_C(dataset_in, param, ML_int, CL_int, CL_depth_int, t_in, grid,
             print('HR_field_on_{grid} not in file')
 
 
-    elif param == 'Cqt':
+    elif param == 'C_qt':
 
 
         if f'f(HR_q_total_f_field_on_{grid})_r' in data_set:
@@ -119,50 +213,55 @@ def get_stats_for_C(dataset_in, param, ML_int, CL_int, CL_depth_int, t_in, grid,
 
 
 
-    dom_num = num_field[t_in, ...]
-    dom_den = den_field[t_in, ...]
+    dom_num = num_field[...]
+    dom_den = den_field[...]
     C_sq_dom = 0.5*(dom_num / dom_den)
     C_dom = dyn.get_Cs( C_sq_dom )
 
-    ML_num = num_field_env[t_in, :, :, ML_int[0] :: ML_int[1] ]
-    ML_den = den_field_env[t_in, :, :, ML_int[0] :: ML_int[1] ]
+    ML_num = num_field_env[:, :, :, ML_r_int[0] :: ML_r_int[1] ]
+    ML_den = den_field_env[:, :, :, ML_r_int[0] :: ML_r_int[1] ]
     C_sq_ML = 0.5*(ML_num / ML_den)
     C_ML = dyn.get_Cs( C_sq_ML )
 
-    CL_num = num_field[t_in, :, :, CL_depth_int[0] :: CL_depth_int[1] ]
-    CL_den = den_field[t_in, :, :, CL_depth_int[0] :: CL_depth_int[1] ]
-    C_sq_CL = 0.5*(CL_num / CL_den)
-    C_CL = dyn.get_Cs( C_sq_CL )
+    CL_num_calc = num_field[:, :, :, CL_depth_int_calc[0] :: CL_depth_int_calc[1] ]
+    CL_den_calc = den_field[:, :, :, CL_depth_int_calc[0] :: CL_depth_int_calc[1] ]
+    C_sq_CL_calc = 0.5*(CL_num_calc / CL_den_calc)
+    C_CL_calc = dyn.get_Cs( C_sq_CL_calc )
 
-    IC_num = num_field_IC[t_in, :, :, CL_int[0] :: CL_int[1] ]
-    IC_den = den_field_IC[t_in, :, :, CL_int[0] :: CL_int[1] ]
+    CL_num_set = num_field[:, :, :, CL_depth_int_set[0] :: CL_depth_int_set[1] ]
+    CL_den_set = den_field[:, :, :, CL_depth_int_set[0] :: CL_depth_int_set[1] ]
+    C_sq_CL_set = 0.5*(CL_num_set / CL_den_set)
+    C_CL_set = dyn.get_Cs( C_sq_CL_set )
+
+    IC_num = num_field_IC[:, :, :, CL_depth_int_calc[0] :: CL_depth_int_calc[1] ]
+    IC_den = den_field_IC[:, :, :, CL_depth_int_calc[0] :: CL_depth_int_calc[1] ]
     C_sq_IC = 0.5*(IC_num / IC_den)
     C_IC = dyn.get_Cs( C_sq_IC )
 
-    CU_num = num_field_CU[t_in, :, :, CL_int[0] :: CL_int[1] ]
-    CU_den = den_field_CU[t_in, :, :, CL_int[0] :: CL_int[1] ]
+    CU_num = num_field_CU[:, :, :, CL_depth_int_calc[0] :: CL_depth_int_calc[1] ]
+    CU_den = den_field_CU[:, :, :, CL_depth_int_calc[0] :: CL_depth_int_calc[1] ]
     C_sq_CU = 0.5*(CU_num / CU_den)
     C_CU = dyn.get_Cs( C_sq_CU )
 
-    CC_num = num_field_CC[t_in, :, :, CL_int[0] :: CL_int[1] ]
-    CC_den = den_field_CC[t_in, :, :, CL_int[0] :: CL_int[1] ]
+    CC_num = num_field_CC[:, :, :, CL_depth_int_calc[0] :: CL_depth_int_calc[1] ]
+    CC_den = den_field_CC[:, :, :, CL_depth_int_calc[0] :: CL_depth_int_calc[1] ]
     C_sq_CC = 0.5*(CC_num / CC_den)
     C_CC = dyn.get_Cs( C_sq_CC )
     
 
 
-    C_partiton = [C_dom, C_ML, C_CL, C_IC, C_CU, C_CC,
-                  C_sq_dom, C_sq_ML, C_sq_CL, C_sq_IC, C_sq_CU, C_sq_CC]
+    C_partiton = [C_dom, C_ML, C_CL_calc, C_CL_set, C_IC, C_CU, C_CC,
+                  C_sq_dom, C_sq_ML, C_sq_CL_calc, C_sq_CL_set, C_sq_IC, C_sq_CU, C_sq_CC]
 
-    partition_name = ['domain', 'ML', 'CL', 'IC', 'CU', 'CC']
+    partition_name = ['domain', 'ML', 'CL_calc', 'CL_set', 'IC', 'CU', 'CC']
     header = ['Smagorinsky_Parameter', 'Partition_Condition', 'Layer_Range', 'Mean', 'Standard_Deviation',
               'Median', 'Lower_Quartile', 'Upper_Quartile', 'Minimum', 'Maximum', 'Number_of_Points']
 
-    CL_range_name = str(CL_int[0]) + '_' + str(CL_int[1])
+    CL_range_name = str(CL_depth_int_set[0]) + '_' + str(CL_depth_int_set[1])
 
 
 
-    file_csv = open(csv_file_path + f'{smag}_{times[-1]}_{t_in}_{Delta_in}_CL_{CL_range_name}', 'w')
+    file_csv = open(csv_file_path + f'{smag}_{times[-1]}_delta_{Delta_in}_CL_{CL_range_name}', 'w')
     C_stats = csv.writer(file_csv)
 
     for it, C_part_in in enumerate(C_partiton):
@@ -173,14 +272,16 @@ def get_stats_for_C(dataset_in, param, ML_int, CL_int, CL_depth_int, t_in, grid,
             param_name = param+'_sq'
 
 
-        if C_part_in == 'CL':
-            range_str = str(CL_depth_int[0]) + '_' + str(CL_depth_int[1])
+        if partition_name[it] == 'CL_set':
+            range_str = str(CL_depth_int_set[0]) + '_' + str(CL_depth_int_set[1])
 
-        elif C_part_in == 'IC' or C_part_in == 'CU'  or C_part_in == 'CC':
-            range_str = str(CL_int[0]) + '_' + str(CL_int[1])
+        elif partition_name[it] == 'IC' or partition_name[it] == 'CU'  or partition_name[it] == 'CC' \
+                or partition_name[it] == 'CL_calc':
+            range_str = str(CL_depth_int_calc[0]) + '_' + str(CL_depth_int_calc[1])
 
-        elif C_part_in == 'ML':
-            range_str = str(ML_int[0]) + '_' + str(ML_int[1])
+        elif partition_name[it] == 'ML':
+            range_str = str(ML_r_int[0]) + '_' + str(ML_r_int[1])
+
         else:
             range_str = 'all_domain'
 
@@ -204,15 +305,17 @@ def get_stats_for_C(dataset_in, param, ML_int, CL_int, CL_depth_int, t_in, grid,
 
 
 for delta, it_d in enumerate(Deltas):
+    for beta_in, it_b in enumerate(beta_filt_num):
 
 
-    cloud_only_mask, env_only_mask, cloud_up_mask, cloud_core_mask = \
-        get_masks(contour_field_in, cloud_thres, other_vars, other_var_thres, less_greater_in, and_or_in, grid)
+        cloud_only_mask, env_only_mask, cloud_up_mask, cloud_core_mask = \
+            get_masks(data_path, file_name, delta, beta_in, **mask_options)
 
 
-    for smag, it_c in enumerate(['Cs', 'Cth', 'Cqt']):
+        for smag, it_c in enumerate(['Cs', 'C_th', 'C_qt']):
 
-        get_stats_for_C(dataset_in, param, ML_int, CL_int, CL_depth_int, t_in, grid,
-                        cloud_only_mask_in, env_only_mask_in, cloud_up_mask_in, cloud_core_mask_in)
+            get_stats_for_C(data_path, file_name, delta, beta_in, smag, z_ML_r_ind, z_cl_r_ind_calc, z_cl_r_ind_set,
+                            cloud_only_mask, env_only_mask, cloud_up_mask, cloud_core_mask)
+
 
 
