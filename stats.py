444f3f878c679b5b2206a0496eeb55df714d1191
alanna <a.power@pgr.reading.ac.uk>
2023-09-06 18:41:25 +0100

    cannot compress np.arrays

--- stats.py
+++ stats.py
@@ -287,30 +287,37 @@ def get_stats_for_C(dataset_path, file_name_in, Delta_in, beta_filt, param, ML_r
             param_name = param+'_sq'
 
 
-        if partition_name[it] == 'CL_set':
+        if partition_name[it % len(partition_name)] == 'CL_set':
             range_str = str(CL_depth_int_set[0]) + '_' + str(CL_depth_int_set[1])
 
-        elif partition_name[it] == 'IC' or partition_name[it] == 'CU'  or partition_name[it] == 'CC' \
-                or partition_name[it] == 'CL_calc':
+        elif partition_name[it % len(partition_name)] == 'IC' or partition_name[it] == 'CU'  or partition_name[it] == 'CC' \
+                or partition_name[it % len(partition_name)] == 'CL_calc':
             range_str = str(CL_depth_int_calc[0]) + '_' + str(CL_depth_int_calc[1])
 
-        elif partition_name[it] == 'ML':
+        elif partition_name[it % len(partition_name)] == 'ML':
             range_str = str(ML_r_int[0]) + '_' + str(ML_r_int[1])
 
         else:
             range_str = 'all_domain'
 
-
         C_mean = np.mean(C_part_in)
         C_st_dev = np.std(C_part_in)
         C_med = np.median(C_part_in)
-        C_25 = np.percentile( C_part_in.compressed(), 25)
-        C_75 = np.percentile( C_part_in.compressed(), 75)
         C_min = np.max(C_part_in)
         C_max = np.min(C_part_in)
-        C_n = len( C_part_in.compressed() )
 
-        row = [param_name, partition_name[it % 6], range_str, C_mean,
+        if partition_name[it % len(partition_name)] == 'ML' or partition_name[it % len(partition_name)] == 'IC' or partition_name[it % len(partition_name)] == 'CU' or partition_name[it % len(partition_name)] == 'CC':
+
+            C_25 = np.percentile( C_part_in.compressed(), 25)
+            C_75 = np.percentile( C_part_in.compressed(), 75)
+            C_n = len( C_part_in.compressed() )
+
+        else:
+            C_25 = np.percentile(C_part_in, 25)
+            C_75 = np.percentile(C_part_in, 75)
+            C_n = len(C_part_in)
+
+        row = [param_name, partition_name[it % len(partition_name)], range_str, C_mean,
                C_st_dev, C_med, C_25, C_75, C_min, C_max, C_n]
 
 
