eda994c55bb0041bd4dc1fca15519f918159d194
alanna <a.power@pgr.reading.ac.uk>
2023-09-25 18:32:26 +0100

    plotting arm and bomex on same plot

--- plot_sigmoid.py
+++ plot_sigmoid.py
@@ -42,6 +42,7 @@ if case == 'BOMEX':
 if case == 'ARM':
     data_path = '/work/scratch-pw3/apower/ARM/corrected_sigmas/filtering_filtered/diagnostics_3d_ts_'
     plotdir = '/gws/nopw/j04/paracon_rdg/users/apower/ARM/corrected_sigma/sigmoid/'
+    bomex_info_in = '/gws/nopw/j04/paracon_rdg/users/apower/BOMEX/data/'
     times = [ '18000', '25200', '32400', '39600' ]
 
     zn_set = np.arange(0, 4410, 10)
@@ -53,6 +54,10 @@ if case == 'ARM':
     z_ml_r_ind_list = [ [20, 75], [20, 80], [20, 85], [20, 90] ]
 
     Delta_values = [2 * 25, 4 * 25, 8 * 25, 16 * 25, 32 * 25, 64 * 25]
+    Delta_values_BOMEX = [2 * 20, 4 * 20, 8 * 20, 16 * 20, 32 * 20, 64 * 20]
+
+    ml_heights = [820, 970, 1120, 1270]
+    ml_height_bomex = 440
 
 
 
@@ -135,5 +140,48 @@ def plot_sigmoid(variable, data_dir, time_list, delta_list, layer, z_l_r_ind_lis
 
     plt.savefig(plotdir + f'{variable}_sigmoid_{layer}_log_{set_log_axis}.pdf')  # ("../plots/5m_w_variance_subgrid.png")
 
-plot_sigmoid(set_var, data_path, times, Deltas, 'Mid ML', z_ml_r_ind_list, set_log_axis)
-plot_sigmoid(set_var, data_path, times, Deltas, 'Mid CL', z_cl_r_ind_set, set_log_axis)
+# plot_sigmoid(set_var, data_path, times, Deltas, 'Mid ML', z_ml_r_ind_list, set_log_axis)
+# plot_sigmoid(set_var, data_path, times, Deltas, 'Mid CL', z_cl_r_ind_set, set_log_axis)
+
+def plot_all_sigmoid(variable, data_dir, extra_case_npy, time_list, delta_list, layer, z_l_r_ind_list_in):
+
+    col_list = ['k', 'r', 'b', 'g', 'y', 'm', 'tab:gray']
+
+    plt.figure(figsize=(6, 6))
+
+    for t, time_str in enumerate(time_list):
+
+        clock_time_int = 05.30 + int(time_str) / (60 * 60)
+        clock_time = str(clock_time_int) + '0L'
+
+        z_l_r = z_l_r_ind_list_in[t]
+        z_l_mid_layer = int( (z_l_r[0] + z_l_r[1])/2 )
+        print('axis index for ', layer, f'at time {time_str} is ', z_l_mid_layer)
+
+        var_sig = calc_variance(variable, data_dir, time_str, z_l_mid_layer, delta_list)
+
+        plt.semilogx(Delta_values/ml_heights[t], var_sig/var_sig[0], col_list[t], label=f'ARM at {clock_time}')
+
+    e_case = np.load(extra_case_npy+f'BOMEX_{variable}_{layer}.npy')
+    plt.semilogx(Delta_values_BOMEX/ml_height_bomex, e_case/e_case[0], col_list[t+1], label=f'BOMEX')
+
+    # plt.errorbar(res[0] / z_i, w_var[0] / w_var[0], yerr=var_err[0] / w_var[0], label=str(res[0]) + 'm',
+    #              color=col_list[0], ecolor='green', fmt='o', capsize=5)
+    # for i in range(len(res) - 1):
+    #     plt.errorbar(res[i + 1] / z_i, w_var[i + 1] / w_var[1], yerr=var_err[i + 1] / w_var[1],
+    #                  label=str(res[i + 1]) + 'm',
+    #                  color=col_list[i + 1], ecolor='green', fmt='o', capsize=5)
+
+    plt.xlabel("$\\widehat{\\bar{\\Delta}} / z_{ML}$", fontsize=14)
+    plt.title(f'{layer}', fontsize=16)
+    if variable == 'w':
+        plt.ylabel("$\\overline{ w'^2 / w'^2_{total}$", fontsize=16)
+    # plt.ylim(ymax=1.1, ymin=0.0)
+    # plt.xlim(xmax=4e3, xmin=3e0)
+    plt.legend(fontsize=12, loc='best')
+    # plt.xticks(Delta_values, Delta_labels)
+
+    plt.savefig(plotdir + f'all_{variable}_sigmoids_{layer}_log_{set_log_axis}.pdf')
+
+plot_all_sigmoid(set_var, data_path, bomex_info_in, times, Deltas, 'Mid ML', z_ml_r_ind_list)
+plot_all_sigmoid(set_var, data_path, bomex_info_in, times, Deltas, 'Mid CL', z_ml_r_ind_list)
