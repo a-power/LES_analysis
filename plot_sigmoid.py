b7640d85b8e8315b3148e0cbd1e713d7dbf61bd6
a-power <a.power@pgr.reading.ac.uk>
2023-10-11 16:05:06 +0100

    added nones

--- plot_sigmoid.py
+++ plot_sigmoid.py
@@ -166,10 +166,13 @@ def plot_sigmoid(variable, data_dir, time_list, delta_list, layer, z_l_r_ind_lis
             if os.path.exists(outdir + f'sigmoid_{case}_{variable}_{layer}_{time_str}.npy'):
                 var_sig = np.load(outdir+f'sigmoid_{case}_{variable}_{layer}_{time_str}.npy')
             else:
-                sig_u = calc_variance('u', data_dir, time_str, z_l_mid_layer, delta_list)
-                sig_v = calc_variance('v', data_dir, time_str, z_l_mid_layer, delta_list)
-                sig_w = calc_variance('w', data_dir, time_str, z_l_mid_layer, delta_list)
-                var_sig = 0.5 * (sig_u + sig_v + sig_w)
+                var_u = calc_variance('u', data_dir, time_str, z_l_mid_layer, delta_list)
+                var_v = calc_variance('v', data_dir, time_str, z_l_mid_layer, delta_list)
+                var_w = calc_variance('w', data_dir, time_str, z_l_mid_layer, delta_list)
+                var_sig = 0.5 * (var_u + var_v + var_w)
+                var_u = None
+                var_v = None
+                var_w = None
                 np.save(outdir+f'sigmoid_{case}_{variable}_{layer}_{time_str}.npy', var_sig)
 
         else:
