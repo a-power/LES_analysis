26431000998d1dd72de7123421c2a6fbad099889
alanna <a.power@pgr.reading.ac.uk>
2023-10-20 17:22:17 +0100

    prettier

--- spectra_from_notebook.py
+++ spectra_from_notebook.py
@@ -161,8 +161,8 @@ ax.loglog(w_kpo_filt3g1*z_ml_height/(2*np.pi), w_filt_1st_2[:,z_set], '--',  lab
 # ax.loglog(w_kpo_filt4g1*z_ml_height/(2*np.pi), w_filt_1st_3[:,z_set], label="$\\sigma$ = 200m")
 # ax.loglog(w_kpo_filt5g1*z_ml_height/(2*np.pi), w_filt_1st_4[:,z_set], label="$\\sigma$ = 400m")
 # ax.loglog(w_kpo_filt6g1*z_ml_height/(2*np.pi), w_filt_1st_5[:,z_set], label="$\\sigma$ = 800m")
-ax.loglog(90*turb_slope_x, 0.02*turb_slope_y, 'k-') #0.015
-ax.text(10, 1.6, r'$k^{-5/3}$', fontsize=14)
+ax.loglog(90*turb_slope_x, 0.025*turb_slope_y, 'k-') #0.015
+ax.text(8, 1.9, r'$k^{-5/3}$', fontsize=14)
 
 ax.loglog(w_kpo_filt1g2*z_ml_height/(2*np.pi), w_filt_2nd_0[:,z_set], '-.', label="$\\sigma$ = 25m, $\\sigma$ = 25m")
 ax.loglog(w_kpo_filt2g2*z_ml_height/(2*np.pi), w_filt_2nd_1[:,z_set], '-.', label="$\\sigma$ = 50m, $\\sigma$ = 25m")
