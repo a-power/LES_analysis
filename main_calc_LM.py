289c2053ddcd1f7a24e688ec1b7e9baad8ef0cd3
a-power <a.power@pgr.reading.ac.uk>
2023-11-27 13:14:30 +0000

    typos

--- main_calc_LM.py
+++ main_calc_LM.py
@@ -5,7 +5,7 @@ import xarray as xr
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--t', type=str, default='0')
+parser.add_argument('--t', type=int, default=0)
 parser.add_argument('--case', type=str, default='ARM')
 args = parser.parse_args()
 t_in = args.t
