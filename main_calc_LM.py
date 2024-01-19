19585b6d65b142832082524a6f6b7bc26c06ad93
alanna <a.power@pgr.reading.ac.uk>
2024-01-19 17:32:53 +0000

    typo fixes

--- main_calc_LM.py
+++ main_calc_LM.py
@@ -67,9 +67,9 @@ for i, C_res_in in enumerate(C_res):
 
     file_in = file_f + f'gaussian_filter_ga0{i}_gaussian_filter_ga00.nc'
     data_in = path_f + folder_ff + file_in
-    dataset_name = [path_f + file_f + f'Cs_{dx_bar_in[i]}_{dx_hat_in[i]}.nc',
-                     path_f + file_f + f'C_th_{dx_bar_in[i]}_{dx_hat_in[i]}.nc',
-                     path_f + file_f + f'C_qt_{dx_bar_in[i]}_{dx_hat_in[i]}.nc']
+    dataset_name = [path_f + folder_ff + file_f + f'Cs_{dx_bar_in[i]}_{dx_hat_in[i]}.nc',
+                     path_f + folder_ff + file_f + f'C_th_{dx_bar_in[i]}_{dx_hat_in[i]}.nc',
+                     path_f + folder_ff + file_f + f'C_qt_{dx_bar_in[i]}_{dx_hat_in[i]}.nc']
 
     DX_in = {
         'indir': data_in,
@@ -90,7 +90,14 @@ for i, C_res_in in enumerate(C_res):
                 dy_s.C_scalar(scalar=scalar_in, ingrid=mygrid, save_all = set_save_all, **DX_in)
 
         ds = xr.Dataset()
-        ds.to_netcdf(dataset_name[i], mode='w')
+        if scalar_in == 'momentum':
+            ds.to_netcdf(dataset_name[0], mode='w')
+        elif scalar_in == 'th':
+            ds.to_netcdf(dataset_name[0], mode='w')
+        elif scalar_in == 'q_total':
+            ds.to_netcdf(dataset_name[0], mode='w')
+        else:
+            print('scalar not set to momentum, th, or q_total')
         ds_in = {'file':dataset_name[i], 'ds': ds}
 
         save_field(ds_in, HR_field)
