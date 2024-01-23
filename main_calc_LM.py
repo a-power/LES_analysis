a87db54c4c9bf70ea2d833e392bdc9d2b2072059
alanna <a.power@pgr.reading.ac.uk>
2024-01-23 16:17:42 +0000

    BOMEX case testing C_th

--- main_calc_LM.py
+++ main_calc_LM.py
@@ -5,8 +5,8 @@ import xarray as xr
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--t', type=int, default=2)
-parser.add_argument('--case', type=str, default='ARM')
+parser.add_argument('--t', type=int, default=0)
+parser.add_argument('--case', type=str, default='BOMEX')
 args = parser.parse_args()
 t_in = args.t
 
