629f0bcc18dd58b33b4a390b52d51d413ce4d899
alanna <a.power@pgr.reading.ac.uk>
2024-04-05 11:40:31 +0100

    why not workingggg

--- main_calc_LM.py
+++ main_calc_LM.py
@@ -21,12 +21,20 @@ case_in = args.case
 av_type = 'all'
 mygrid = 'p'
 
+
+set_save_all = 2
+
+print('about to start code')
+
 if filtering_filters_yn == 'y' or filtering_filters_yn == 'yes':
     filtering_filters = True
+    print('filtering_filters is se to False')
 else:
     filtering_filters = False
+    print('filtering_filters is se to True')
 
 if case_in == 'BOMEX':
+    print('using BOMEX')
     path_f = '/work/scratch-pw3/apower/BOMEX/'
     folder_f = 'first_filt/'
     folder_ff = 'second_filt/'
@@ -34,20 +42,21 @@ if case_in == 'BOMEX':
     time_in = times_list[0]
     file_f = f'BOMEX_m0020_g0800_all_{time_in}_'
     Delta = 20
-    if filtering_filters == True:
-        scalar = ['momentum', 'f(th_on_p)_r', 'q_total']
-    else:
-        scalar = ['momentum', 'th', 'q_total']
+
     if filtering_filters == False:
         dx_bar_in = np.array([20, 20, 20, 20, 20, 20])
         dx_hat_in = np.array([40, 80, 160, 320, 640, 1280])
         C_res = ['2D', '4D', '8D', '16D', '32D', '64D']
+        scalar = ['momentum', 'th', 'q_total']
+
     elif filtering_filters == True:
         dx_bar_in = 2*np.array([20, 40, 80, 160, 320, 640])
         dx_hat_in = 2*np.array([40, 80, 160, 320, 640, 1280])
         C_res = ['4D', '8D', '16D', '32D', '64D', '128D']
+        scalar = ['momentum', 'f(th_on_p)_r', 'q_total']
 
 elif case_in == 'ARM':
+    print('using ARM')
     times_list = ['18000', '25200', '32400', '39600']
     time_in = times_list[t_in]
     path_f = '/work/scratch-pw3/apower/ARM/'
@@ -55,27 +64,22 @@ elif case_in == 'ARM':
     folder_ff = 'second_filt/'
     file_f = f'diagnostics_3d_ts_{time_in}_'
     Delta = 25
+
     if filtering_filters == False:
         dx_bar_in = np.array([25, 25, 25, 25, 25, 25])
         dx_hat_in = np.array([50, 100, 200, 400, 800, 1600])
         C_res = ['2D', '4D', '8D', '16D', '32D', '64D']
-    # dx_bar_in = np.array([400, 800])
-    # dx_hat_in = np.array([800, 1600])
-
-
-    #C_res = ['32D', '64D']
+        scalar = ['momentum', 'th', 'q_total']
 
     elif filtering_filters == True:
+        scalar = ['momentum', 'f(th_on_p)_r', 'q_total']
         dx_bar_in = 2*np.array([25, 50, 100, 200, 400, 800])
         dx_hat_in = 2*np.array([50, 100, 200, 400, 800, 1600])
         C_res = ['4D', '8D', '16D', '32D', '64D', '128D']
 
-    if filtering_filters == True:
-        scalar = ['momentum', 'f(th_on_p)_r', 'q_total']
-    else:
-        scalar = ['momentum', 'th', 'q_total']
 
 elif case_in=='dry':
+    print('using dry CBL case')
     times_list = ['13800']
     time_in = times_list[t_in]
     path_f = f'/storage/silver/greybls/si818415/dry_CBL/'
@@ -83,18 +87,19 @@ elif case_in=='dry':
     folder_ff = 'second_filt/'
     file_f = f'cbl_{time_in}_'
     Delta=20
-    if filtering_filters == True:
-        scalar = ['momentum', 'f(th_on_p)_r']
-    else:
-        scalar = ['momentum', 'th']
+
     if filtering_filters == False:
         dx_bar_in = np.array([20, 20, 20, 20, 20, 20])
         dx_hat_in = np.array([40, 80, 160, 320, 640, 1280])
         C_res = ['2D', '4D', '8D', '16D', '32D', '64D']
-    if filtering_filters == True:
+        scalar = ['momentum', 'th']
+
+    elif filtering_filters == True:
         dx_bar_in = 2*np.array([20, 40, 80, 160, 320, 640])
         dx_hat_in = 2*np.array([40, 80, 160, 320, 640, 1280])
         C_res = ['4D', '8D', '16D', '32D', '64D', '128D']
+        scalar = ['momentum', 'f(th_on_p)_r']
+
 
 else:
     print('case not recognised')
@@ -103,13 +108,13 @@ else:
 #     os.makedirs(path_f+folder_ff, exist_ok = True)
 
 
-set_save_all = 2
 
 
 for it in range(len(C_res) - nfilt):
     i = it+nfilt
 
     if filtering_filters == True:
+        print('using 2nd filt')
         file_in = file_f + f'gaussian_filter_ga0{i}_gaussian_filter_ga00.nc'
         data_in = path_f + folder_ff + file_in
         print('reading files', data_in)
@@ -120,6 +125,7 @@ for it in range(len(C_res) - nfilt):
                          path_f + folder_ff + 'LM/' + file_f + f'C_qt_{dx_bar_in[i]}_{dx_hat_in[i]}.nc']
 
     elif filtering_filters == False:
+        print('using 1st filt')
         file_in = file_f + f'gaussian_filter_ga0{i}.nc'
         data_in = path_f + folder_f + file_in
         print('reading files', data_in)
@@ -141,7 +147,7 @@ for it in range(len(C_res) - nfilt):
 
         if scalar_in == 'momentum':
             scalar_index = 0
-        elif scalar_in == 'th'  or scalar_in == 'th_tot' or scalar_in == 'f(th_on_p)_r':
+        elif scalar_in == 'th' or scalar_in == 'th_tot' or scalar_in == 'f(th_on_p)_r':
             scalar_index = 1
         elif scalar_in == 'q_total':
             scalar_index = 2
@@ -154,9 +160,11 @@ for it in range(len(C_res) - nfilt):
         # ds.to_netcdf(file_setup, mode='w')
 
         if scalar_in == 'momentum':
+            print('about to start the Cs routine')
             #z_save, zn_save, C_sq_prof, C_prof, HR_prof, RR_prof, HR_field, RR_field = \
             dy_s.Cs(file_save_to=file_setup, ingrid=mygrid, save_all=set_save_all, **DX_in)
             # = \ #, C_sq_field_2D, Hj_2D, Rj_2D = \
         else:
+            print('about to start the Cs routine')
             #z_save, zn_save, C_sq_prof, C_prof, HR_prof, RR_prof, HR_field, RR_field = \
             dy_s.C_scalar(scalar=scalar_in, file_save_to=file_setup, ingrid=mygrid, save_all=set_save_all, **DX_in)
