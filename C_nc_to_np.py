2b94a0438fce09c46b5bd9438aff81a6cd596145
alanna <a.power@pgr.reading.ac.uk>
2024-04-16 16:57:16 +0100

    typo fixes

--- C_nc_to_np.py
+++ C_nc_to_np.py
@@ -59,7 +59,7 @@ elif case=='ARM':
 
 
 
-C_sq =  np.zeros_like((len(scalars), len(partitions), len(res), len(zn_set)))
+C_sq =  np.zeros_like((len(numer), len(partitions), len(res), len(zn_set)))
 
 for r in res:
     C_data = xr.open_dataset(homedir + f'{r}.nc')
