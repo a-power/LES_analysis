d3c4c6ecdf9253b46c5b3b80dcc453f2f3a441ce
alanna <a.power@pgr.reading.ac.uk>
2024-04-19 18:29:41 +0100

    running for th_e. forgot to add alias for th_e

--- main_dynamic_on_filtered_data.py
+++ main_dynamic_on_filtered_data.py
@@ -114,6 +114,7 @@ elif case_in=='ARM':
                             'w': [f'f(w_on_{opgrid})_r'],
                             'th_tot': [f'f(th_on_{opgrid})_r'],
                             'th_v': [f'f(th_v_on_{opgrid})_r'],
+                            'th_e': [f'f(th_e_on_{opgrid})_r'],
                             'q_total': [f'f(q_total_on_{opgrid})_r'],
                             'q_vapour': [f'f(q_vapour_on_{opgrid})_r'],
                             'buoyancy': [f'f(buoyancy_on_{opgrid})_r'],
