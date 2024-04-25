504068b81a44a592e48d0d251339872b2b36f77f
alanna <a.power@pgr.reading.ac.uk>
2024-04-25 10:20:00 +0100

    updating to th_L

--- main_calc_LM.py
+++ main_calc_LM.py
@@ -148,10 +148,10 @@ for it in range(len(dx_hat_in) - nfilt):
         data_in = path_f + folder_ff + file_in
         print('reading files', data_in)
 
-        os.makedirs(path_f + folder_ff + 'LM/', exist_ok = True)
-        dataset_name = [path_f + folder_ff + 'LM/' + file_f + f'Cs_{dx_bar_in[i]}_{dx_hat_in[i]}.nc',
-                         path_f + folder_ff + 'LM/' + file_f + f'C_th_{dx_bar_in[i]}_{dx_hat_in[i]}.nc',
-                         path_f + folder_ff + 'LM/' + file_f + f'C_qt_{dx_bar_in[i]}_{dx_hat_in[i]}.nc']
+        os.makedirs(path_f + folder_ff + 'LM/update/', exist_ok = True)
+        dataset_name = [path_f + folder_ff + 'LM/update/' + file_f + f'Cs_{dx_bar_in[i]}_{dx_hat_in[i]}.nc',
+                         path_f + folder_ff + 'LM/update/' + file_f + f'C_th_{dx_bar_in[i]}_{dx_hat_in[i]}.nc',
+                         path_f + folder_ff + 'LM/update/' + file_f + f'C_qt_{dx_bar_in[i]}_{dx_hat_in[i]}.nc']
 
     elif filtering_filters == False:
         print('using 1st filt')
