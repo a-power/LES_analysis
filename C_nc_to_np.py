d06ffdb486de5a3082dac507e7dc46a67fad08d0
alanna <a.power@pgr.reading.ac.uk>
2024-04-26 12:32:14 +0100

    new data: new paths (woops)

--- C_nc_to_np.py
+++ C_nc_to_np.py
@@ -30,11 +30,11 @@ if case=='BOMEX':
     zn_set = np.arange(0, 3020, 20)
     time = '14400'
     if filt_num == 1:
-        homedir = '/work/scratch-pw3/apower/BOMEX/first_filt/LM/C_profs/BOMEX_m0020_g0800_all_14400_C_cond_profs_'
+        homedir = '/work/scratch-pw3/apower/BOMEX/first_filt/LM/update/C_profs/BOMEX_m0020_g0800_all_14400_C_cond_profs_'
         res = ['20_40', '20_80D', '20_160', '20_320', '20_640', '20_1280']
         #res = ['20_40', '40_80', '80_160', '160_320', '320_640', '640_1280']
     elif filt_num == 2:
-        homedir = '/work/scratch-pw3/apower/BOMEX/second_filt/LM/C_profs/BOMEX_m0020_g0800_all_14400_C_cond_profs_'
+        homedir = '/work/scratch-pw3/apower/BOMEX/second_filt/LM/update/C_profs/BOMEX_m0020_g0800_all_14400_C_cond_profs_'
         if beta == 0:
             res = ['40_80D_0', '80_160_0', '160_320_0', '320_640_0', '640_1280_0', '1280_2560_0']
         else:
@@ -47,11 +47,11 @@ elif case=='ARM':
     times_analysed = ['18000', '25200', '32400', '39600']
     time = times_analysed[t_in]
     if filt_num == 1:
-        homedir = f'/work/scratch-pw3/apower/ARM/first_filt/LM/C_profs/diagnostics_3d_ts_{time}_C_cond_profs_'
+        homedir = f'/work/scratch-pw3/apower/ARM/first_filt/LM/update/C_profs/diagnostics_3d_ts_{time}_C_cond_profs_'
         res = ['2D', '4D', '8D', '16D', '32D', '64D']
         #res = ['25_50', '50_100', '100_200']
     elif filt_num == 2:
-        homedir = f'/work/scratch-pw3/apower/ARM/second_filt/LM/C_profs/diagnostics_3d_ts_{time}_C_cond_profs_'
+        homedir = f'/work/scratch-pw3/apower/ARM/second_filt/LM/update/C_profs/diagnostics_3d_ts_{time}_C_cond_profs_'
         if beta == 0:
             res = ['50_100_0', '100_200_0', '200_400_0', '400_800_0', '800_1600_0', '1600_3200_0']
         else:
