992529310aa8a8fa23b1fd63b61993ecb925ae4a
a-power <a.power@pgr.reading.ac.uk>
2024-05-07 18:03:54 +0100

    need to do qv as well as qt for all cases

--- C_nc_to_np.py
+++ C_nc_to_np.py
@@ -17,14 +17,16 @@ case = args.case
 beta=args.b
 
 th_type = 'th_v'
+q_type = 'qt'
+folder = th_type
 
-numer = ['LM', f'HR_{th_type}', 'HR_qt']
-denom = ['MM', f'RR_{th_type}', 'RR_qt']
+numer = ['LM', f'HR_{th_type}', f'HR_{q_type}']
+denom = ['MM', f'RR_{th_type}', f'RR_{q_type}']
 partitions = ['prof', 'env_prof', 'cloud_prof', 'w_prof', 'w_th_v_prof']
 
 filt_num = 2
 
-data_dir = f'/gws/nopw/j04/paracon_rdg/users/apower/LES_analysis/data/C_profs_th_L/{case}/'
+data_dir = f'/gws/nopw/j04/paracon_rdg/users/apower/LES_analysis/data/C_profs/{folder}/{case}/'
 os.makedirs(data_dir, exist_ok = True)
 
 if case=='BOMEX':
@@ -32,11 +34,11 @@ if case=='BOMEX':
     zn_set = np.arange(0, 3020, 20)
     time = '14400'
     if filt_num == 1:
-        homedir = '/work/scratch-pw3/apower/BOMEX/first_filt/LM/update/C_profs/BOMEX_m0020_g0800_all_14400_C_cond_profs_'
+        homedir = f'/work/scratch-pw3/apower/BOMEX/first_filt/LM/{folder}/C_profs/BOMEX_m0020_g0800_all_14400_C_cond_profs_'
         res = ['20_40', '20_80', '20_160', '20_320', '20_640', '20_1280']
         #res = ['20_40', '40_80', '80_160', '160_320', '320_640', '640_1280']
     elif filt_num == 2:
-        homedir = '/work/scratch-pw3/apower/BOMEX/second_filt/LM/update/C_profs/BOMEX_m0020_g0800_all_14400_C_cond_profs_'
+        homedir = f'/work/scratch-pw3/apower/BOMEX/second_filt/LM/{folder}/C_profs/BOMEX_m0020_g0800_all_14400_C_cond_profs_'
         if beta == 0:
             res = ['40_80_0', '80_160_0', '160_320_0', '320_640_0', '640_1280_0', '1280_2560_0']
         else:
@@ -49,11 +51,11 @@ elif case=='ARM':
     times_analysed = ['18000', '25200', '32400', '39600']
     time = times_analysed[t_in]
     if filt_num == 1:
-        homedir = f'/work/scratch-pw3/apower/ARM/first_filt/LM/update/C_profs/diagnostics_3d_ts_{time}_C_cond_profs_'
+        homedir = f'/work/scratch-pw3/apower/ARM/first_filt/LM/{folder}/C_profs/diagnostics_3d_ts_{time}_C_cond_profs_'
         res = ['2D', '4D', '8D', '16D', '32D', '64D']
         #res = ['25_50', '50_100', '100_200']
     elif filt_num == 2:
-        homedir = f'/work/scratch-pw3/apower/ARM/second_filt/LM/update/C_profs/diagnostics_3d_ts_{time}_C_cond_profs_'
+        homedir = f'/work/scratch-pw3/apower/ARM/second_filt/LM/{folder}/C_profs/diagnostics_3d_ts_{time}_C_cond_profs_'
         if beta == 0:
             res = ['50_100_0', '100_200_0', '200_400_0', '400_800_0', '800_1600_0', '1600_3200_0']
         else:
