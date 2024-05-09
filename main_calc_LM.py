6ff095eda4a231e395ba3ca891791f420335cfd5
alanna <a.power@pgr.reading.ac.uk>
2024-05-09 11:03:34 +0100

    fixing typos

--- main_calc_LM.py
+++ main_calc_LM.py
@@ -48,7 +48,7 @@ if case_in == 'BOMEX':
     print('using BOMEX')
     path_f = '/work/scratch-pw3/apower/BOMEX/'
     folder_f = 'first_filt/'
-    folder_ff = f'second_filt/{th_type}/'
+    folder_ff = f'second_filt/{th_type}_and{q_type}/'
     times_list = ['14400']
     time_in = times_list[0]
     file_f = f'BOMEX_m0020_g0800_all_{time_in}_'
