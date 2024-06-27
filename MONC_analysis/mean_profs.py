2a9c750d84514319e7488765568ce8da07f52301
alanna <a.power@pgr.reading.ac.uk>
2024-06-27 13:54:20 +0100

    trying to get x axis on local time

---
+++ MONC_analysis/mean_profs.py
@@ -0,0 +1,81 @@
+import numpy as np
+import numpy.ma as ma
+import xarray as xr
+import matplotlib.pyplot as plt
+import datetime
+
+
+def get_cloud_only(CT_or_CB_field, dist_from_surf_threas=1):
+
+    mask_no_cloud = ma.masked_less_equal(CT_or_CB_field, dist_from_surf_threas)
+
+    return mask_no_cloud
+
+
+
+
+path_ARM25 = '/work/scratch-pw3/apower/ARM/MONC_out/25m/'
+path_MONC_alt = '/storage/silver/scenario/si818415/altered_MONC/'
+
+plotdir = '/home/users/si818415/phd/plots/MONC_alt/'
+
+list_timestamps = [17400, 18000, 24600, 25200, 31800, 32400, 39000, 39600]
+
+#np.ndarray.tolist( np.arange(17400, 40000, 600) )
+
+
+
+CB_mean_height_ts = np.zeros(len(list_timestamps))
+CT_mean_height_ts = np.zeros(len(list_timestamps))
+
+for nt, time in enumerate(list_timestamps):
+
+    filein = f'arm_3d_{str(time)}.nc'
+
+    ds_in = xr.open_dataset(path_MONC_alt+filein)
+    CB_field = ds_in['clbas'].data
+    CT_field = ds_in['cltop'].data
+
+    CB_cloud_only = get_cloud_only(CB_field)
+    CT_cloud_only = get_cloud_only(CT_field)
+
+    CB_mean_height_ts[nt] = np.mean(CB_cloud_only)
+    CT_mean_height_ts[nt] = np.mean(CT_cloud_only)
+
+
+
+
+fig = plt.plot(figsize=(12, 4))
+plt.tight_layout(pad=0.5)
+
+plt.plot(list_timestamps, CB_mean_height_ts, 'k')
+plt.plot(list_timestamps, CT_mean_height_ts, 'k', label='HCs $\\widehat{\\bar{\\Delta}} = 200$m')
+
+plt.legend(fontsize=13, loc='upper right')
+
+
+# bottom, top = plt.ylim()
+# plt.ylim(bottom=0, top = 2200)
+
+# og_xtic = plt.xticks()
+# print(og_xtic)
+
+# time_label_temp = "%.2f"%(05.50 + og_xtic[0]/(60*60))
+# time_label_temp_min = (( np.round(05.50 + og_xtic[0]/(60*60), 2 ) - "%.2f"%(05.50 + og_xtic[0]/(60*60)) )*60 )/100
+# time_label = np.round(time_label_temp + time_label_temp_min, 2)
+
+# time_label = []
+#
+# for i in range(len(og_xtic[0])):
+#     time_label.append(datetime.timedelta(seconds=og_xtic[0][i] + 19800))
+#
+# plt.xticks(og_xtic[0], time_label)
+
+plt.ylabel('z (m)', fontsize=14)
+plt.xlabel(f'{var}', fontsize=14)
+
+plt.tight_layout()
+
+plt.savefig(plotdir+f'{var}_mean_prof.png', bbox_inches='tight')
+plt.savefig(plotdir + f'{var}_mean_prof.pdf', bbox_inches='tight')
+plt.close()
