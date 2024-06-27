22f213dfaab7263c79c19c5d89cda75a0aa5bbc5
alanna <a.power@pgr.reading.ac.uk>
2024-06-27 13:26:17 +0100

    trying to get x axis on local time

--- MONC_analysis/getting_CB_and_CT_with_time.py
+++ MONC_analysis/getting_CB_and_CT_with_time.py
@@ -2,6 +2,7 @@ import numpy as np
 import numpy.ma as ma
 import xarray as xr
 import matplotlib.pyplot as plt
+import math
 
 
 def get_cloud_only(CT_or_CB_field, dist_from_surf_threas=1):
@@ -59,8 +60,8 @@ plt.ylim(bottom=0, top = 2200)
 og_xtic = plt.xticks()
 print(og_xtic)
 
-time_label_temp = np.round(05.50 + og_xtic[0]/(60*60), 2 )
-time_label_temp_min = (( np.round(05.50 + og_xtic[0]/(60*60), 2 ) - np.round(05.50 + og_xtic[0]/(60*60), 0) )*60 )/100
+time_label_temp = math.floor(05.50 + og_xtic[0]/(60*60))
+time_label_temp_min = (( np.round(05.50 + og_xtic[0]/(60*60), 2 ) - math.floor(05.50 + og_xtic[0]/(60*60)) ) *60 )/100
 time_label = np.round(time_label_temp + time_label_temp_min, 2)
 
 plt.xticks(og_xtic[0], time_label)
