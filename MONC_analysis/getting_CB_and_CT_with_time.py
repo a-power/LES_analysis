1d4a012748c20263cb8136db021d5b93c867b2e3
alanna <a.power@pgr.reading.ac.uk>
2024-06-27 12:11:03 +0100

    plotting CT and CB vs t

---
+++ MONC_analysis/getting_CB_and_CT_with_time.py
@@ -0,0 +1,65 @@
+import numpy as np
+import numpy.ma as ma
+import xarray as xr
+import matplotlib.pyplot as plt
+
+
+def get_cloud_only(CT_or_CB_field, dist_from_surf_threas=1):
+
+    mask_no_cloud = ma.masked_less_equal(CT_or_CB_field, dist_from_surf_threas)
+
+    return mask_no_cloud
+
+
+
+
+path_ARM25 = '/work/scratch-pw3/apower/ARM/MONC_out/25m/'
+path_MONC_alt = '/storage/silver/scenario/si818415/altered_MONC/'
+
+plotdir = '/home/users/si818415/phd/plots/MONC_alt/'
+
+list_timestamps = [17400, 18000, 24600, 25200, 31800, 32400, 39000, 39600]
+
+#np.ndarray.tolist( np.arange(17400, 40000, 600) )
+
+
+
+CB_mean_height_ts = np.zeros(len(list_timestamps))
+CT_mean_height_ts = np.zeros(len(list_timestamps))
+
+for nt in (list_timestamps):
+
+    filein = f'arm_3d_{str(nt)}.nc'
+
+    ds_in = xr.open_dataset(path_MONC_alt+filein)
+    CB_field = ds_in['clbase'].data
+    CT_field = ds_in['cltop'].data
+
+    CB_cloud_only = get_cloud_only(CB_field)
+    CT_cloud_only = get_cloud_only(CT_field)
+
+    CB_mean_height_ts[nt] = np.mean(CB_cloud_only)
+    CT_mean_height_ts[nt] = np.mean(CT_cloud_only)
+
+
+
+
+fig, ax = plt.plot(figsize=(10, 5))
+fig.tight_layout(pad=0.5)
+
+plt.plot(list_timestamps, CB_mean_height_ts, 'k')
+plt.plot(list_timestamps, CT_mean_height_ts, 'k', label='HCs $\\widehat{\\bar{\\Delta}} = 200$m')
+
+ax.legend(fontsize=13, loc='upper right')
+
+
+bottom, top = plt.ylim()
+plt.ylim(bottom=0)
+plt.ylabel('z (m)', fontsize=14)
+plt.xlabel('Cloud top and base height', fontsize=14)
+
+plt.tight_layout()
+
+plt.savefig(plotdir+f'ARM_cloud_top_and_base_ts.png', bbox_inches='tight')
+plt.savefig(plotdir + f'ARM_cloud_top_and_base_ts.pdf', bbox_inches='tight')
+plt.close()
