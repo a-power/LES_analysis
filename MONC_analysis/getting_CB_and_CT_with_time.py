e83ba41a3dfbf7808493cc2d96a41038b4eac043
alanna <a.power@pgr.reading.ac.uk>
2024-06-27 13:39:23 +0100

    trying to get x axis on local time

--- MONC_analysis/getting_CB_and_CT_with_time.py
+++ MONC_analysis/getting_CB_and_CT_with_time.py
@@ -64,7 +64,11 @@ print(og_xtic)
 # time_label_temp_min = (( np.round(05.50 + og_xtic[0]/(60*60), 2 ) - "%.2f"%(05.50 + og_xtic[0]/(60*60)) )*60 )/100
 # time_label = np.round(time_label_temp + time_label_temp_min, 2)
 
-plt.xticks(og_xtic[0], datetime.timedelta(seconds=og_xtic[0]))
+time_label = np.zeros(len(og_xtic[0])
+for i in len(og_xtic[0]):
+    time_label[i] = datetime.timedelta(seconds=og_xtic[0][i])
+
+plt.xticks(og_xtic[0], time_label)
 
 plt.xlabel('Local time (hh.mm)', fontsize=14)
 plt.ylabel('z (m)', fontsize=14)
