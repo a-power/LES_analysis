c3c1550846b4bbf146172cb48700881d44b14ce5
alanna <a.power@pgr.reading.ac.uk>
2024-06-28 18:32:58 +0100

    getting the heights correct (percentiles not mean)

--- MONC_analysis/getting_CB_and_CT_with_time.py
+++ MONC_analysis/getting_CB_and_CT_with_time.py
@@ -40,8 +40,8 @@ for nt, time in enumerate(list_timestamps):
     CB_cloud_only = get_cloud_only(CB_field)
     CT_cloud_only = get_cloud_only(CT_field)
 
-    CB_mean_height_ts[nt] = np.nanpercentile(CB_cloud_only, 25)
-    CT_mean_height_ts[nt] = np.nanpercentile(CT_cloud_only,90)
+    CB_mean_height_ts[nt] = np.nanpercentile(CB_cloud_only, 10)
+    CT_mean_height_ts[nt] = np.nanpercentile(CT_cloud_only,95)
 
 
 
