16ecc5d3886123dac9407ebc7617f06d67529db6
alanna <a.power@pgr.reading.ac.uk>
2024-07-10 21:07:04 +0100

    typo fixes

--- MONC_analysis/getting_CB_and_CT_with_time.py
+++ MONC_analysis/getting_CB_and_CT_with_time.py
@@ -62,8 +62,13 @@ for n in range(6):
 fig = plt.plot(figsize=(12, 4))
 plt.tight_layout(pad=0.5)
 for i in range(6):
-    plt.plot(list_timestamps, CB_mean_height_ts[i,:], colour_cycle[i%3], line_list[i],)
-    plt.plot(list_timestamps, CT_mean_height_ts[i,:], colour_cycle[i%3], line_list[i], label=model_param[i]+f'{2 ** ((i+1) % 3)}$\\Delta$')
+    plt.plot(list_timestamps, CB_mean_height_ts[i,:], colour_cycle[i%3], linestyle=line_list[i])
+    # plt.plot(list_timestamps, CT_mean_height_ts[i,:], colour_cycle[i%3], linestyle=line_list[i], label=model_param[i]+f'{2 ** ((i+1) % 8)}$\\Delta$')
+    if i < 3:
+        plt.plot(list_timestamps, CT_mean_height_ts[i, :], colour_cycle[i % 3], linestyle=line_list[i],
+                 label=model_param[i] + f'{2 ** ((i + 1) % 4)}$\\Delta$')
+    else:
+        plt.plot(list_timestamps, CT_mean_height_ts[i, :], colour_cycle[i % 3], linestyle=line_list[i])
 
 plt.legend(fontsize=13, loc='upper right')
 
