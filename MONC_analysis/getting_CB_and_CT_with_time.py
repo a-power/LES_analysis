805bc90b22090bf5ae3f81edd342be192d9ec8e4
alanna <a.power@pgr.reading.ac.uk>
2024-07-10 20:57:22 +0100

    typo fixes

--- MONC_analysis/getting_CB_and_CT_with_time.py
+++ MONC_analysis/getting_CB_and_CT_with_time.py
@@ -29,12 +29,12 @@ colour_cycle = ['#377eb8', '#ff7f00', '#4daf4a',
                   '#f781bf', '#a65628', '#984ea3',
                   '#999999', '#e41a1c', '#dede00']
 line_list = ['--', '--', '--', ':', ':', ':']
-model_param = ['Stand', 'HCs'] #'HCs $\\widehat{\\bar{\\Delta}}'
+model_param = ['Stand', 'Stand', 'Stand', 'HCs', 'HCs', 'HCs'] #'HCs $\\widehat{\\bar{\\Delta}}'
 
 
 
-CB_mean_height_ts = np.zeros(6, len(list_timestamps))
-CT_mean_height_ts = np.zeros(6, len(list_timestamps))
+CB_mean_height_ts = np.zeros( (6, len(list_timestamps)) )
+CT_mean_height_ts = np.zeros( (6, len(list_timestamps)) )
 
 for n in range(6):
     if n <3:
