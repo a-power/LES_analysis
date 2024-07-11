645c6da5851a0d0c7b8c44056d8ba7326ae7dd83
alanna <a.power@pgr.reading.ac.uk>
2024-07-11 13:56:30 +0100

    test

--- MONC_analysis/mean_profs.py
+++ MONC_analysis/mean_profs.py
@@ -3,6 +3,7 @@ import numpy.ma as ma
 import xarray as xr
 import matplotlib.pyplot as plt
 import datetime
+import os
 
 
 def get_cloud_only(CT_or_CB_field, dist_from_surf_threas=1):
@@ -14,68 +15,100 @@ def get_cloud_only(CT_or_CB_field, dist_from_surf_threas=1):
 
 
 
-path_ARM25 = '/work/scratch-pw3/apower/ARM/MONC_out/25m/'
-path_MONC_alt = '/storage/silver/scenario/si818415/altered_MONC/'
+path_ARM25 = '/storage/silver/MONC_data/Alanna/ARM/MONC_out/25m/'
+path_MONC_alt_HCs = '/storage/silver/scenario/si818415/altered_MONC/'
+path_MONC_stand = '/storage/silver/scenario/si818415/og_monc/'
 
-plotdir = '/home/users/si818415/phd/plots/MONC_alt/'
+plotdir = '/home/users/si818415/phd/plots/MONC_alt/mean_profs/'
+os.makedirs(plotdir, exist_ok = True)
 
-list_timestamps = [17400, 18000, 24600, 25200, 31800, 32400, 39000, 39600]
+list_timestamps = [18000, 25200, 32400, 39600]
+#list_timestamps = [17400, 18000, 24600, 25200, 31800, 32400, 39000, 39600]
+#list_timestamps = np.arange(600, 40200, 600)
 
-#np.ndarray.tolist( np.arange(17400, 40000, 600) )
+var_list = ['wtheta_cn_mean', 'w_qt', 'ww_mean', 'wwsg_mean', 'total_cloud_fraction', 'tke_tendancy', 'tke_sq_mean',
+            'resolved_buoyant_production', 'resolved_shear_production', 'resolved_turbulent_transport',
+            'subgrid_buoyant_production', 'subgrid_shear_stress', 'subgrid_turbulent_transport']
+
+zn = np.arange(0, 44000, 10)
 
 
-
-CB_mean_height_ts = np.zeros(len(list_timestamps))
-CT_mean_height_ts = np.zeros(len(list_timestamps))
-
-for nt, time in enumerate(list_timestamps):
-
-    filein = f'arm_3d_{str(time)}.nc'
-
-    ds_in = xr.open_dataset(path_MONC_alt+filein)
-    CB_field = ds_in['clbas'].data
-    CT_field = ds_in['cltop'].data
-
-    CB_cloud_only = get_cloud_only(CB_field)
-    CT_cloud_only = get_cloud_only(CT_field)
-
-    CB_mean_height_ts[nt] = np.mean(CB_cloud_only)
-    CT_mean_height_ts[nt] = np.mean(CT_cloud_only)
+colour_cycle = ['#377eb8', '#ff7f00', '#4daf4a',
+                  '#f781bf', '#a65628', '#984ea3',
+                  '#999999', '#e41a1c', '#dede00']
+line_list = ['--', '--', '--', ':', ':', ':']
+model_param = ['Stand', 'Stand', 'Stand', 'HCs', 'HCs', 'HCs'] #'HCs $\\widehat{\\bar{\\Delta}}'
 
 
 
 
-fig = plt.plot(figsize=(12, 4))
-plt.tight_layout(pad=0.5)
 
-plt.plot(list_timestamps, CB_mean_height_ts, 'k')
-plt.plot(list_timestamps, CT_mean_height_ts, 'k', label='HCs $\\widehat{\\bar{\\Delta}} = 200$m')
+for nv, var in enumerate(var_list):
+    for nt, time in enumerate(list_timestamps):
 
-plt.legend(fontsize=13, loc='upper right')
+        var_prof = np.zeros( (6, len(zn) ) )
+
+        for n in range(6):
+            if n <3:
+                path_in = path_MONC_stand + f'{2 ** n}00m/'
+
+                filein = f'arm_{str(time)}.nc'
+                ds_in = xr.open_dataset(path_in + filein)
+                var_prof[n, :] = ds_in[f'{var}'].data
+
+            else:
+                path_in = path_MONC_alt_HCs + f'{2 ** (n - 3)}00m/'
 
 
-# bottom, top = plt.ylim()
-# plt.ylim(bottom=0, top = 2200)
+                if n == 1:
+                    if time > 30000:
+                        var_prof[n, :] = np.nan
 
-# og_xtic = plt.xticks()
-# print(og_xtic)
+                    else:
+                        filein = f'arm_{str(time)}.nc'
 
-# time_label_temp = "%.2f"%(05.50 + og_xtic[0]/(60*60))
-# time_label_temp_min = (( np.round(05.50 + og_xtic[0]/(60*60), 2 ) - "%.2f"%(05.50 + og_xtic[0]/(60*60)) )*60 )/100
-# time_label = np.round(time_label_temp + time_label_temp_min, 2)
+                        ds_in = xr.open_dataset(path_in + filein)
+                        var_prof[n, :] = ds_in[f'{var}'].data
 
-# time_label = []
-#
-# for i in range(len(og_xtic[0])):
-#     time_label.append(datetime.timedelta(seconds=og_xtic[0][i] + 19800))
-#
-# plt.xticks(og_xtic[0], time_label)
 
-plt.ylabel('z (m)', fontsize=14)
-plt.xlabel(f'{var}', fontsize=14)
 
-plt.tight_layout()
+        plt.plot(figsize=(5, 8))
+        for i in range(6):
+            # plt.plot(list_timestamps, CT_mean_height_ts[i,:], colour_cycle[i%3], linestyle=line_list[i], label=model_param[i]+f'{2 ** ((i+1) % 8)}$\\Delta$')
+            if i < 3:
 
-plt.savefig(plotdir+f'{var}_mean_prof.png', bbox_inches='tight')
-plt.savefig(plotdir + f'{var}_mean_prof.pdf', bbox_inches='tight')
-plt.close()
+                plt.plot(var_prof[i, :], zn, colour_cycle[i % 3], linewidth=4,
+                         label=f'{2 ** ((i + 2))}$\\Delta$')
+            else:
+
+                plt.plot(var_prof[i, :], zn, colour_cycle[i % 3], linestyle=line_list[i],
+                         marker='*')
+        plt.tight_layout(pad=0.5)
+        plt.gcf().set_size_inches(4, 9)
+        plt.legend(fontsize=13, loc='upper right')
+
+        bottom, top = plt.ylim()
+        # plt.ylim(bottom=0, top=4000)
+
+        # og_xtic = plt.xticks()
+        # print(og_xtic)
+        #
+        # # time_label_temp = "%.2f"%(05.50 + og_xtic[0]/(60*60))
+        # # time_label_temp_min = (( np.round(05.50 + og_xtic[0]/(60*60), 2 ) - "%.2f"%(05.50 + og_xtic[0]/(60*60)) )*60 )/100
+        # # time_label = np.round(time_label_temp + time_label_temp_min, 2)
+        #
+        # time_label = []
+        #
+        # for i in range(len(og_xtic[0])):
+        #     time_label.append(datetime.timedelta(seconds=og_xtic[0][i] + 19800))
+        #
+        # plt.xticks(og_xtic[0], time_label)
+
+        plt.xlabel(f'{var}', fontsize=14)
+        plt.ylabel('z (m)', fontsize=14)
+
+        plt.tight_layout()
+
+        plt.savefig(plotdir + f'ARM_{var}_{time}_mean_prof_HCs_vs_stand.png', bbox_inches='tight')
+        plt.savefig(plotdir + f'ARM_{var}_{time}_mean_prof_HCs_vs_stand.pdf', bbox_inches='tight')
+        plt.close()
