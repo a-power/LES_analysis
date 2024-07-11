ba309d47df9ce462030ac88b048ac80e4ea20e4b
alanna <a.power@pgr.reading.ac.uk>
2024-07-11 12:32:54 +0100

    making plot clearer

--- MONC_analysis/getting_CB_and_CT_with_time.py
+++ MONC_analysis/getting_CB_and_CT_with_time.py
@@ -80,12 +80,12 @@ plt.plot(figsize=(12, 4))
 for i in range(6):
     # plt.plot(list_timestamps, CT_mean_height_ts[i,:], colour_cycle[i%3], linestyle=line_list[i], label=model_param[i]+f'{2 ** ((i+1) % 8)}$\\Delta$')
     if i < 3:
-        plt.plot(list_timestamps, CB_mean_height_ts[i, :], colour_cycle[i % 3], linestyle=line_list[i])
-        plt.plot(list_timestamps, CT_mean_height_ts[i, :], colour_cycle[i % 3], linestyle=line_list[i],
+        plt.plot(list_timestamps, CB_mean_height_ts[i, :], colour_cycle[i % 3], linewidth=3)
+        plt.plot(list_timestamps, CT_mean_height_ts[i, :], colour_cycle[i % 3], linewidth=3,
                  label=f'{2 ** ((i + 2))}$\\Delta$')
     else:
-        plt.plot(list_timestamps, CB_mean_height_ts[i, :], colour_cycle[i % 3], linestyle=line_list[i], linewidth=2, marker='*')
-        plt.plot(list_timestamps, CT_mean_height_ts[i, :], colour_cycle[i % 3], linestyle=line_list[i], linewidth=2, marker='*')
+        plt.plot(list_timestamps, CB_mean_height_ts[i, :], colour_cycle[i % 3], linestyle=line_list[i], marker='*')
+        plt.plot(list_timestamps, CT_mean_height_ts[i, :], colour_cycle[i % 3], linestyle=line_list[i], marker='*')
 plt.tight_layout(pad=0.5)
 plt.legend(fontsize=13, loc='upper left')
 
