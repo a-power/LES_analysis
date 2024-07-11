fc51e5fff8ec8b5efe03c0e1f9c7fee499f1c554
alanna <a.power@pgr.reading.ac.uk>
2024-07-11 11:24:18 +0100

    double checking dx

--- get_dx.py
+++ get_dx.py
@@ -2,42 +2,49 @@ import numpy as np
 import re
 import xarray as xr
 
-data_test_path = '/storage/silver/scenario/si818415/altered_MONC/400m/arm_39600.nc'
-data_test = xr.open_dataset(data_test_path)
-
-def _bytarr_to_dict(d):
-
-    # Converted for xarray use
-
-    while len(np.shape(d))>2:
-        d = d[0]
-    res = {}
-    for i in range(np.shape(d)[0]):
-        opt = d[i,0].decode('utf-8')
-        val = d[i,1].decode('utf-8')
-
-        res[opt] = val
-    return res
-
-def options_database(source_dataset):
-
-    if 'options_database' in source_dataset.variables:
-        options_database = _bytarr_to_dict(
-            source_dataset['options_database'].values)
+for i in range(6):
+    if i < 3:
+        model = 'standard'
+        data_test_path = f'/storage/silver/scenario/si818415/og_monc/{2 * (i % 3)}00m/arm_18000.nc'
     else:
-        options_database = None
-    return options_database
+        model = "new param"
+        data_test_path = f'/storage/silver/scenario/si818415/altered_MONC/{2*(i%3)}00m/arm_18000.nc'
 
-od = options_database(data_test)
-attrs = data_test.attrs
+    data_test = xr.open_dataset(data_test_path)
 
-# 1st priority: pull from options_database, if present
-if type(od) is dict:
-    dx = float(od['dxx'])
-    dy = float(od['dyy'])
-# 2nd priority: pull from dataset attributes
-elif ('dx' in attrs and 'dy' in attrs):
-    dx = attrs['dx']
-    dy = attrs['dy']
+    def _bytarr_to_dict(d):
 
-print('dx = ', dx)
+        # Converted for xarray use
+
+        while len(np.shape(d))>2:
+            d = d[0]
+        res = {}
+        for i in range(np.shape(d)[0]):
+            opt = d[i,0].decode('utf-8')
+            val = d[i,1].decode('utf-8')
+
+            res[opt] = val
+        return res
+
+    def options_database(source_dataset):
+
+        if 'options_database' in source_dataset.variables:
+            options_database = _bytarr_to_dict(
+                source_dataset['options_database'].values)
+        else:
+            options_database = None
+        return options_database
+
+    od = options_database(data_test)
+    attrs = data_test.attrs
+
+    # 1st priority: pull from options_database, if present
+    if type(od) is dict:
+        dx = float(od['dxx'])
+        dy = float(od['dyy'])
+    # 2nd priority: pull from dataset attributes
+    elif ('dx' in attrs and 'dy' in attrs):
+        dx = attrs['dx']
+        dy = attrs['dy']
+
+    print(f'{model} {2*(i%3)}00m dx = ', dx)
