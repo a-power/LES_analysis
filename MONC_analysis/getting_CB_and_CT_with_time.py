f86749f28fe0859602a8c78be07600ca8dd3cd56
a-power <a.power@pgr.reading.ac.uk>
2024-07-18 13:45:23 +0100

    plot for SA

--- MONC_analysis/getting_CB_and_CT_with_time.py
+++ MONC_analysis/getting_CB_and_CT_with_time.py
@@ -112,7 +112,7 @@ plt.xticks(og_xtic[0], time_label)
 
 plt.xlabel('Local time (hh:mm:ss)', fontsize=14)
 plt.ylabel('z (m)', fontsize=14)
-plt.title('Cloud top and base height', fontsize=14)
+plt.title('Cloud top and base height for HCsSA', fontsize=14)
 
 plt.tight_layout()
 
