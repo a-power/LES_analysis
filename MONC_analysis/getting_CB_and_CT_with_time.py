c8fbf3d0a7927f90ef2ca4467b4e36b2e75673cd
a-power <a.power@pgr.reading.ac.uk>
2024-07-29 18:16:20 +0100

    making all cloud top and base calcs the same: issues with the C_th_L run not finishing

--- MONC_analysis/getting_CB_and_CT_with_time.py
+++ MONC_analysis/getting_CB_and_CT_with_time.py
@@ -317,6 +317,7 @@ elif plot_choice == 'all_Cs_at_D_200':
     for i in range(6):
         # plt.plot(list_timestamps, CT_mean_height_ts[i,:], colour_cycle[i%3], linestyle=line_list[i], label=model_param[i]+f'{2 ** ((i+1) % 8)}$\\Delta$')
         if i == 0:
+            print(f'S$C_s$0.23:, len(plot_ref_tstamps) = {len(plot_ref_tstamps)}, and len(CB_mean_height_ts = {CB_mean_height_ts[i, :]}')
             plt.plot(plot_ref_tstamps, CB_mean_height_ts[i, :], colour_cycle[0], linestyle=':', marker='*')
             plt.plot(plot_ref_tstamps, CT_mean_height_ts[i, :], colour_cycle[0], linestyle=':', marker='*',
                      label='S$C_s$0.23') #f'$\\Delta$ = {2 ** ((i-3))}00m'
@@ -338,8 +339,8 @@ elif plot_choice == 'all_Cs_at_D_200':
                      label='H$C_s$SA')
 
         elif i == 5:
-            plt.plot(list_timestamps, CB_mean_height_ts[i, :], colour_cycle[5], linestyle=':', marker='^')
-            plt.plot(list_timestamps, CT_mean_height_ts[i, :], colour_cycle[5], linestyle=':', marker='^',
+            plt.plot(list_timestamps, CB_mean_height_ts[i, :len(list_timestamps)], colour_cycle[5], linestyle=':', marker='^')
+            plt.plot(list_timestamps, CT_mean_height_ts[i, :len(list_timestamps)], colour_cycle[5], linestyle=':', marker='^',
                      label='H$C_sC_{\theta_L}$')
 
     plt.tight_layout(pad=0.5)
