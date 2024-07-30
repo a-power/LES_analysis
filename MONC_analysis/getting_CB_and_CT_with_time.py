8c877fb487a71d370df533f8a6e4a37c68595431
a-power <a.power@pgr.reading.ac.uk>
2024-07-30 17:27:41 +0100

    plotting the 400m data - making more general

--- MONC_analysis/getting_CB_and_CT_with_time.py
+++ MONC_analysis/getting_CB_and_CT_with_time.py
@@ -348,18 +348,18 @@ elif plot_choice == 'all_Cs_at_D_200':
             plt.plot(plot_ref_tstamps, CB_mean_height_ts[i, :], colour_cycle[1], linestyle='--')#, marker='*')
             plt.plot(plot_ref_tstamps, CT_mean_height_ts[i, :], colour_cycle[1], linestyle='--',
                      label=f'Smag 0.137')
-        elif i == 2:
-            plt.plot(plot_ref_tstamps, CB_mean_height_ts[i, :], colour_cycle[2], linestyle='--')#, marker='*')
-            plt.plot(plot_ref_tstamps, CT_mean_height_ts[i, :], colour_cycle[2], linestyle='--',
-                     label=f'Smag {sa_smag}')
+        # elif i == 2:
+        #     plt.plot(plot_ref_tstamps, CB_mean_height_ts[i, :], colour_cycle[2], linestyle='--')#, marker='*')
+        #     plt.plot(plot_ref_tstamps, CT_mean_height_ts[i, :], colour_cycle[2], linestyle='--',
+        #              label=f'Smag {sa_smag}')
         elif i == 3:
             plt.plot(plot_ref_tstamps, CB_mean_height_ts[i, :], colour_cycle[3]) #, linestyle='--')#, linewidth=2)
             plt.plot(plot_ref_tstamps, CT_mean_height_ts[i, :], colour_cycle[3],
                      label='$C_s$ prof')
-        elif i == 4:
-            plt.plot(plot_ref_tstamps, CB_mean_height_ts[i, :], colour_cycle[4]) #, linestyle='--')#, marker='x')
-            plt.plot(plot_ref_tstamps, CT_mean_height_ts[i, :], colour_cycle[4],
-                     label='S-A $C_s$ prof')
+        # elif i == 4:
+        #     plt.plot(plot_ref_tstamps, CB_mean_height_ts[i, :], colour_cycle[4]) #, linestyle='--')#, marker='x')
+        #     plt.plot(plot_ref_tstamps, CT_mean_height_ts[i, :], colour_cycle[4],
+        #              label='S-A $C_s$ prof')
         # elif i == 5:
         #     plt.plot(list_timestamps, CB_mean_height_ts[i, :len(list_timestamps)], colour_cycle[5]) #, linestyle='--')#, marker='^')
         #     plt.plot(list_timestamps, CT_mean_height_ts[i, :len(list_timestamps)], colour_cycle[5],
@@ -393,7 +393,7 @@ elif plot_choice == 'all_Cs_at_D_200':
 
     plt.tight_layout()
 
-    save_name = f'ARM_cloud_top_and_base_ts_D_{res_in_m}_all_Cs_cases'
+    save_name = f'ARM_cloud_top_and_base_ts_D_{res_in_m}_Cs0_23_Cs0_137_vs_HCs_cases'
         # f'ARM_cloud_top_and_base_ts_D_{res_in_m}_Cs0_23_Cs_sa_vs_HCsSA_cases'
         # f'ARM_cloud_top_and_base_ts_D_{res_in_m}_Cs0_23_Cs0_137_vs_HCs_cases'
         # f'ARM_cloud_top_and_base_ts_D_{res_in_m}_all_Cs_cases'
