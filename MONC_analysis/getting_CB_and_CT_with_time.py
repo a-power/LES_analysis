51d27c7fc213bada5ad32652c04cec0235b4177b
a-power <a.power@pgr.reading.ac.uk>
2024-07-30 12:08:34 +0100

    cleaning up the plot

--- MONC_analysis/getting_CB_and_CT_with_time.py
+++ MONC_analysis/getting_CB_and_CT_with_time.py
@@ -318,29 +318,29 @@ elif plot_choice == 'all_Cs_at_D_200':
         # plt.plot(list_timestamps, CT_mean_height_ts[i,:], colour_cycle[i%3], linestyle=line_list[i], label=model_param[i]+f'{2 ** ((i+1) % 8)}$\\Delta$')
         if i == 0:
             print(f'S$C_s$0.23:, len(plot_ref_tstamps) = {len(plot_ref_tstamps)}, and len(CB_mean_height_ts = {CB_mean_height_ts[i, :]}')
-            plt.plot(plot_ref_tstamps, CB_mean_height_ts[i, :], colour_cycle[0], linestyle=':', marker='*')
-            plt.plot(plot_ref_tstamps, CT_mean_height_ts[i, :], colour_cycle[0], linestyle=':', marker='*',
+            plt.plot(plot_ref_tstamps, CB_mean_height_ts[i, :], colour_cycle[0], linestyle=':')#, marker='*')
+            plt.plot(plot_ref_tstamps, CT_mean_height_ts[i, :], colour_cycle[0], linestyle=':',
                      label='S$C_s$0.23') #f'$\\Delta$ = {2 ** ((i-3))}00m'
         elif i == 1:
-            plt.plot(plot_ref_tstamps, CB_mean_height_ts[i, :], colour_cycle[1], linestyle=':', marker='*')
-            plt.plot(plot_ref_tstamps, CT_mean_height_ts[i, :], colour_cycle[1], linestyle=':', marker='*',
+            plt.plot(plot_ref_tstamps, CB_mean_height_ts[i, :], colour_cycle[1], linestyle=':')#, marker='*')
+            plt.plot(plot_ref_tstamps, CT_mean_height_ts[i, :], colour_cycle[1], linestyle=':',
                      label='S$C_s$0.137')
         elif i == 2:
-            plt.plot(plot_ref_tstamps, CB_mean_height_ts[i, :], colour_cycle[2], linestyle=':', marker='*')
-            plt.plot(plot_ref_tstamps, CT_mean_height_ts[i, :], colour_cycle[2], linestyle=':', marker='*',
+            plt.plot(plot_ref_tstamps, CB_mean_height_ts[i, :], colour_cycle[2], linestyle=':')#, marker='*')
+            plt.plot(plot_ref_tstamps, CT_mean_height_ts[i, :], colour_cycle[2], linestyle=':',
                      label='S$C_s$0.11')
         elif i == 3:
-            plt.plot(plot_ref_tstamps, CB_mean_height_ts[i, :], colour_cycle[3], linewidth=2)
-            plt.plot(plot_ref_tstamps, CT_mean_height_ts[i, :], colour_cycle[3], linewidth=2,
+            plt.plot(plot_ref_tstamps, CB_mean_height_ts[i, :], colour_cycle[3], linestyle='--')#, linewidth=2)
+            plt.plot(plot_ref_tstamps, CT_mean_height_ts[i, :], colour_cycle[3], linestyle='--',
                      label='H$C_s$')
         elif i == 4:
-            plt.plot(plot_ref_tstamps, CB_mean_height_ts[i, :], colour_cycle[4], linestyle='--', marker='x')
-            plt.plot(plot_ref_tstamps, CT_mean_height_ts[i, :], colour_cycle[4], linestyle='--', marker='x',
+            plt.plot(plot_ref_tstamps, CB_mean_height_ts[i, :], colour_cycle[4], linestyle='--')#, marker='x')
+            plt.plot(plot_ref_tstamps, CT_mean_height_ts[i, :], colour_cycle[4], linestyle='--',
                      label='H$C_s$SA')
 
         elif i == 5:
-            plt.plot(list_timestamps, CB_mean_height_ts[i, :len(list_timestamps)], colour_cycle[5], linestyle=':', marker='^')
-            plt.plot(list_timestamps, CT_mean_height_ts[i, :len(list_timestamps)], colour_cycle[5], linestyle=':', marker='^',
+            plt.plot(list_timestamps, CB_mean_height_ts[i, :len(list_timestamps)], colour_cycle[5], linestyle='--')#, marker='^')
+            plt.plot(list_timestamps, CT_mean_height_ts[i, :len(list_timestamps)], colour_cycle[5], linestyle='--',
                      label='H$C_sC_{\theta_L}$')
 
     plt.tight_layout(pad=0.5)
