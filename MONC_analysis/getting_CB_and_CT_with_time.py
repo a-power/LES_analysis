7aad9f3f84a3d5d90eebd021f68cb3502a970d1c
a-power <a.power@pgr.reading.ac.uk>
2024-07-31 14:50:15 +0100

    trying savgol filter

--- MONC_analysis/getting_CB_and_CT_with_time.py
+++ MONC_analysis/getting_CB_and_CT_with_time.py
@@ -82,8 +82,10 @@ def get_CT_and_CB(ts_of_cloud_frac_prof, len_ts, len_zn_in):
     CT_nan_index = np.argwhere(np.isnan(CT_ref_25m))
     CB_nan_index = np.argwhere(np.isnan(CB_ref_25m))
 
-    CT_nan_end = CT_nan_index[-1, 1]
-    CB_nan_end = CB_nan_index[-1, 1]
+    print('shape of CT_nan_index and CB_nan_index is ', np.shape(CT_nan_index), ' and ', np.shape(CB_nan_index))
+
+    CT_nan_end = CT_nan_index[-1, 0]
+    CB_nan_end = CB_nan_index[-1, 0]
 
     CT_ref_25m[:CT_nan_end] = savgol_filter((CT_ref_25m[CT_nan_end:]), 5, 3)
     CB_ref_25m[:CB_nan_end] = savgol_filter((CB_ref_25m[CB_nan_end:]), 5, 3)
