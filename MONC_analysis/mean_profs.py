8fd5247739876f2052fccb4d5141554609b2f9f3
a-power <a.power@pgr.reading.ac.uk>
2024-08-01 13:33:17 +0100

    SAHCs vs HCs

--- MONC_analysis/mean_profs.py
+++ MONC_analysis/mean_profs.py
@@ -13,6 +13,9 @@ def get_cloud_only(CT_or_CB_field, dist_from_surf_threas=1):
     return mask_no_cloud
 
 
+plotting = 'HCs_vs_SAHCs' # 'og_vs_HCs 'HCs_vs_SAHCs'
+
+
 
 path_ARM25 = '/storage/silver/greybls/si818415/arm_2d_25m/diagnostics_ts_'
 path_MONC_alt_HCs = '/storage/silver/scenario/si818415/altered_MONC/'
@@ -51,102 +54,206 @@ model_param = ['Smag 0.23', 'Smag 0.137', 'Smag 0.11', 'Smag 0.075',
 
 
 
+if plotting == 'og_vs_Hs':
+
+    for nv, var in enumerate(var_list):
+        print(var)
+        for nt, time in enumerate(list_timestamps):
+
+            clock_time_int = 05.30 + int(time) / (60 * 60)
+            clock_time = str(clock_time_int) + '0L'
+
+            var_prof = np.zeros( (7, len(zn) ) )
+            var_prof_40 = np.zeros((7, len(zn_40)))
+            var_prof_440 = np.zeros((7, len(zn_440)))
+
+            for n in range(7):
+                print(n)
+                if n <3:
+                    path_in = path_MONC_stand + f'{2 ** (n)}00m/'
+                    if n==2:
+                        filein = f'arm_{str(time)}.nc'
+                        ds_in = xr.open_dataset(path_in + filein)
+                        var_prof_40[n, :] = np.mean(ds_in[f'{var}'].data, axis=0)
+                    else:
+                        filein = f'arm_{str(time)}.nc'
+                        ds_in = xr.open_dataset(path_in + filein)
+                        var_prof[n, :] = np.mean(ds_in[f'{var}'].data, axis = 0)
 
 
-for nv, var in enumerate(var_list):
-    print(var)
-    for nt, time in enumerate(list_timestamps):
+                elif n<6:
 
-        clock_time_int = 05.30 + int(time) / (60 * 60)
-        clock_time = str(clock_time_int) + '0L'
-
-        var_prof = np.zeros( (7, len(zn) ) )
-        var_prof_40 = np.zeros((7, len(zn_40)))
-        var_prof_440 = np.zeros((7, len(zn_440)))
-
-        for n in range(7):
-            print(n)
-            if n <3:
-                path_in = path_MONC_stand + f'{2 ** (n)}00m/'
-                if n==2:
+                    path_in = path_MONC_alt_HCs + f'{2 ** (n - 3)}00m/'
                     filein = f'arm_{str(time)}.nc'
+
                     ds_in = xr.open_dataset(path_in + filein)
-                    var_prof_40[n, :] = np.mean(ds_in[f'{var}'].data, axis=0)
+                    if n == 4:
+                        var_prof_440[n, :] = np.mean(ds_in[f'{var}'].data, axis=0)
+                    else:
+                        var_prof[n, :] = np.mean(ds_in[f'{var}'].data, axis = 0)
+
                 else:
-                    filein = f'arm_{str(time)}.nc'
+                    path_in = path_ARM25
+                    filein = f'{str(time)}.nc'
+
                     ds_in = xr.open_dataset(path_in + filein)
-                    var_prof[n, :] = np.mean(ds_in[f'{var}'].data, axis = 0)
+                    var_prof[6, :] = np.mean(ds_in[f'{var}'].data, axis=0)
 
 
-            elif n<6:
 
-                path_in = path_MONC_alt_HCs + f'{2 ** (n - 3)}00m/'
-                filein = f'arm_{str(time)}.nc'
+            plt.plot(figsize=(5, 8))
 
-                ds_in = xr.open_dataset(path_in + filein)
-                if n == 4:
-                    var_prof_440[n, :] = np.mean(ds_in[f'{var}'].data, axis=0)
+            plt.plot(var_prof[6, :], zn, 'k', linewidth=2,
+                     label='LES $\\Delta$ = 25m')
+
+            for i in range(6):
+                # plt.plot(list_timestamps, CT_mean_height_ts[i,:], colour_cycle[i%3], linestyle=line_list[i], label=model_param[i]+f'{2 ** ((i+1) % 8)}$\\Delta$')
+                if i < 3:
+                    if i == 2:
+                        plt.plot(var_prof_40[i, :], zn_40, colour_cycle[i % 3], linestyle=':')
+                    else:
+                        plt.plot(var_prof[i, :], zn, colour_cycle[i % 3], linestyle=':')
                 else:
-                    var_prof[n, :] = np.mean(ds_in[f'{var}'].data, axis = 0)
-
-            else:
-                path_in = path_ARM25
-                filein = f'{str(time)}.nc'
-
-                ds_in = xr.open_dataset(path_in + filein)
-                var_prof[6, :] = np.mean(ds_in[f'{var}'].data, axis=0)
-
-
-
-        plt.plot(figsize=(5, 8))
-
-        plt.plot(var_prof[6, :], zn, 'k', linewidth=2,
-                 label='LES $\\Delta$ = 25m')
-
-        for i in range(6):
-            # plt.plot(list_timestamps, CT_mean_height_ts[i,:], colour_cycle[i%3], linestyle=line_list[i], label=model_param[i]+f'{2 ** ((i+1) % 8)}$\\Delta$')
-            if i < 3:
-                if i == 2:
-                    plt.plot(var_prof_40[i, :], zn_40, colour_cycle[i % 3], linestyle=':')
-                else:
-                    plt.plot(var_prof[i, :], zn, colour_cycle[i % 3], linestyle=':')
-            else:
-                if i == 4:
-                    plt.plot(var_prof_440[i, :], zn_440, colour_cycle[i % 3],
+                    if i == 4:
+                        plt.plot(var_prof_440[i, :], zn_440, colour_cycle[i % 3],
+                                 label='$\\Delta$'+f' = {(2**i)}00m')
+                            # marker='*')
+                    else:
+                        plt.plot(var_prof[i, :], zn, colour_cycle[i % 3],
                              label='$\\Delta$'+f' = {(2**i)}00m')
-                        # marker='*')
+                            # marker='*')
+
+            plt.title(f'{clock_time}: Smag 0.23 (dotted) vs '+'$C_s$ prof (solid)')
+            plt.tight_layout(pad=0.5)
+            plt.gcf().set_size_inches(5.5, 7)
+            plt.legend(fontsize=13, loc='upper right')
+
+            bottom, top = plt.ylim()
+            # plt.ylim(bottom=0, top=4000)
+
+            # og_xtic = plt.xticks()
+            # print(og_xtic)
+            #
+            # # time_label_temp = "%.2f"%(05.50 + og_xtic[0]/(60*60))
+            # # time_label_temp_min = (( np.round(05.50 + og_xtic[0]/(60*60), 2 ) - "%.2f"%(05.50 + og_xtic[0]/(60*60)) )*60 )/100
+            # # time_label = np.round(time_label_temp + time_label_temp_min, 2)
+            #
+            # time_label = []
+            #
+            # for i in range(len(og_xtic[0])):
+            #     time_label.append(datetime.timedelta(seconds=og_xtic[0][i] + 19800))
+            #
+            # plt.xticks(og_xtic[0], time_label)
+
+            plt.xlabel(f'{var}', fontsize=14)
+            plt.ylabel('z (m)', fontsize=14)
+
+            plt.tight_layout()
+
+            plt.savefig(plotdir + f'ARM_{var}_{time}_mean_prof_HCs_vs_stand.png', bbox_inches='tight')
+            plt.savefig(plotdir + f'ARM_{var}_{time}_mean_prof_HCs_vs_stand.pdf', bbox_inches='tight')
+            plt.close()
+
+
+
+
+elif plotting == 'HCs_vs_SAHCs':
+
+    for nv, var in enumerate(var_list):
+        print(var)
+        for nt, time in enumerate(list_timestamps):
+
+            clock_time_int = 05.30 + int(time) / (60 * 60)
+            clock_time = str(clock_time_int) + '0L'
+
+            var_prof = np.zeros((7, len(zn)))
+            var_prof_40 = np.zeros((7, len(zn_40)))
+            var_prof_440 = np.zeros((7, len(zn_440)))
+
+            for n in range(7):
+                print(n)
+                if n < 3:
+                    path_in = path_MONC_alt_HCs + f'SA/{2 ** (n)}00m/'
+
+                    filein = f'arm_{str(time)}.nc'
+                    ds_in = xr.open_dataset(path_in + filein)
+                    var_prof[n, :] = np.mean(ds_in[f'{var}'].data, axis=0)
+
+
+                elif n < 6:
+
+                    path_in = path_MONC_alt_HCs + f'{2 ** (n - 3)}00m/'
+                    filein = f'arm_{str(time)}.nc'
+
+                    ds_in = xr.open_dataset(path_in + filein)
+                    if n == 4:
+                        var_prof_440[n, :] = np.mean(ds_in[f'{var}'].data, axis=0)
+                    else:
+                        var_prof[n, :] = np.mean(ds_in[f'{var}'].data, axis=0)
+
                 else:
-                    plt.plot(var_prof[i, :], zn, colour_cycle[i % 3],
-                         label='$\\Delta$'+f' = {(2**i)}00m')
+                    path_in = path_ARM25
+                    filein = f'{str(time)}.nc'
+
+                    ds_in = xr.open_dataset(path_in + filein)
+                    var_prof[6, :] = np.mean(ds_in[f'{var}'].data, axis=0)
+
+            plt.plot(figsize=(5, 8))
+
+            plt.plot(var_prof[6, :], zn, 'k', linewidth=2,
+                     label='LES $\\Delta$ = 25m')
+
+            for i in range(6):
+                # plt.plot(list_timestamps, CT_mean_height_ts[i,:], colour_cycle[i%3], linestyle=line_list[i], label=model_param[i]+f'{2 ** ((i+1) % 8)}$\\Delta$')
+                if i < 3:
+                    plt.plot(var_prof[i, :], zn, colour_cycle[i % 3], linestyle='--')
+                else:
+                    if i == 4:
+                        plt.plot(var_prof_440[i, :], zn_440, colour_cycle[i % 3],
+                                 label='$\\Delta$' + f' = {(2 ** i)}00m')
+                        # marker='*')
+                    else:
+                        plt.plot(var_prof[i, :], zn, colour_cycle[i % 3],
+                                 label='$\\Delta$' + f' = {(2 ** i)}00m')
                         # marker='*')
 
-        plt.title(f'{clock_time}: Smag 0.23 (dotted) vs '+'$C_s$ prof (solid)')
-        plt.tight_layout(pad=0.5)
-        plt.gcf().set_size_inches(5.5, 7)
-        plt.legend(fontsize=13, loc='upper right')
+            plt.title(f'{clock_time}: Scale aware $C_s$ prof (dashed) vs ' + '$C_s$ prof (solid)')
+            plt.tight_layout(pad=0.5)
+            plt.gcf().set_size_inches(5.5, 7)
+            plt.legend(fontsize=13, loc='upper right')
 
-        bottom, top = plt.ylim()
-        # plt.ylim(bottom=0, top=4000)
+            bottom, top = plt.ylim()
+            # plt.ylim(bottom=0, top=4000)
 
-        # og_xtic = plt.xticks()
-        # print(og_xtic)
-        #
-        # # time_label_temp = "%.2f"%(05.50 + og_xtic[0]/(60*60))
-        # # time_label_temp_min = (( np.round(05.50 + og_xtic[0]/(60*60), 2 ) - "%.2f"%(05.50 + og_xtic[0]/(60*60)) )*60 )/100
-        # # time_label = np.round(time_label_temp + time_label_temp_min, 2)
-        #
-        # time_label = []
-        #
-        # for i in range(len(og_xtic[0])):
-        #     time_label.append(datetime.timedelta(seconds=og_xtic[0][i] + 19800))
-        #
-        # plt.xticks(og_xtic[0], time_label)
+            # og_xtic = plt.xticks()
+            # print(og_xtic)
+            #
+            # # time_label_temp = "%.2f"%(05.50 + og_xtic[0]/(60*60))
+            # # time_label_temp_min = (( np.round(05.50 + og_xtic[0]/(60*60), 2 ) - "%.2f"%(05.50 + og_xtic[0]/(60*60)) )*60 )/100
+            # # time_label = np.round(time_label_temp + time_label_temp_min, 2)
+            #
+            # time_label = []
+            #
+            # for i in range(len(og_xtic[0])):
+            #     time_label.append(datetime.timedelta(seconds=og_xtic[0][i] + 19800))
+            #
+            # plt.xticks(og_xtic[0], time_label)
 
-        plt.xlabel(f'{var}', fontsize=14)
-        plt.ylabel('z (m)', fontsize=14)
+            plt.xlabel(f'{var}', fontsize=14)
+            plt.ylabel('z (m)', fontsize=14)
 
-        plt.tight_layout()
+            plt.tight_layout()
 
-        plt.savefig(plotdir + f'ARM_{var}_{time}_mean_prof_HCs_vs_stand.png', bbox_inches='tight')
-        plt.savefig(plotdir + f'ARM_{var}_{time}_mean_prof_HCs_vs_stand.pdf', bbox_inches='tight')
-        plt.close()
+            plt.savefig(plotdir + f'ARM_{var}_{time}_mean_prof_HCs_vs_SAHCs.png', bbox_inches='tight')
+            plt.savefig(plotdir + f'ARM_{var}_{time}_mean_prof_HCs_vs_SAHCs.pdf', bbox_inches='tight')
+            plt.close()
+
+
+
+
+
+
+
+
+
+
