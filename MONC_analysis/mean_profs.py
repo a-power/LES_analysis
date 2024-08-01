2a27e8c15fc6752a0a9850291345faba204526a9
a-power <a.power@pgr.reading.ac.uk>
2024-08-01 12:52:20 +0100

    adding 25m to mean profs - prettier

--- MONC_analysis/mean_profs.py
+++ MONC_analysis/mean_profs.py
@@ -104,17 +104,17 @@ for nv, var in enumerate(var_list):
             # plt.plot(list_timestamps, CT_mean_height_ts[i,:], colour_cycle[i%3], linestyle=line_list[i], label=model_param[i]+f'{2 ** ((i+1) % 8)}$\\Delta$')
             if i < 3:
                 if i == 2:
-                    plt.plot(var_prof_40[i, :], zn_40, colour_cycle[i % 3], linestyle=':',
-                             label='$\\Delta$'+f' = {(2**i)}00m')
+                    plt.plot(var_prof_40[i, :], zn_40, colour_cycle[i % 3], linestyle=':')
                 else:
-                    plt.plot(var_prof[i, :], zn, colour_cycle[i % 3], linestyle=':',
-                         label='$\\Delta$'+f' = {(2**i)}00m')
+                    plt.plot(var_prof[i, :], zn, colour_cycle[i % 3], linestyle=':')
             else:
                 if i == 4:
-                    plt.plot(var_prof_440[i, :], zn_440, colour_cycle[i % 3])
+                    plt.plot(var_prof_440[i, :], zn_440, colour_cycle[i % 3],
+                             label='$\\Delta$'+f' = {(2**i)}00m')
                         # marker='*')
                 else:
-                    plt.plot(var_prof[i, :], zn, colour_cycle[i % 3])
+                    plt.plot(var_prof[i, :], zn, colour_cycle[i % 3],
+                         label='$\\Delta$'+f' = {(2**i)}00m')
                         # marker='*')
         plt.tight_layout(pad=0.5)
         plt.gcf().set_size_inches(5, 7)
