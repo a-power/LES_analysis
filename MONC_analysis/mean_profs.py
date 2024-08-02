ab7256c23ed3abcf6262fec94c9afb28c7ecac1d
a-power <a.power@pgr.reading.ac.uk>
2024-08-02 18:05:50 +0100

    SAHCs_vs_SA_Smag

--- MONC_analysis/mean_profs.py
+++ MONC_analysis/mean_profs.py
@@ -275,7 +275,10 @@ elif plotting == 'SAHCs_vs_SA_Smag':
             for i in range(6):
                 # plt.plot(list_timestamps, CT_mean_height_ts[i,:], colour_cycle[i%3], linestyle=line_list[i], label=model_param[i]+f'{2 ** ((i+1) % 8)}$\\Delta$')
                 if i < 3:
-                    plt.plot(var_prof[i, :], zn, colour_cycle[i % 3], linestyle='-.')
+                    if i == 2:
+                        plt.plot(var_prof_40[i, :], zn_40, colour_cycle[i % 3], linestyle='-.')
+                    else:
+                        plt.plot(var_prof[i, :], zn, colour_cycle[i % 3], linestyle='-.')
                 else:
                     if i == 4:
                         plt.plot(var_prof_440[i, :], zn_440, colour_cycle[i % 3], linestyle='--',
