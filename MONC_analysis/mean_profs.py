409fbcc21cf79e4511a64940244eae9842e2da2f
a-power <a.power@pgr.reading.ac.uk>
2024-08-02 17:17:54 +0100

    fixing label

--- MONC_analysis/mean_profs.py
+++ MONC_analysis/mean_profs.py
@@ -13,7 +13,7 @@ def get_cloud_only(CT_or_CB_field, dist_from_surf_threas=1):
     return mask_no_cloud
 
 
-plotting = 'HCs_vs_SAHCs' # 'og_vs_HCs 'HCs_vs_SAHCs'
+plotting = 'HCs_vs_SAHCs' # 'og_vs_HCs 'HCs_vs_SAHCs' 'SAHCs_vs_SA_Smag'
 
 
 
@@ -30,11 +30,11 @@ list_timestamps = [18000, 25200, 32400, 39600]
 
 var_list = ['wtheta_cn_mean', 'wtheta_ad_mean', 'wtsg_mean',
             'wqv_cn_mean', 'wqv_ad_mean', 'wqv_sg_mean', 'w_qt',
-            'ww_mean', 'wwsg_mean',
-            'theta_mean', 'total_cloud_fraction',
-            'viscosity_coef_mean', 'diffusion_coef_mean', 'dissipation_mean',
-            'resolved_buoyant_production', 'resolved_shear_production', 'resolved_turbulent_transport',
-            'subgrid_buoyant_production', 'subgrid_shear_stress', 'subgrid_turbulent_transport']
+            'ww_mean', 'wwsg_mean', 'qt_qt', 'sqt_qt',
+            'theta_mean', 'total_cloud_fraction', 'tkesg_mean',
+            'viscosity_coef_mean', 'diffusion_coef_mean', 'dissipation_mean'] #,
+            # 'resolved_buoyant_production', 'resolved_shear_production', 'resolved_turbulent_transport'] #,
+            # 'subgrid_buoyant_production', 'subgrid_shear_stress', 'subgrid_turbulent_transport']
 
 # 'tke_tendency', 'tkesg_mean'
 
@@ -251,7 +251,99 @@ elif plotting == 'HCs_vs_SAHCs':
             plt.savefig(plotdir + f'ARM_{var}_{time}_mean_prof_HCs_vs_SAHCs.pdf', bbox_inches='tight')
             plt.close()
 
+elif plotting == 'SAHCs_vs_SA_Smag':
 
+    for nv, var in enumerate(var_list):
+        print(var)
+        for nt, time in enumerate(list_timestamps):
+
+            clock_time_int = 05.30 + int(time) / (60 * 60)
+            clock_time = str(clock_time_int) + '0L'
+
+            var_prof = np.zeros((7, len(zn)))
+            var_prof_40 = np.zeros((7, len(zn_40)))
+            var_prof_440 = np.zeros((7, len(zn_440)))
+
+            for n in range(7):
+                print(n)
+                if n < 3:
+                    if n == 0:
+                        path_in = path_MONC_alt_HCs + f'/{2 ** (n)}00m/'
+                    else:
+                        path_in = path_MONC_alt_HCs + f'/{2 ** (n)}00m/SA/'
+
+                    filein = f'arm_{str(time)}.nc'
+                    ds_in = xr.open_dataset(path_in + filein)
+                    var_prof[n, :] = np.mean(ds_in[f'{var}'].data, axis=0)
+
+
+                elif n < 6:
+
+                    path_in = path_MONC_alt_HCs + f'{2 ** (n - 3)}00m/'
+                    filein = f'arm_{str(time)}.nc'
+
+                    ds_in = xr.open_dataset(path_in + filein)
+                    if n == 4:
+                        var_prof_440[n, :] = np.mean(ds_in[f'{var}'].data, axis=0)
+                    else:
+                        var_prof[n, :] = np.mean(ds_in[f'{var}'].data, axis=0)
+
+                else:
+                    path_in = path_ARM25
+                    filein = f'{str(time)}.nc'
+
+                    ds_in = xr.open_dataset(path_in + filein)
+                    var_prof[6, :] = np.mean(ds_in[f'{var}'].data, axis=0)
+
+            plt.plot(figsize=(5, 8))
+
+            plt.plot(var_prof[6, :], zn, 'k', linewidth=2,
+                     label='LES $\\Delta$ = 25m')
+
+            for i in range(6):
+                # plt.plot(list_timestamps, CT_mean_height_ts[i,:], colour_cycle[i%3], linestyle=line_list[i], label=model_param[i]+f'{2 ** ((i+1) % 8)}$\\Delta$')
+                if i < 3:
+                    plt.plot(var_prof[i, :], zn, colour_cycle[i % 3], linestyle='--')
+                else:
+                    if i == 4:
+                        plt.plot(var_prof_440[i, :], zn_440, colour_cycle[i % 3],
+                                 label='$\\Delta$' + f' = {(2 ** i)}00m')
+                        # marker='*')
+                    else:
+                        plt.plot(var_prof[i, :], zn, colour_cycle[i % 3],
+                                 label='$\\Delta$' + f' = {(2 ** i)}00m')
+                        # marker='*')
+
+            plt.title(f'{clock_time}: Scale aware $C_s$ prof (dashed) vs ' + '$C_s$ prof (solid)')
+            plt.tight_layout(pad=0.5)
+            plt.gcf().set_size_inches(5.5, 7)
+            plt.legend(fontsize=13, loc='upper right')
+
+            bottom, top = plt.ylim()
+            # plt.ylim(bottom=0, top=4000)
+
+            # og_xtic = plt.xticks()
+            # print(og_xtic)
+            #
+            # # time_label_temp = "%.2f"%(05.50 + og_xtic[0]/(60*60))
+            # # time_label_temp_min = (( np.round(05.50 + og_xtic[0]/(60*60), 2 ) - "%.2f"%(05.50 + og_xtic[0]/(60*60)) )*60 )/100
+            # # time_label = np.round(time_label_temp + time_label_temp_min, 2)
+            #
+            # time_label = []
+            #
+            # for i in range(len(og_xtic[0])):
+            #     time_label.append(datetime.timedelta(seconds=og_xtic[0][i] + 19800))
+            #
+            # plt.xticks(og_xtic[0], time_label)
+
+            plt.xlabel(f'{var}', fontsize=14)
+            plt.ylabel('z (m)', fontsize=14)
+
+            plt.tight_layout()
+
+            plt.savefig(plotdir + f'ARM_{var}_{time}_mean_prof_HCs_vs_SAHCs.png', bbox_inches='tight')
+            plt.savefig(plotdir + f'ARM_{var}_{time}_mean_prof_HCs_vs_SAHCs.pdf', bbox_inches='tight')
+            plt.close()
 
 
 
