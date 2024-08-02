33433b24073be85bc92768a16765d281a0781039
a-power <a.power@pgr.reading.ac.uk>
2024-08-02 15:27:14 +0100

    vlines at cloud initation ?????!

--- MONC_analysis/getting_CB_and_CT_with_time.py
+++ MONC_analysis/getting_CB_and_CT_with_time.py
@@ -359,7 +359,7 @@ elif plot_choice == 'all_Cs_at_D_200':
     for i in range(6):
         # plt.plot(list_timestamps, CT_mean_height_ts[i,:], colour_cycle[i%3], linestyle=line_list[i], label=model_param[i]+f'{2 ** ((i+1) % 8)}$\\Delta$')
         if i == 0:
-            print(f'S$C_s$0.23:, len(plot_ref_tstamps) = {len(plot_ref_tstamps)})
+            print(f'S$C_s$0.23:, len(plot_ref_tstamps) = {len(plot_ref_tstamps)}')
         #     plt.plot(plot_ref_tstamps, CB_mean_height_ts[i, :], colour_cycle[0], linestyle='--')#, marker='*')
         #     plt.plot(plot_ref_tstamps, CT_mean_height_ts[i, :], colour_cycle[0], linestyle='--',
         #              label='Smag 0.23') #f'$\\Delta$ = {2 ** ((i-3))}00m'
