a44899854d3c29101342de566cf6d908ce29f1fc
a-power <a.power@pgr.reading.ac.uk>
2024-08-02 15:20:39 +0100

    vlines at cloud initation

--- MONC_analysis/getting_CB_and_CT_with_time.py
+++ MONC_analysis/getting_CB_and_CT_with_time.py
@@ -363,32 +363,32 @@ elif plot_choice == 'all_Cs_at_D_200':
         #     plt.plot(plot_ref_tstamps, CB_mean_height_ts[i, :], colour_cycle[0], linestyle='--')#, marker='*')
         #     plt.plot(plot_ref_tstamps, CT_mean_height_ts[i, :], colour_cycle[0], linestyle='--',
         #              label='Smag 0.23') #f'$\\Delta$ = {2 ** ((i-3))}00m'
-        #     plt.vines(cloud_init[i], 250, 1500, colour_cycle[i], linestyle='--')
+        #     plt.vlines(cloud_init[i], 250, 1500, colour_cycle[i], linestyle='--')
         # elif i == 1:
         #     plt.plot(plot_ref_tstamps, CB_mean_height_ts[i, :], colour_cycle[1], linestyle='--')#, marker='*')
         #     plt.plot(plot_ref_tstamps, CT_mean_height_ts[i, :], colour_cycle[1], linestyle='--',
         #              label=f'Smag 0.137')
-        #     plt.vines(cloud_init[i], 250, 1500, colour_cycle[i], linestyle='--')
+        #     plt.vlines(cloud_init[i], 250, 1500, colour_cycle[i], linestyle='--')
         # elif i == 2:
         #     plt.plot(plot_ref_tstamps, CB_mean_height_ts[i, :], colour_cycle[2], linestyle='--')#, marker='*')
         #     plt.plot(plot_ref_tstamps, CT_mean_height_ts[i, :], colour_cycle[2], linestyle='--',
         #              label=f'Smag {sa_smag}')
-        #     plt.vines(cloud_init[i], 250, 1500, colour_cycle[i], linestyle='--')
+        #     plt.vlines(cloud_init[i], 250, 1500, colour_cycle[i], linestyle='--')
         elif i == 3:
             plt.plot(plot_ref_tstamps, CB_mean_height_ts[i, :], colour_cycle[3]) #, linestyle='--')#, linewidth=2)
             plt.plot(plot_ref_tstamps, CT_mean_height_ts[i, :], colour_cycle[3],
                      label='$C_s$ prof')
-            plt.vines(cloud_init[i], 250, 1500, colour_cycle[n])
+            plt.vlines(cloud_init[i], 250, 1500, colour_cycle[n])
         elif i == 4:
             plt.plot(plot_ref_tstamps, CB_mean_height_ts[i, :], colour_cycle[4]) #, linestyle='--')#, marker='x')
             plt.plot(plot_ref_tstamps, CT_mean_height_ts[i, :], colour_cycle[4],
                      label='S-A $C_s$ prof')
-            plt.vines(cloud_init[i], 250, 1500, colour_cycle[n])
+            plt.vlines(cloud_init[i], 250, 1500, colour_cycle[n])
         elif i == 5:
             plt.plot(plot_ref_tstamps, CB_mean_height_ts[i, :], colour_cycle[5]) #, linestyle='--')#, marker='^')
             plt.plot(plot_ref_tstamps, CT_mean_height_ts[i, :], colour_cycle[5],
                      label='$C_s C_{\\theta_L}$ profs')
-            plt.vines(cloud_init[i], 250, 1500, colour_cycle[n])
+            plt.vlines(cloud_init[i], 250, 1500, colour_cycle[n])
 
     plt.tight_layout(pad=0.5)
     plt.gcf().set_size_inches(10, 5.5)
