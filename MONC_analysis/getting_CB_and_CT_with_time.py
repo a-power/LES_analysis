01a31ea0e640223d516b18f42b145c2c43bb6c16
a-power <a.power@pgr.reading.ac.uk>
2024-08-02 13:42:27 +0100

    C_th

--- MONC_analysis/getting_CB_and_CT_with_time.py
+++ MONC_analysis/getting_CB_and_CT_with_time.py
@@ -377,8 +377,8 @@ elif plot_choice == 'all_Cs_at_D_200':
             plt.plot(plot_ref_tstamps, CT_mean_height_ts[i, :], colour_cycle[4],
                      label='S-A $C_s$ prof')
         elif i == 5:
-            plt.plot(list_timestamps, CB_mean_height_ts[i, :len(list_timestamps)], colour_cycle[5]) #, linestyle='--')#, marker='^')
-            plt.plot(list_timestamps, CT_mean_height_ts[i, :len(list_timestamps)], colour_cycle[5],
+            plt.plot(plot_ref_tstamps, CB_mean_height_ts[i, :], colour_cycle[5]) #, linestyle='--')#, marker='^')
+            plt.plot(plot_ref_tstamps, CT_mean_height_ts[i, :], colour_cycle[5],
                      label='$C_s C_{\\theta_L}$ profs')
 
     plt.tight_layout(pad=0.5)
