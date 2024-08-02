559de702d83e8e06d77a2602dba434053e05e4bc
a-power <a.power@pgr.reading.ac.uk>
2024-08-02 16:14:01 +0100

    vlines at cloud initation - 400

--- MONC_analysis/getting_CB_and_CT_with_time.py
+++ MONC_analysis/getting_CB_and_CT_with_time.py
@@ -7,7 +7,7 @@ import numpy.ma as ma
 import datetime
 
 plot_choice = 'all_Cs_at_D_200' #'og_HCs'  'HCs_HCsSA'  'all_Cs_at_D_200'
-res_in_m = '200'
+res_in_m = '400'
 
 def get_cloud_only(CT_or_CB_field, dist_from_surf_threas=10):
 
