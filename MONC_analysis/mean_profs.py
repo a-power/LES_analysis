1e84369802ca4947d7434536f8a44b7914eaa5fc
a-power <a.power@pgr.reading.ac.uk>
2024-08-07 01:18:09 +0100

    fixing SAHCs_vs_SA_Smag (with Cth_L S-A to be fiished)

--- MONC_analysis/mean_profs.py
+++ MONC_analysis/mean_profs.py
@@ -13,7 +13,7 @@ def get_cloud_only(CT_or_CB_field, dist_from_surf_threas=1):
     return mask_no_cloud
 
 
-plotting = 'SAHCs_vs_SA_Smag' # 'og_vs_HCs' 'HCs_vs_SAHCs' 'SAHCs_vs_SA_Smag'
+plotting = 'og_vs_HCs' # 'og_vs_HCs' 'HCs_vs_SAHCs' 'SAHCs_vs_SA_Smag'
 
 
 
