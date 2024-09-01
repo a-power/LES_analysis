7cf6f9dfb719fec0919e3a889e7115971a379534
a-power <a.power@pgr.reading.ac.uk>
2024-09-01 19:26:24 +0100

    ughhhh data overwrite

--- dynamic_script.py
+++ dynamic_script.py
@@ -609,9 +609,9 @@ def run_dyn_on_filtered(res_in, time_in, filt_in, filt_scale, indir, odir, opt, 
                                 ["v", "v"],
                                 ["v", "w"],
                                 ["w", "w"],
-                                ["u", "th"],
-                                ["v", "th"],
-                                ["w", "th"]
+                                ["u", theta],
+                                ["v", theta],
+                                ["w", theta]
                                 ]
 
                 elif c_th == 'th_e':
