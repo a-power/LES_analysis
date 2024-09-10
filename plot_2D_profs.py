c7ad6840097147ee1b8c49ad1ba28c154a0cc01e
alanna <a.power@pgr.reading.ac.uk>
2024-09-10 20:09:32 +0100

    trying to fix matplotlib being dumb with x-axis

--- plot_2D_profs.py
+++ plot_2D_profs.py
@@ -11,7 +11,7 @@ np.seterr(invalid='ignore')
 
 parser = argparse.ArgumentParser()
 #parser.add_argument('--times', type=str, default='28800')
-parser.add_argument('--case_in', type=str, default='BOMEX')
+parser.add_argument('--case_in', type=str, default='ARM')
 args = parser.parse_args()
 
 case = args.case_in
