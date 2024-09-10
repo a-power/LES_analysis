5c74a0fdbfaa55dfcd5dd3bc0b69b873adac21b3
alanna <a.power@pgr.reading.ac.uk>
2024-09-10 20:50:02 +0100

    trying to fix matplotlib being dumb with x-axis

--- plot_2D_profs.py
+++ plot_2D_profs.py
@@ -158,16 +158,17 @@ def plot_MONC_profs(file_path, field_path, times, time_stamp_in='mean'):
             clock_time = '4 hours'
 
         locA = plt.MultipleLocator(base=0.025)
-        locB = plt.MultipleLocator(base=0.005)
+        locB = plt.MultipleLocator(base=0.01)
 
         ax[0].plot(wth_prof[it,:], z[it, :] / z_i[it], color=colours[it], label = f'{clock_time},'+' z$_{ML}$ = ' + str(int(z_i[it])) + 'm')
         ax[0].set_xlabel("$ \\overline{w' \\theta}$ (K m s$^{-1}$)", fontsize=16)
         ax[0].set_ylabel("z/z$_{ML}$", fontsize=16)
         if case == 'ARM':
             ax[0].xaxis.set_major_locator(locA)
+            ax[0].set_xticks(ax[0].get_xticks()[::2])
         if case == 'BOMEX':
             ax[0].xaxis.set_major_locator(locB)
-        ax[0].set_xticks(ax[0].get_xticks()[::2])
+
 
         ax[3].plot(cloud_prof[it,:]*100, z[it, :] / z_i[it], color=colours[it], label = f'{clock_time},'+' z$_{ML}$ = ' + str(int(z_i[it])) + 'm')
         ax[3].set_xlabel('cloud cover (%)', fontsize=16)
