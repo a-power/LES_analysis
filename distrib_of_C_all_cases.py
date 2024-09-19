793f21c80914b5ba1f78dd29bc14dee0dc731bca
alanna <a.power@pgr.reading.ac.uk>
2024-09-19 19:14:14 +0100

    trying to force python to flushprint statements to output files

--- distrib_of_C_all_cases.py
+++ distrib_of_C_all_cases.py
@@ -6,10 +6,12 @@ import mask_cloud_vs_env as clo
 import matplotlib.ticker as ticker
 import numpy.ma as ma
 import dynamic_functions as dyn
-import sys
+import functools
 
-sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
+print = functools.partial(print, flush=True)
 
+
+print('test')
 
 
 
