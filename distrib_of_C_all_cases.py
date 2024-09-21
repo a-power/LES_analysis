876d48cf9ab125678bf73fe8056e05e9d034a444
a-power <a.power@pgr.reading.ac.uk>
2024-09-21 12:36:46 +0100

    trying to fix

--- distrib_of_C_all_cases.py
+++ distrib_of_C_all_cases.py
@@ -409,7 +409,7 @@ def plot_hist(plotdir_in, data1, data2, data3, data4, data5, region, bins_in=set
                      weights=np.ones(arm_horiz_domain*(A2[3]-A1[3])) / (arm_horiz_domain*(A2[3]-A1[3])), label='ARM 16:30L')
             ax[i,j].set_xlabel(f"{fields_latex_in[i]}", fontsize=16)
             ax[i,j].yaxis.set_major_formatter(ticker.PercentFormatter(1))
-            a[i,j].set_xlim(0, 0.5)
+            ax[i,j].set_xlim(0, 0.5)
             ax[0,j].set_title(f'{Deltas[j]}')
         ax[i,0].set_ylabel("Percentage of Occurrences", fontsize=16)
 
