19ac8f3d4ec06ba046761179882fb7eb72610047
a-power <a.power@pgr.reading.ac.uk>
2024-09-22 00:40:57 +0100

    trying to fix issue in CL, and scale by region not domain

--- distrib_of_C_all_cases.py
+++ distrib_of_C_all_cases.py
@@ -436,21 +436,20 @@ def plot_hist(plotdir_in, data1, data2, data3, data4, data5, region, bins_in=set
 
 
 
-
-            ax[i,j].hist(data1_masked[:,:,B1:B2].flatten(), bins=bins_in, histtype='step', stacked=False, color=colours[0],
-                     weights=np.ones(bomex_horiz_domain*(B2-B1)) / (num_unmasked1), label='BOMEX')
-            ax[i,j].hist(data2_masked[:,:,A1[0]:A2[0]].flatten(), bins=bins_in, histtype='step', stacked=False, color=colours[1],
-                     weights=np.ones(arm_horiz_domain*(A2[0]-A1[0])) / (num_unmasked2), label='ARM 10:30L')
-            ax[i,j].hist(data3_masked[:,:,A1[1]:A2[1]].flatten(), bins=bins_in, histtype='step', stacked=False, color=colours[2],
-                     weights=np.ones(arm_horiz_domain*(A2[1]-A1[1])) / (num_unmasked3), label='ARM 12:30L')
-            ax[i,j].hist(data4_masked[:,:,A1[2]:A2[2]].flatten(), bins=bins_in, histtype='step', stacked=False, color=colours[3],
-                     weights=np.ones(arm_horiz_domain*(A2[2]-A1[2])) / (num_unmasked4), label='ARM 14:30L')
-            ax[i,j].hist(data5_masked[:,:,A1[3]:A2[3]].flatten(), bins=bins_in, histtype='step', stacked=False, color=colours[4],
-                     weights=np.ones(arm_horiz_domain*(A2[3]-A1[3])) / (num_unmasked5), label='ARM 16:30L')
-            ax[i,j].set_xlabel(f"{fields_latex_in[i]}", fontsize=16)
-            ax[i,j].yaxis.set_major_formatter(ticker.PercentFormatter(1))
-            ax[i,j].set_xlim(0, 0.3)
-            ax[0,j].set_title(f'{region} '+'$\\widehat{\\bar{\\Delta}}$ = '+f'{Deltas[j]}', fontsize=14)
+                ax[i,j].hist(data1_masked[:,:,B1:B2].flatten(), bins=bins_in, histtype='step', stacked=False, color=colours[0],
+                         weights=np.ones(bomex_horiz_domain*(B2-B1)) / (num_unmasked1), label='BOMEX')
+                ax[i,j].hist(data2_masked[:,:,A1[0]:A2[0]].flatten(), bins=bins_in, histtype='step', stacked=False, color=colours[1],
+                         weights=np.ones(arm_horiz_domain*(A2[0]-A1[0])) / (num_unmasked2), label='ARM 10:30L')
+                ax[i,j].hist(data3_masked[:,:,A1[1]:A2[1]].flatten(), bins=bins_in, histtype='step', stacked=False, color=colours[2],
+                         weights=np.ones(arm_horiz_domain*(A2[1]-A1[1])) / (num_unmasked3), label='ARM 12:30L')
+                ax[i,j].hist(data4_masked[:,:,A1[2]:A2[2]].flatten(), bins=bins_in, histtype='step', stacked=False, color=colours[3],
+                         weights=np.ones(arm_horiz_domain*(A2[2]-A1[2])) / (num_unmasked4), label='ARM 14:30L')
+                ax[i,j].hist(data5_masked[:,:,A1[3]:A2[3]].flatten(), bins=bins_in, histtype='step', stacked=False, color=colours[4],
+                         weights=np.ones(arm_horiz_domain*(A2[3]-A1[3])) / (num_unmasked5), label='ARM 16:30L')
+                ax[i,j].set_xlabel(f"{fields_latex_in[i]}", fontsize=16)
+                ax[i,j].yaxis.set_major_formatter(ticker.PercentFormatter(1))
+                ax[i,j].set_xlim(0, 0.3)
+                ax[0,j].set_title(f'{region} '+'$\\widehat{\\bar{\\Delta}}$ = '+f'{Deltas[j]}', fontsize=14)
         ax[i,0].set_ylabel("Percentage of Occurrences", fontsize=14)
 
 
