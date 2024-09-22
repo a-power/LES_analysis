de017a21855e63068a435dd75a7298f7a94619c5
a-power <a.power@pgr.reading.ac.uk>
2024-09-22 01:21:09 +0100

    fixing the scale

--- neg_C_sq_profile_percentages.py
+++ neg_C_sq_profile_percentages.py
@@ -228,6 +228,7 @@ def negs_in_field(plotdir, field, c, c_latex, z, z_i, time_in, nt_in, data_field
     ax1.set_title(f"{labels_title[nt_in]}", fontsize=13)
     ax1.set_ylabel("$z/z_{ML}$ $z_{ML} = $"+f'{z_i}m', fontsize=13)
     ax1.set_xlabel(f"% of Negative {c_latex} Values", fontsize=13)
+    ax1.set_xlim(0,100)
     fig1.savefig(plotdir + f'percent_neg_{c}_vs_z_{time_in}.pdf', bbox_inches='tight')
     # ax1.clf()
 
@@ -239,6 +240,7 @@ def negs_in_field(plotdir, field, c, c_latex, z, z_i, time_in, nt_in, data_field
     ax3.set_title(f"{labels_title[nt_in]}", fontsize=13)
     ax3.set_ylabel("$z/z_{ML}$ $z_{ML} = $"+f'{z_i}m', fontsize=13)
     ax3.set_xlabel(f"% of Negative {c_latex}", fontsize=13)
+    ax3.set_xlim(0,100)
     fig3.savefig(plotdir + f'overall_percent_neg_{c}_vs_z_{time_in}.pdf', bbox_inches='tight')
     #ax2.clf()
 
