6dc7f5275560a9a22e81f4e6ee5050b044d59feb
a-power <a.power@pgr.reading.ac.uk>
2024-09-30 18:38:51 +0100

    trying to fix the damn plot size

--- MONC_analysis/mean_profs.py
+++ MONC_analysis/mean_profs.py
@@ -122,7 +122,7 @@ if plotting == 'og_vs_HCs':
 
 
 
-            plt.plot(figsize=(5,8))
+            plt.plot(figsize=(4,8))
 
             plt.plot(var_prof[6, :], zn/cloudtop25[nt], 'k', linewidth=2,
                      label='LES $\\Delta$ = 25m')
@@ -148,7 +148,7 @@ if plotting == 'og_vs_HCs':
             else:
                 plt.title(f'BOMEX: Smag 0.23 (dot) vs ' + '$C_s$ prof (dash)')
             plt.tight_layout(pad=0.5)
-            #plt.gcf().set_size_inches(3, 5)
+            plt.gcf().set_size_inches(3, 5)
             plt.legend(fontsize=13, loc='upper right')
 
             bottom, top = plt.ylim()
@@ -210,7 +210,7 @@ elif plotting == 'HCs_vs_SAHCs':
                     ds_in = xr.open_dataset(path_in + filein)
                     var_prof[6, :] = np.mean(ds_in[f'{var}'].data, axis=0)
 
-            plt.plot(figsize=(5,8))
+            plt.plot(figsize=(4,8))
 
             plt.plot(var_prof[6, :], zn/cloudtop25[nt], 'k', linewidth=2,
                      label='LES $\\Delta$ = 25m')
@@ -232,7 +232,7 @@ elif plotting == 'HCs_vs_SAHCs':
             else:
                 plt.title(f'BOMEX:'+' $C_s$ prof (dot) vs S-A $C_s$ prof (dash)')
             plt.tight_layout(pad=0.5)
-            #plt.gcf().set_size_inches(3, 5)
+            plt.gcf().set_size_inches(3, 5)
             plt.legend(fontsize=13, loc='upper right')
 
             bottom, top = plt.ylim()
@@ -297,7 +297,7 @@ elif plotting == 'SAHCs_vs_SA_Smag':
                     ds_in = xr.open_dataset(path_in + filein)
                     var_prof[6, :] = np.mean(ds_in[f'{var}'].data, axis=0)
 
-            plt.plot(figsize=(5,8))
+            plt.plot(figsize=(4,8))
 
             plt.plot(var_prof[6, :], zn/cloudtop25[nt], 'k', linewidth=2,
                      label='LES $\\Delta$ = 25m')
@@ -391,7 +391,7 @@ elif plotting == 'SAHCs_vs_SAHCsCth_L':
                     ds_in = xr.open_dataset(path_in + filein)
                     var_prof[6, :] = np.mean(ds_in[f'{var}'].data, axis=0)
 
-            plt.plot(figsize=(5,8))
+            plt.plot(figsize=(4,8))
 
             plt.plot(var_prof[6, :], zn/cloudtop25[nt], 'k', linewidth=2,
                      label='LES $\\Delta$ = 25m')
