840647d6a304243961e262e6432877a8083d5b0d
a-power <a.power@pgr.reading.ac.uk>
2025-04-23 17:38:47 +0100

    plot to pdf

--- spectra_from_notebook.py
+++ spectra_from_notebook.py
@@ -166,33 +166,34 @@ turb_slope_y = turb_slope_x**(-5/3)
 filt_slope_x = np.linspace(0.5,1,50)
 filt_slope_y = filt_slope_x**(-11/2)
 
+w_star=2
 
-ax.loglog(w_kpo*z_ml_height/(2*np.pi), w_spec[:,z_set], lw = 2, label="$\\overline{\\Delta}$ = 25m")
-#ax.loglog(w_kpo_50*z_ml_height/(2*np.pi), w_spec_50[:,z_set_50], lw = 2, label="$\\overline{\\Delta}$ = 50m")
+ax.loglog(w_kpo*z_ml_height/(2*np.pi), w_spec[:,z_set]/w_star, lw = 2, label="$\\overline{\\Delta}$ = 25m")
+ax.loglog(w_kpo_50*z_ml_height/(2*np.pi), w_spec_50[:,z_set_50]/w_star, lw = 2, label="$\\overline{\\Delta}$ = 50m")
 
-ax.loglog(w_kpo_filt1g1*z_ml_height/(2*np.pi), w_filt_1st_0[:,z_set], '--',  label="$\\sigma$ = 25m") #\\Delta
-ax.loglog(w_kpo_filt2g1*z_ml_height/(2*np.pi), w_filt_1st_1[:,z_set], '--',  label="$\\sigma$ = 50m")
-ax.loglog(w_kpo_filt3g1*z_ml_height/(2*np.pi), w_filt_1st_2[:,z_set], '--',  label="$\\sigma$ = 100m")
-ax.loglog(w_kpo_filt4g1*z_ml_height/(2*np.pi), w_filt_1st_3[:,z_set], '--', label="$\\sigma$ = 200m")
-# ax.loglog(w_kpo_filt5g1*z_ml_height/(2*np.pi), w_filt_1st_4[:,z_set], label="$\\sigma$ = 400m")
-# ax.loglog(w_kpo_filt6g1*z_ml_height/(2*np.pi), w_filt_1st_5[:,z_set], label="$\\sigma$ = 800m")
+ax.loglog(w_kpo_filt1g1*z_ml_height/(2*np.pi), w_filt_1st_0[:,z_set]/w_star, '--',  label="$\\sigma$ = 25m") #\\Delta
+ax.loglog(w_kpo_filt2g1*z_ml_height/(2*np.pi), w_filt_1st_1[:,z_set]/w_star, '--',  label="$\\sigma$ = 50m")
+ax.loglog(w_kpo_filt3g1*z_ml_height/(2*np.pi), w_filt_1st_2[:,z_set]/w_star, '--',  label="$\\sigma$ = 100m")
+ax.loglog(w_kpo_filt4g1*z_ml_height/(2*np.pi), w_filt_1st_3[:,z_set]/w_star, '--', label="$\\sigma$ = 200m")
+# ax.loglog(w_kpo_filt5g1*z_ml_height/(2*np.pi), w_filt_1st_4[:,z_set]/w_star, label="$\\sigma$ = 400m")
+# ax.loglog(w_kpo_filt6g1*z_ml_height/(2*np.pi), w_filt_1st_5[:,z_set]/w_star, label="$\\sigma$ = 800m")
 ax.loglog(turb_slope_x, 60*turb_slope_y, 'k-') #0.015
 ax.text(4, 8, r'$k^{-5/3}$', fontsize=14)
 
-ax.loglog(w_kpo_filt1g2*z_ml_height/(2*np.pi), w_filt_2nd_0[:,z_set], '-.', label="$\\sigma$ = 25m, $\\sigma$ = 25m")
-ax.loglog(w_kpo_filt2g2*z_ml_height/(2*np.pi), w_filt_2nd_1[:,z_set], '-.', label="$\\sigma$ = 50m, $\\sigma$ = 50m")
-ax.loglog(w_kpo_filt3g2*z_ml_height/(2*np.pi), w_filt_2nd_2[:,z_set], '-.', label="$\\sigma$ = 100m, $\\sigma$ = 100m")
-ax.loglog(w_kpo_filt4g2*z_ml_height/(2*np.pi), w_filt_2nd_3[:,z_set], '-.', label="$\\sigma$ = 200m, $\\sigma$ = 200m")
-# ax.loglog(w_kpo_filt5g2*z_ml_height/(2*np.pi), w_filt_2nd_4[:,z_set], '--', label="$\\sigma$ = 400m, $\\sigma$ = 400m")
-# ax.loglog(w_kpo_filt6g2*z_ml_height/(2*np.pi), w_filt_2nd_5[:,z_set], '--', label="$\\sigma$ = 800m, $\\sigma$ = 800m")
+# ax.loglog(w_kpo_filt1g2*z_ml_height/(2*np.pi), w_filt_2nd_0[:,z_set]/w_star, '-.', label="$\\sigma$ = 25m, $\\sigma$ = 25m")
+# ax.loglog(w_kpo_filt2g2*z_ml_height/(2*np.pi), w_filt_2nd_1[:,z_set]/w_star, '-.', label="$\\sigma$ = 50m, $\\sigma$ = 50m")
+# ax.loglog(w_kpo_filt3g2*z_ml_height/(2*np.pi), w_filt_2nd_2[:,z_set]/w_star, '-.', label="$\\sigma$ = 100m, $\\sigma$ = 100m")
+# ax.loglog(w_kpo_filt4g2*z_ml_height/(2*np.pi), w_filt_2nd_3[:,z_set]/w_star, '-.', label="$\\sigma$ = 200m, $\\sigma$ = 200m")
+# ax.loglog(w_kpo_filt5g2*z_ml_height/(2*np.pi), w_filt_2nd_4[:,z_set]/w_star, '--', label="$\\sigma$ = 400m, $\\sigma$ = 400m")
+# ax.loglog(w_kpo_filt6g2*z_ml_height/(2*np.pi), w_filt_2nd_5[:,z_set]/w_star, '--', label="$\\sigma$ = 800m, $\\sigma$ = 800m")
 
-# ax.loglog(w_kpo_filt1g3*z_ml_height/(2*np.pi), w_filt_3rd_0[:,z_set], '-.',
-#           label="$\\sigma$ = 25m, $\\sigma$ = 50m")
-# ax.loglog(w_kpo_filt2g3*z_ml_height/(2*np.pi), w_filt_3rd_1[:,z_set], '-.',
-#           label="$\\sigma$ = 50m, $\\sigma$ = 100m")
+ax.loglog(w_kpo_filt1g3*z_ml_height/(2*np.pi), w_filt_3rd_0[:,z_set]/w_star, '-.',
+          label="$\\sigma$ = 25m, $\\sigma$ = 50m")
+ax.loglog(w_kpo_filt2g3*z_ml_height/(2*np.pi), w_filt_3rd_1[:,z_set]/w_star, '-.',
+          label="$\\sigma$ = 50m, $\\sigma$ = 100m")
 
-# ax.loglog(w_kpo_filt1check*z_ml_height/(2*np.pi), w_filt_check_0[:,z_set], ':', label="$\\sigma$ = 50m, $\\sigma$ = 25m")
-# ax.loglog(w_kpo_filt2check*z_ml_height/(2*np.pi), w_filt_check_1[:,z_set], ':', label="$\\sigma$ = 100m, $\\sigma$ = 50m")
+ax.loglog(w_kpo_filt1check*z_ml_height/(2*np.pi), w_filt_check_0[:,z_set]/w_star, ':', label="$\\sigma$ = 50m, $\\sigma$ = 25m")
+ax.loglog(w_kpo_filt2check*z_ml_height/(2*np.pi), w_filt_check_1[:,z_set]/w_star, ':', label="$\\sigma$ = 100m, $\\sigma$ = 50m")
 #
 
 
@@ -230,4 +231,5 @@ def ltok(l):
 secax = ax.secondary_xaxis('top', functions=(ltok, ktol))
 secax.set_xlabel('$\\lambda$', fontsize=14)
 
-plt.savefig(plotdir+"ARM_w_spectra_C_calc_first_and_second.pdf", pad_inches=0)
+#ARM_w_spectra_C_calc_first_and_second
+plt.savefig(plotdir+"ARM_w_spectra_testing_25_50_unscaled.pdf", pad_inches=0)
